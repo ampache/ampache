11201fad55f11974c19b9bafc182c6b207ca75a8
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2005-06-16 14:10:54 +0000

    clean theme

---
+++ contrib/themes/Clean/theme.cfg.php
@@ -0,0 +1,67 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# Clean Ampache Theme
+###########################
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "Clean Ampache"
+
+# Theme Author
+# This is just a way of giving credit to the
+# person who actually created this theme
+# DEFAULT: N/A
+author = "J"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+maintainer = "J <jumperooter@yahoo.com>"
+
+# Theme Colors
+###################
+[color]
+###################
+# Below is a list of the default colors for this theme, upon
+# applying this theme the users color preferences will be reset
+# to what is listed below...
+
+# Background Color 1
+bg_color1 = "#FFFFFF"
+
+# Background Color 2
+bg_color2 = "#E6EAC6"
+
+# Base Color 1
+base_color1 = "#bbbbbb"
+
+# Base Color 2
+base_color2 = "#E1E9EE"
+
+# Font Color 1-
+font_color1 = "#666666"
+
+# Font Color 2-
+font_color2 = "#3F5F5E"
+
+# Font Color 3-665975
+font_color3 = "#666666"
+
+# Row Color 1-
+row_color1 = "#ffffff"
+
+# Row Color 2-FCFCF8
+row_color2 = "#F7FAFF"
+
+# Row Color 3
+row_color3 = "#E6EAC6"
+
+# Error Color
+error_color = "#990033"
