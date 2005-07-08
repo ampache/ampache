bb0e979ad50c4b97178b10db00fb346d275de4fc
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2005-07-08 07:17:38 +0000

    added new theme from ros

---
+++ contrib/themes/IL01/theme.cfg.php
@@ -0,0 +1,68 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# IL01 
+###########################
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "IL01"
+
+# Theme Author
+# This is just a way of giving credit to the 
+# person who actually created this theme
+# DEFAULT: N/A
+#author = "Rodrigo Galindo (ros)"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+#maintainer = "Rodrigo Galindo <ros@interleucina.org>"
+
+# Theme Colors
+###################
+[color]
+###################
+# Below is a list of the default colors for this theme, upon
+# applying this theme the users color preferences will be reset
+# to what is listed below... 
+
+# Background Color 1
+bg_color1 = "#4B67A0"
+
+# Background Color 2
+bg_color2 = "#D1D0D1"
+
+# Base Color 1 
+base_color1 = "#FFFFFF"
+
+# Base Color 2 
+base_color2 = "#F5F5F5"
+
+# Font Color 1 
+font_color1 = "#222222"
+
+# Font Color 2
+font_color2 = "#000000"
+
+# Font Color 3
+font_color3 = "#ffffff"
+
+# Row Color 1 
+row_color1 = "#f3f3f3"
+
+# Row Color 2
+row_color2 = "#EDEDED"
+
+# Row Color 3
+row_color3 = "#F9F9F9"
+
+# Error Color
+error_color = "#990033"
+
