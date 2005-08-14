f0347f69cbf99a99af90d1d3b0cda2fa19ecd545
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2005-08-14 17:14:09 +0000

    new solar theme by TS

---
+++ contrib/themes/Solar/theme.cfg.php
@@ -0,0 +1,70 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# Solar Ampache Theme 
+###########################
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "Solar"
+
+# Theme Author
+# This is just a way of giving credit to the 
+# person who actually created this theme
+# DEFAULT: N/A
+#author = "Nobody You known where to find"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+#maintainer = "TS"
+
+version = "1.0"
+
+# Theme Colors
+###################
+[color]
+###################
+# Below is a list of the default colors for this theme, upon
+# applying this theme the users color preferences will be reset
+# to what is listed below... 
+
+# Background Color 1
+bg_color1 = "###"
+
+# Background Color 2
+bg_color2 = "###"
+
+# Base Color 1 
+base_color1 = "###"
+
+# Base Color 2 
+base_color2 = "#000000"
+
+# Font Color 1 
+font_color1 = "#4876FF"
+
+# Font Color 2
+font_color2 = "#8B8B00"
+
+# Font Color 3
+font_color3 = "#8B8B00"
+
+# Row Color 1 
+row_color1 = "#FFFF00"
+
+# Row Color 2
+row_color2 = "#FFFF00"
+
+# Row Color 3
+row_color3 = "#FFFF00"
+
+# Error Color
+error_color = "#990033"
+
