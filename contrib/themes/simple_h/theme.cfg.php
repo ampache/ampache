1e602edccc85d35293bde19dd2a451cbcd09bad7
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2005-08-29 15:58:59 +0000

    new theme

---
+++ contrib/themes/simple_h/theme.cfg.php
@@ -0,0 +1,74 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# Simple Ampache Theme 
+###########################
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "Simple_h"
+
+# Theme Author
+# This is just a way of giving credit to the 
+# person who actually created this theme
+# DEFAULT: N/A
+author = "Lars Olafsen"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+#maintainer = "Lars Olafsen" larsolafsen@gmail.com
+
+version = "1.0"
+
+#Image Credits
+#Please give credit where credit is due
+# background headphones by Alfonso Diaz http://sxc.hu/browse.phtml?f=view&id=293324
+
+# Theme Colors
+###################
+[color]
+###################
+# Below is a list of the default colors for this theme, upon
+# applying this theme the users color preferences will be reset
+# to what is listed below... 
+
+# Background Color 1
+bg_color1 = "#"
+
+# Background Color 2
+bg_color2 = "#"
+
+# Base Color 1 
+base_color1 = "#"
+
+# Base Color 2 
+base_color2 = "#"
+
+# Font Color 1 
+font_color1 = "#222"
+
+# Font Color 2
+font_color2 = "#222"
+
+# Font Color 3
+font_color3 = "#222"
+
+# Row Color 1 
+row_color1 = "#"
+
+# Row Color 2
+row_color2 = "#"
+
+# Row Color 3
+row_color3 = "#"
+
+# Error Color
+error_color = "#FF0000"
+
