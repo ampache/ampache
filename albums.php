be23e09225cb961e94dbd95c6f394a5d5d7036cb
xgizzmo <xgizzmo@ampache>
2005-09-06 19:12:59 +0000

    Resolved bug #78 and fixed a few html problems

--- albums.php
+++ albums.php
@@ -77,8 +77,8 @@ elseif ($_REQUEST['action'] === 'find_art') {
 	if ($result) {
 		show_confirmation(_("Album Art Located"),_("Album Art information has been located in Amazon. If incorrect, click \"Reset Album Art\" below to remove the artwork."),"/albums.php?action=show&amp;album=" . $album->id);
 		echo "&nbsp;[ <a href=\"" . conf('web_path') . "/albums.php?action=clear_art&amp;album_id=" . $album->id . "\">Reset Album Art</a> ]";
-		echo "<p align=left><img src=\"" . conf('web_path') . "/albumart.php?id=" . $album->id . "\"></p>";
-		echo "<p><form name=\"cover\" method=\"get\" action=\"".$_SERVER['PHP_SELF']."\">";
+		echo "<p align=left><img src=\"" . conf('web_path') . "/albumart.php?id=" . $album->id . "\" /></p>";
+		echo "<form name=\"coverart\" method=\"get\" action=\"".$_SERVER['PHP_SELF']."\">";
 		echo "Enter URL to album art ";
 		echo "<input type=\"text\" size=\"40\" id=\"cover\" name=\"cover\" value=\"\" />\n";
 		echo "<input type=\"hidden\" name=\"action\" value=\"find_art\" />\n";
@@ -88,7 +88,7 @@ elseif ($_REQUEST['action'] === 'find_art') {
 	}
         else {
                 show_confirmation(_("Album Art Not Located"),_("Album Art could not be located at this time. This may be due to Amazon being busy, or the album not being present in their collection."),"/albums.php?action=show&amp;album=" . $album->id);
-                echo "<p><form name=\"cover\" method=\"get\" action=\"".$_SERVER['PHP_SELF']."\">";
+                echo "<form name=\"coverart\" method=\"get\" action=\"".$_SERVER['PHP_SELF']."\">";
                 echo "Enter URL to album art ";
                 echo "<input type=\"text\" size=\"40\" id=\"cover\" name=\"cover\" value=\"\" />";
                 echo "<input type=\"hidden\" name=\"action\" value=\"find_art\" />";
