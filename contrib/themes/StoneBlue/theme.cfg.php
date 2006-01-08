a3b348ff76a133966173db3cc7a741f342ac5c63
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2006-01-08 00:52:15 +0000

    new stoneblue theme

--- contrib/themes/StoneBlue/theme.cfg.php
+++ contrib/themes/StoneBlue/theme.cfg.php
@@ -3,7 +3,7 @@
 ##################
 
 ###########################
-# Classic Ampache Theme
+# StoneBlue Ampache Theme
 ###########################
 
 # Theme Name
@@ -25,7 +25,21 @@ author = "SoundOfEmotion"
 # DEFAULT: N/A
 maintainer = "SoundOfEmotion <soundofemotion@gmail.com>"
 
-version = "1.0"
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "2.0"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+#submenu = "simple"
 
 # Theme Colors
 ###################
