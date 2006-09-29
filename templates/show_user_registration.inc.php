1fee2840f53567aa29249c9699faa85d9fb161a1
pb1dft <pb1dft@ampache>
2006-09-29 23:41:19 +0000

    Stylesheet adjustment and some text adjustment

--- templates/show_user_registration.inc.php
+++ templates/show_user_registration.inc.php
@@ -27,8 +27,8 @@ $htmllang = str_replace("_","-",conf('lang'));
 <head>
 <link rel="shortcut icon" href="<?php echo conf('web_path'); ?>/favicon.ico" />
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo conf('site_charset'); ?>" />
-<link rel="stylesheet" href="<?php echo $web_path; ?>/templates/default.css" type="text/css" />
-<link rel="stylesheet" href="<?php echo $web_path; ?><?php echo conf('theme_path'); ?>/templates/default.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo conf('web_path'); ?>/templates/default.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo conf('web_path'); ?><?php echo conf('theme_path'); ?>/templates/default.css" type="text/css" />
 <title><?php echo conf('site_title'); ?> - <?php echo $location['title']; ?></title>
 </head>
 <body>
