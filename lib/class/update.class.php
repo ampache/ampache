278434b60106387171ab51d036b3195d121de46f
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2006-09-30 21:25:54 +0000

    arg!

--- lib/class/update.class.php
+++ lib/class/update.class.php
@@ -1664,7 +1664,7 @@ class Update {
 		$sql = "ALTER TABLE `access_list` ADD INDEX `level` (`level`)";
 		$db_results = mysql_query($sql, dbh());
 
-		$Sql = "ALTER TABLE `access_list` ADD `type` VARCHAR( 64 ) NOT NULL DEFAULT 'interface' AFTER `level`";
+		$sql = "ALTER TABLE `access_list` ADD `type` VARCHAR( 64 ) NOT NULL DEFAULT 'interface' AFTER `level`";
 		$db_results = mysql_query($sql, dbh());
 
 		// Album Table
