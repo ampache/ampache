8257b8bd4ba57f8c433a5642de5d2d35e45de280
pb1dft <pb1dft@ampache>
2006-10-12 22:54:01 +0000

    Theme made compatible with newest version of Ampache

--- contrib/themes/Supernova/theme.cfg.php
+++ contrib/themes/Supernova/theme.cfg.php
@@ -3,7 +3,7 @@
 ##################
 
 ###########################
-# Supernova
+# Burgundy Ampache Theme 
 ###########################
 
 # Theme Name
@@ -16,16 +16,30 @@ name = "Supernova"
 # This is just a way of giving credit to the 
 # person who actually created this theme
 # DEFAULT: N/A
-author = "Ripmaster TS"
+author = "Ripmaster TS modified by Terence Theijn"
 
 # Theme Maintainer
 # This is just a way of listing who is responsible for
 # maintaining this theme incase it's not working right
 # please include an e-mail address so you can be contacted
 # DEFAULT: N/A
-#maintainer = "no@mail!"
+#maintainer = "no@mail"
 
-version = "1.0"
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "1.2"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal 
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the 
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+# submenu = "simple"
 
 # Theme Colors
 ###################
@@ -41,13 +55,13 @@ bg_color1 = "#000000"
 # Background Color 2
 bg_color2 = "#000000"
 
-# Base Color 1 
+# Base Color 1
 base_color1 = "#000000"
 
-# Base Color 2 
+# Base Color 2
 base_color2 = "#000000"
 
-# Font Color 1 
+# Font Color 1
 font_color1 = "#BBBBBB"
 
 # Font Color 2
@@ -56,7 +70,7 @@ font_color2 = "#BBBBBB"
 # Font Color 3
 font_color3 = "#FFFFFF"
 
-# Row Color 1 
+# Row Color 1
 row_color1 = "#404040"
 
 # Row Color 2
