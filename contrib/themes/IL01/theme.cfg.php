859fa01c1be7b5a5ab2408db874ed11967a7b552
pb1dft <pb1dft@ampache>
2006-10-13 00:14:02 +0000

    Theme made compatible with newest version of Ampache

--- contrib/themes/IL01/theme.cfg.php
+++ contrib/themes/IL01/theme.cfg.php
@@ -16,7 +16,7 @@ name = "IL01"
 # This is just a way of giving credit to the 
 # person who actually created this theme
 # DEFAULT: N/A
-author = "Rodrigo Galindo (ros)"
+author = "Rodrigo Galindo (ros) modified by Terence Theijn"
 
 # Theme Maintainer
 # This is just a way of listing who is responsible for
@@ -24,6 +24,22 @@ author = "Rodrigo Galindo (ros)"
 # please include an e-mail address so you can be contacted
 # DEFAULT: N/A
 #maintainer = "Rodrigo Galindo <ros@interleucina.org>"
+
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "1.1"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+# submenu = "simple"
 
 # Theme Colors
 ###################
