202407da68e507cc3d66c4902747367506fdd514
pb1dft <pb1dft@ampache>
2006-10-15 11:00:44 +0000

    Theme made compatible with newest version of Ampache

--- contrib/themes/Solar/theme.cfg.php
+++ contrib/themes/Solar/theme.cfg.php
@@ -16,16 +16,30 @@ name = "Solar"
 # This is just a way of giving credit to the 
 # person who actually created this theme
 # DEFAULT: N/A
-#author = "Nobody You known where to find"
+author = "TS modified by Terence Theijn"
 
 # Theme Maintainer
 # This is just a way of listing who is responsible for
 # maintaining this theme incase it's not working right
 # please include an e-mail address so you can be contacted
 # DEFAULT: N/A
-#maintainer = "TS"
+maintainer = "TS"
 
-version = "1.0"
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "1.1"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+# submenu = "simple"
 
 # Theme Colors
 ###################
