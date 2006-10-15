f4bf617e40d00d2670fe2b21b61de5e17473ed4b
pb1dft <pb1dft@ampache>
2006-10-15 19:18:40 +0000

    Theme made compatible with newest version of Ampache

--- contrib/themes/Spiffy/theme.cfg.php
+++ contrib/themes/Spiffy/theme.cfg.php
@@ -16,7 +16,7 @@ name = "Spiffy"
 # This is just a way of giving credit to the 
 # person who actually created this theme
 # DEFAULT: N/A
-author = "Ripmaster TS"
+author = "Ripmaster TS modified by Terence Theijn"
 
 # Theme Maintainer
 # This is just a way of listing who is responsible for
@@ -25,7 +25,21 @@ author = "Ripmaster TS"
 # DEFAULT: N/A
 #maintainer = "no@mail.org
 
-version = "1.0"
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "1.1"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal
+# theme then it will not show the quick search and quick random play forms
+orientation = "vertical"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+# submenu = "simple"
 
 # Theme Colors
 ###################
