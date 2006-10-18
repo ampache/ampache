cba25b570f58d4cbbc0eefbd4b9840b8dc4f0a0b
pb1dft <pb1dft@ampache>
2006-10-18 21:09:44 +0000

    added new theme for INHOLLAND University

---
+++ contrib/themes/INHOLLAND/theme.cfg.php
@@ -0,0 +1,44 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# Simple Matrix Ampache Theme 
+###########################
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "Hogeschool INHOLLAND"
+
+# Theme Author
+# This is just a way of giving credit to the 
+# person who actually created this theme
+# DEFAULT: N/A
+author = "Terence Theijn"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+maintainer = "Terence Theijn <terence.theijn@gmail.com>"
+
+# Version
+# This is the revision for this theme, It's up to the maintainer
+# or author to decide how they want to format this
+version = "1.0"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+#submenu = "simple"
+
