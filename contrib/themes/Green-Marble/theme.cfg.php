d93527bece43eb4a3a564d892ba9ddc653ac11b0
pb1dft <pb1dft@ampache>
2006-10-23 22:08:56 +0000

    Added version info

--- contrib/themes/Green-Marble/theme.cfg.php
+++ contrib/themes/Green-Marble/theme.cfg.php
@@ -25,4 +25,20 @@ author = "XGizzmo"
 # DEFAULT: N/A
 maintainer = "XGizzmo  irc.freenode.net #ampache"
 
+# Version
+# This is the revision for this theme, It's up to the maintainer
+# or author to decide how they want to format this
+version = "1.1"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal
+# theme then it will not show the quick search and quick random play forms
+orientation = "vertical"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+#submenu = "simple"
 
