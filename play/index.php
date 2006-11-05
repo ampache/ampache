673dfd2bff70bdfc1489d6f8217c58e8e964cce2
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2006-11-05 10:02:43 +0000

    wups gotta wrap that baby

--- play/index.php
+++ play/index.php
@@ -256,8 +256,10 @@ while (!feof($fp) && (connection_status() == 0)) {
 if ($bytesStreamed > $minBytesStreamed) {
         $user->update_stats($song_id);
 	/* If this is a voting tmp playlist remove the entry */
-	if ($tmp_playlist->type == 'vote') { 
-		$tmp_playlist->delete_track($song_id);
+	if (is_object($tmp_playlist) { 
+		if ($tmp_playlist->type == 'vote') { 
+			$tmp_playlist->delete_track($song_id);
+		}
 	}
 } 
 
