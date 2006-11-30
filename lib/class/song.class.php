0af24517dc17d953d5cc8fcd6f7f848d27f96163
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2006-11-30 17:18:13 +0000

    wups bad links

--- lib/class/song.class.php
+++ lib/class/song.class.php
@@ -609,8 +609,8 @@ class Song {
 
 		// Create Links for the different objects 
 		$this->f_link = "<a href=\"" . conf('web_path') . "/song.php?action=single_song&amp;song_id=" . $this->id . "\">$this->f_title</a>";
-		$this->f_album_link = "<a href=\"" . conf('web_path') . "/album.php?action=show&amp;album_id=" . $this->album . "\">$this->f_album</a>";
-		$this->f_artist_link = "<a href=\"" . conf('web_path') . "/artist.php?action=show&amp;artist_id=" . $this->artist . "\">$this->f_artist</a>";	
+		$this->f_album_link = "<a href=\"" . conf('web_path') . "/albums.php?action=show&amp;album=" . $this->album . "\">$this->f_album</a>";
+		$this->f_artist_link = "<a href=\"" . conf('web_path') . "/artists.php?action=show&amp;artist=" . $this->artist . "\">$this->f_artist</a>";	
 
 		// Format the Bitrate
 		$this->f_bitrate = intval($this->bitrate/1000) . "-" . strtoupper($this->mode);
