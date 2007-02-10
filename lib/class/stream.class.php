66de7ffff1c95e5bb415f1c3b56966f2d9916864
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2007-02-10 23:12:34 +0000

    fixed old reference to libglueconf

--- lib/class/stream.class.php
+++ lib/class/stream.class.php
@@ -137,7 +137,7 @@ class Stream {
 			if ($song->type == ".flac") { $song->type = ".ogg"; }
 	                $song_name = $song->f_artist_full . " - " . $song->title . "." . $song->type;
 	                echo "#EXTINF:$song->time," . $song->f_artist_full . " - " . $song->title . "\n";
-	                $sess = $_COOKIE[libglue_param('sess_name')];
+	                $sess = $_COOKIE[conf('sess_name')];
 	                if($GLOBALS['user']->prefs['play_type'] == 'downsample') {
 	                	$ds = $GLOBALS['user']->prefs['sample_rate'];
 			}
