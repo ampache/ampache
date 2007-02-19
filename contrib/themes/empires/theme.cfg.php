ce617e69811065253acdb2cc4009d9ee2280d804
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2007-02-19 20:07:07 +0000

    added two themes and removed one I know is dead..

---
+++ contrib/themes/empires/theme.cfg.php
@@ -0,0 +1,65 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# Ampache Theme: Empires 
+###########################
+
+# Notes
+# This theme is based off a Joomla! template called js_empire.
+# I've been running the 3.3.2 Ampache with this color scheme
+# hard-coded in preferences for a while and decided it was time
+# to make a new theme, and make it work with the new Ampache.
+#
+# 01/15/07 updated to include color scheming for user_offline/
+# user_online classes, removed some no-longer-used stuff from
+# this file, changed the blank-ampache.gif.
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "Empires"
+
+# Theme Author
+# This is just a way of giving credit to the 
+# person who actually created this theme
+# DEFAULT: N/A
+author = "jazzs3quence"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+maintainer = "jazzs3quence -- c dot s dot reynolds at gmail dot com"
+
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "1.3.1"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal 
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the 
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+# submenu = "simple"
+
+# Theme Colors
+###################
+[color]
+###################
+# this isn't really used anymore, so the list below is the basic theme colors used.
+
+# background                  = "#000000"
+# text boxes and stuff        = "#1D1D1D"
+# plain text and mouseovers   = "#0099FF"
+# links                       = "#F9F9F9"
+# errors                      = "#990033"
+
