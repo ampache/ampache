a12f1083e30ae16ded9dc7aa464015ad07413632
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2007-04-23 19:23:57 +0000

    two new themes

---
+++ contrib/themes/exterminal/theme.cfg.php
@@ -0,0 +1,58 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# Ampache Theme: exterminal 
+###########################
+
+# Notes
+# i wrote this theme because i was inspired by exterminal.net and wanted
+# to make a sci-fi looking theme.  also, i'm depressed that no one continued
+# the simple matrix theme that was for ampache 3.2 and never got fixed up
+# for 3.3.  i used some of apex's icons from the apex and aphelion themes.
+# ALSO:
+# i had issues with the transparency for ampache.gif.  so i tweaked the header.inc
+# to use ampache.png.  i've provided a hacked an unhacked copy.  just copy the 
+# included header.inc to your /templates folder.  if you want to use a different
+# theme, copy the header.inc.bak to /templates and rename it to header.inc
+#
+# ~jazzs3quence (c <dot> s <dot> reynolds <at> gmail <dot> com)
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "exterminal"
+
+# Theme Author
+# This is just a way of giving credit to the 
+# person who actually created this theme
+# DEFAULT: N/A
+author = "jazzs3quence"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+maintainer = "jazzs3quence -- c dot s dot reynolds at gmail dot com"
+
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "1.0"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal 
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the 
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+# submenu = "simple"
+
+
+
---
+++ contrib/themes/exterminal_exhibitA/theme.cfg.php
@@ -0,0 +1,54 @@
+##################
+#<?php exit(); ?>#
+##################
+
+###########################
+# Ampache Theme: exterminal: exhibitA 
+###########################
+
+# Notes
+# i wrote this theme because i was inspired by exterminal.net and wanted
+# to make a sci-fi looking theme.  this is the second theme i've made with 
+# this look (see: exterminal).  i wanted to do something that looked a bit 
+# more artistic and colorful and made more use of using png instead of gif
+# for the ampache logo.  i used some of apex's icons from the apex and 
+# aphelion themes.
+# ~jazzs3quence (c <dot> s <dot> reynolds <at> gmail <dot> com)
+
+# Theme Name
+# This is the actual name of the theme that
+# will be displayed in the preferences screen
+# DEFAULT: ampache-theme
+name = "exterminal_exhibitA"
+
+# Theme Author
+# This is just a way of giving credit to the 
+# person who actually created this theme
+# DEFAULT: N/A
+author = "jazzs3quence"
+
+# Theme Maintainer
+# This is just a way of listing who is responsible for
+# maintaining this theme incase it's not working right
+# please include an e-mail address so you can be contacted
+# DEFAULT: N/A
+maintainer = "jazzs3quence -- c dot s dot reynolds at gmail dot com"
+
+# Version
+# This is the version of the Theme (usefull if you've updated it)
+version = "1.0"
+
+# Orientation
+# This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+# uses vertical or horizontal orientation of the menu, if this is a horizontal 
+# theme then it will not show the quick search and quick random play forms
+orientation = "horizontal"
+
+# Submenu
+# If this is set to simple the sub menu's will only be shown when you're on one of the 
+# respective pages. If you want to make the menu's something like the classic theme
+# comment this out
+# submenu = "simple"
+
+
+
