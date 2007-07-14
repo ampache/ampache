6a929af1bf1953b74471220e22df22321b71630d
nf <nf@ampache>
2007-07-14 10:13:14 +0000

    Modified versions of now_playing_row.inc - Hopefully no bugs inside

--- templates/show_now_playing_row.inc.php
+++ templates/show_now_playing_row.inc.php
@@ -24,26 +24,24 @@ $title = scrub_out(truncate_with_ellipse($song->title,'25'));
 $album = scrub_out(truncate_with_ellipse($song->f_album_full,'25'));
 $artist = scrub_out(truncate_with_ellipse($song->f_artist_full,'25'));
 ?>
-<td class="np_cell">
-	<a href="<?php echo $web_path; ?>/stats.php?action=show_user&amp;user_id=<?php echo $np_user->id; ?>">
-		<?php echo scrub_out($np_user->fullname); ?>
-	</a>
+<td class="np_cell_left"><b><?php echo _('Username'); ?></b>:<br><?php echo scrub_out($np_user->fullname); ?><br><br><!-- ##  modified ##-->
+    <b><?php echo _('Song'); ?></b><br /><a title="<?php echo scrub_out($song->title); ?>" href="<?php echo $web_path; ?>/song.php?action=single_song&amp;song_id=<?php echo $song->id; ?>">
+        <?php echo $title; ?>
+    </a>
+    <?php if(conf('ratings')) { ?>
+        <br /><?php show_rating($song->id,'song'); ?>
+    <?php } ?>
 </td>
-<td class="np_cell">
-	<a title="<?php echo scrub_out($song->title); ?>" href="<?php echo $web_path; ?>/song.php?action=single_song&amp;song_id=<?php echo $song->id; ?>">
-		<?php echo $title; ?>
-	</a>
-</td>
-<td class="np_cell">
-	<a title="<?php echo scrub_out($song->f_album_full); ?>" href="<?php echo $web_path; ?>/albums.php?action=show&amp;album=<?php echo $song->album; ?>">
-		<?php echo $album; ?></a> /
-	<a title="<?php echo scrub_out($song->f_artist_full); ?>" href="<?php echo $web_path; ?>/artists.php?action=show&amp;artist=<?php echo $song->artist; ?>">
-		<?php echo $artist; ?>
-	</a>
+<td class="np_cell_m">
+    <b><?php echo _('Artist'); ?></b><br /><a title="<?php echo scrub_out($song->f_artist_full); ?>" href="<?php echo $web_path; ?>/artists.php?action=show&amp;artist=<?php echo $song->artist; ?>">
+        <?php echo $artist; ?>
+    </a><br /><br />
+    <b><?php echo _('Album'); ?></b><br /><a title="<?php echo scrub_out($song->f_album_full); ?>" href="<?php echo $web_path; ?>/albums.php?action=show&amp;album=<?php echo $song->album; ?>">
+        <?php echo $album; ?></a>
 </td>
 	<?php if (Config::get('show_album_art')) { ?>
-<td class="np_cell">
-	<a target="_blank" href="<?php echo $web_path; ?>/image.php?id=<?php echo $song->album; ?>&amp;type=popup&amp;sid=<?php echo session_id(); ?>" onclick="popup_art('<?php echo $web_path; ?>/image.php?id=<?php echo $song->album; ?>&amp;type=popup&amp;sid=<?php echo session_id(); ?>'); return false;">
-	<img align="middle" border="0" src="<?php echo $web_path; ?>/image.php?id=<?php echo $song->album; ?>&amp;thumb=1&amp;sid=<?php echo session_id(); ?>" alt="Album Art" height="75" width="75" /></a>
+<td class="np_cell_right">
+    <a target="_blank" href="<?php echo $web_path; ?>/image.php?id=<?php echo $song->album; ?>&amp;type=popup&amp;sid=<?php echo session_id(); ?>" onclick="popup_art('<?php echo $web_path; ?>/image.php?id=<?php echo $song->album; ?>&amp;type=popup&amp;sid=<?php echo session_id(); ?>'); return false;">
+    <img align="middle" border="0" src="<?php echo $web_path; ?>/image.php?id=<?php echo $song->album; ?>&amp;thumb=1&amp;sid=<?php echo session_id(); ?>" alt="Album Art" height="75" width="75" /></a>
 </td>
 <?php } // end play album art ?>
