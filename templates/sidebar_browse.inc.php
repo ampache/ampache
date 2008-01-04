b55fd99e5e5dc39f13e02869a6f6c885b0880a8f
pb1dft <pb1dft@ampache>
2008-01-04 06:38:22 +0000

    Some headers modified

--- templates/sidebar_browse.inc.php
+++ templates/sidebar_browse.inc.php
@@ -1,4 +1,27 @@
-<?php $ajax_info = Config::get('ajax_url'); $web_path = Config::get('web_path'); ?>
+<?php 
+/*
+
+ Copyright (c) 2001 - 2007 Ampache.org
+ All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License v2
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+$ajax_info = Config::get('ajax_url'); $web_path = Config::get('web_path'); 
+
+?>
 <ul class="sb2" id="sb_browse">
   <li><h4><?php echo _('Browse By'); ?></h4>
   <?php 
