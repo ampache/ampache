f4bd8ffecab6a060252c93379b3c6b1763ab6392
spocky <spocky@ampache>
2008-01-29 22:14:31 +0000

    Moved star rating reset symbol to the left Updated Greysme theme config file

--- themes/greysme/theme.cfg.php
+++ themes/greysme/theme.cfg.php
@@ -1,25 +1,3 @@
-<?php
-/*
-
- Copyright (c) 2001 - 2007 Ampache.org
- All rights reserved.
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License v2
- as published by the Free Software Foundation.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-*/
-
-?>
 ;;;;;;;;;;;;;;;;;;
 ;<?php exit(); ?>;
 ;;;;;;;;;;;;;;;;;;
@@ -40,7 +18,7 @@
 ; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Greysme Ampache Theme 
+; Alternate Ampache Theme 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Theme Name
@@ -53,14 +31,14 @@ name = "Greysme"
 ; This is just a way of giving credit to the 
 ; person who actually created this theme
 ; DEFAULT: N/A
-author = "SPocKy"
+author = "Spocky"
 
 ; Theme Maintainer
 ; This is just a way of listing who is responsible for
 ; maintaining this theme incase it's not working right
 ; please include an e-mail address so you can be contacted
 ; DEFAULT: N/A
-maintainer = "SPocKy"
+maintainer = "Spocky"
 
 ; Orientation
 ; This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
