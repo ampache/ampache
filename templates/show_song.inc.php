6952af791f180929bb0e31c5f2e0942fe25df86d
spocky <spocky@ampache>
2008-04-18 23:10:30 +0000

    added "add to basket" and "download file" options on song page

--- templates/show_song.inc.php
+++ templates/show_song.inc.php
@@ -22,9 +22,9 @@
 <?php 
   show_box_top($song->title . ' ' . _('Details')); 
   
-  $songprops['Title']   = scrub_out($song->title);
+  $songprops['Title']   = scrub_out($song->title) . Ajax::button('?action=basket&type=song&id=' . $song->id,'add',_('Add'),'add_' . $song->id);
   $songprops['Artist']  = $song->f_artist_link;
-  $songprops['Album']   = $song->f_album_link . " " . scrub_out($song->year);
+  $songprops['Album']   = $song->f_album_link . " (" . scrub_out($song->year). ")";
   $songprops['Genre']   = $song->f_genre_link;
   $songprops['Length']  = scrub_out($song->f_time); 
   $songprops['Comment'] = scrub_out($song->comment);
@@ -35,6 +35,9 @@
   if ($GLOBALS['user']->has_access('75')) {
     $songprops['Filename']   = scrub_out($song->file) . " " . $song->f_size . "MB";
   }
+  if (Config::get('download')) {
+  	$songprops['Filename'] = "<a href=\"" . Config::get('web_path') . "/stream.php?action=download&amp;song_id=" . $song->id . "\">" . $songprops['Filename'] . "</a>";
+	}
   if ($song->update_time) {
     $songprops['Last Updated']   = date("d/m/Y H:i",$song->update_time);
   }
