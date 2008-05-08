efd932b9fde167ec1a54809fe5fd9286f29eda2c
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2008-05-08 06:10:17 +0000

    fixed some copyright lines and removed unused file

--- modules/kajax/ajax.js
+++ modules/kajax/ajax.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2001 - 2007 Ampache.org
+// Copyright (c) Ampache.org
 // All rights reserved.
 //
 // This program is free software; you can redistribute it and/or
--- shout.php
+++ shout.php
@@ -1,7 +1,7 @@
 <?php
 /*
 
- Copyright (c) 2001 - 2007 ampache.org
+ Copyright (c) Ampache.org
  All rights reserved.
 
  This program is free software; you can redistribute it and/or
