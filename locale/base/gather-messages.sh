c16f287900ca5ec869b151af00759ea852f39287
momo-i <momo-i@ampache>
2008-08-25 00:28:39 +0000

    remove merge shell and integrate gather-messages.sh.

--- locale/base/gather-messages.sh
+++ locale/base/gather-messages.sh
@@ -17,8 +17,17 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
+PATH=$PATH:/bin:/usr/bin:/usr/local/bin
+
+# gettext package test
+if [ ! -x `which xgettext` ]
+then
+	echo "xgettext was not found. please install gettext packages."
+	exit 0;
+fi
+
 SCRIPTS=$0
-MAILADDR="translation@ampache.org"
+MAILADDR="translations at ampache.org"
 OLANG=`echo $LANG | sed 's/\..*//;'`
 POTNAME="messages.pot"
 PODIR="../$OLANG/LC_MESSAGES"
@@ -27,34 +36,49 @@ MONAME="messages.mo"
 
 if [ ! -d $PODIR ]
 then
-       mkdir -p $PODIR
-       echo "$PODIR has created."
+	mkdir -p $PODIR
+	echo "$PODIR has created."
 fi
 
 if [ $# = 0 ]; then
-    echo "usage: $SCRIPTS [--help|--get|--init|--merge]"
-    exit
+	echo "usage: $SCRIPTS [--help|--get|--init|--merge|--format|--all]"
+	exit
 fi
 
 case $1 in
-        "--get"|"-g"|"get")
-                xgettext --from-code=UTF-8 --msgid-bugs-address=$MAILADDR -L php -o $POTNAME `find ../../ -name \*.php -type f` `find ../../ -name \*.inc -type f`;
-                if [ $? = 0 ]; then
-                        echo "pot file creation was done.";
-                else
-                        echo "pot file creation wasn't done.";
-                fi
-                ;;
-        "--init"|"-i"|"init")
-                msginit -l $LANG -i $POTNAME -o $PODIR/$PONAME;
-                ;;
-        "--format"|"-f"|"format")
-                msgfmt -v --check $PODIR/$PONAME -o $PODIR/$MONAME;
-                ;;
-        "--merge"|"-m"|"merge")
-                msgmerge --update $PODIR/$PONAME $POTNAME;
-                ;;
-        "--help"|"-h"|"help"|"*")
-                echo "usage: $SCRIPTS [--help|--get|--init|--merge]";
-                ;;
+	"--all"|"-a"|"all")
+		xgettext --from-code=UTF-8 --msgid-bugs-address="$MAILADDR" -L php -o $POTNAME `find ../../ -name \*.php -type f` `find ../../ -name \*.inc -type f`
+		OLANG=`ls ../ | grep -v base`
+		for i in $OLANG
+		do
+			echo "$i PO file merging..."
+			msgmerge --update ../$i/LC_MESSAGES/messages.po messages.pot
+			echo "$i MO file creating..."
+			msgfmt -v -c ../$i/LC_MESSAGES/messages.po -o ../$i/LC_MESSAGES/messages.mo
+			rm -f ../$i/LC_MESSAGES/messages.po~
+		done
+		;;
+	"--get"|"-g"|"get")
+		xgettext --from-code=UTF-8 --msgid-bugs-address=$MAILADDR -L php -o $POTNAME `find ../../ -name \*.php -type f` `find ../../ -name \*.inc -type f`;
+		if [ $? = 0 ]; then
+			echo "pot file creation was done.";
+		else
+			echo "pot file creation wasn't done.";
+		fi
+		;;
+	"--init"|"-i"|"init")
+		msginit -l $LANG -i $POTNAME -o $PODIR/$PONAME;
+		;;
+	"--format"|"-f"|"format")
+		msgfmt -v --check $PODIR/$PONAME -o $PODIR/$MONAME;
+		;;
+	"--merge"|"-m"|"merge")
+		msgmerge --update $PODIR/$PONAME $POTNAME;
+		rm -f $PODIR/messages.po~
+		;;
+	"--help"|"-h"|"help"|"*")
+		echo "usage: $SCRIPTS [--help|--get|--init|--merge|--format|--all]";
+		echo "";
+		echo "Please read for translation: http://ampache.org/wiki/dev:translation";
+		;;
 esac
