ef9ebefaddfb5a5b3b6cf39dfb585f0a7dc8fd05
momo-i <momo-i@ampache>
2008-10-02 05:24:10 +0000

    Fixed #309: $web_path (Thx juanjo)

--- templates/show_democratic_playlist.inc.php
+++ templates/show_democratic_playlist.inc.php
@@ -18,6 +18,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
+$web_path = Config::get('web_path');
 ?>
 <table class="tabledata" cellpadding="0" cellspacing="0">
 <colgroup>
