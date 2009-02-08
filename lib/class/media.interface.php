00f3ef062c9187b5c75f1a2b0c553f139e83ee8d
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2009-02-08 00:46:05 +0000

    forgot interface

---
+++ lib/class/media.interface.php
@@ -0,0 +1,35 @@
+<?php
+/*
+
+ Copyright (c) Ampache.org
+ All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License v2
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+/**
+ * media interface
+ * This defines how the media file classes should
+ * work, this lists all required functions and the expected
+ * input
+ */
+interface media { 
+
+	public function format(); 
+	public function native_stream(); 
+	public function play_url($oid,$sid='',$force_http=''); 
+
+} // end interface
+
+?>
