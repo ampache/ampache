700ca3c9db4af7930fa6ecda7cc0546ac1ed24e0
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2009-07-09 11:08:38 +0000

    Fix user stats page so that it looks sane and works as advertised Closes #493

--- templates/show_user.inc.php
+++ templates/show_user.inc.php
@@ -45,31 +45,19 @@ $client->format();
 			$tmp_playlist = new tmpPlaylist(tmpPlaylist::get_from_userid($client->id)); 
 			$object_ids = $tmp_playlist->get_items(); 
 			foreach ($object_ids as $object_data) { 
-				if ($object_data['1'] == 'radio' || $object_data['1'] == 'song') { 
-					$object = new $object_data['1']($object_data['0']);
-					$object->format(); 
-				} 
-				else { 
-					$object = new Random(); 
-					$object->f_link = Random::get_type_name($object_data['1']); 
-				} 
+				$type = array_shift($object_data); 
+				$object = new $type(array_shift($object_data)); 
+				$object->format(); 
 		?>
 		<?php echo $object->f_link; ?><br />
 		<?php } ?>
 		</div>
 	</td>
 </tr>
-<tr>
-	<td valign="top">
-		&nbsp;
-	</td>
-	<td>
-	<?php 
-		echo "<h2>" . _('Recently Played') . "</h2>\n"; 
-		$data = Song::get_recently_played($client->id); 
-		require Config::get('prefix') . '/templates/show_recently_played.inc.php'; 
-	?>
-	</td>
-</tr>
 </table>
 <?php show_box_bottom(); ?>
+<?php 
+	$data = Song::get_recently_played($client->id); 
+	require Config::get('prefix') . '/templates/show_recently_played.inc.php'; 
+?>
+
