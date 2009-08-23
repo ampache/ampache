fad4e448eb4503fb2ff68683f14fb374123cc0b3
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2009-08-23 15:06:31 +0000

    sync from trunk for copyright headers

--- templates/sidebar_preferences.inc.php
+++ templates/sidebar_preferences.inc.php
@@ -1,4 +1,24 @@
 <?php 
+/*
+
+ Copyright (c) Ampache.org
+ All Rights Reserved
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License v2
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
 /* This one is a little dynamic as we add plugins or localplay modules
  * they can have their own preference sections so we need to build the
  * links based on that, always ignore 'internal' though
