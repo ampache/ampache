fca458781155b497a796474608aed593daaf9fd1
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2009-09-14 13:54:42 +0000

    Add Penguin theme as a new stock theme

---
+++ themes/penguin/theme.cfg.php
@@ -0,0 +1,53 @@
+;;;;;;;;;;;;;;;;;;
+;<?php exit(); ?>;
+;;;;;;;;;;;;;;;;;;
+; Copyright (c) 2001 - 2007 Ampache.org
+; All rights reserved.
+;
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License v2
+; as published by the Free Software Foundation.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Penguin Ampache Theme 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Theme Name
+; This is the actual name of the theme that
+; will be displayed in the preferences screen
+; DEFAULT: ampache-theme
+name = "Penguin"
+
+; Theme Author
+; This is just a way of giving credit to the 
+; person who actually created this theme
+; DEFAULT: N/A
+author = "Jeroen Doppenberg"
+
+; Theme Maintainer
+; This is just a way of listing who is responsible for
+; maintaining this theme incase it's not working right
+; please include an e-mail address so you can be contacted
+; DEFAULT: N/A
+maintainer = "info@dopdop.nl"
+
+; Orientation
+; This was added as of 3.3.2-Alpha4, this tells Ampache if this theme
+; uses vertical or horizontal orientation of the menu, if this is a horizontal
+; theme then it will not show the quick search and quick random play forms
+orientation = "vertical"
+
+; Submenu
+; If this is set to simple the sub menu's will only be shown when you're on one of the
+; respective pages. If you want to make the menu's something like the classic theme
+; comment this out
+;submenu = "simple"
