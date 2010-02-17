f1e0007860fc2decdbb47fb07f0f8ab3e5431872
Karl 'vollmerk' Vollmer <vollmer@ampache.org>
2010-02-17 04:37:14 +0000

    Add Art class, empty but will be used for album/artist/movie art

---
+++ lib/class/art.class.php
@@ -0,0 +1,42 @@
+<?php
+/*
+
+ Copyright (c) Ampache.org
+ All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License v2
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+/**
+ * Art
+ * This class handles the images / artwork in ampache
+ * This was initially in the album class, but was pulled out 
+ * to be more general, and apply to albums, artists, movies etc
+ */
+class Art extends database_object { 
+
+
+	/**
+	 * Constructor
+	 * Art constructor, not sure what's here for now
+	 */
+	public function __construct() { 
+
+
+
+	} // constructor
+
+
+} // Art
