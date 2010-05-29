cceb1a4a9b41daa3f7e582839a09b0e48406c715
Paul 'flowerysong' Arthur <flowerysong00@yahoo.com>
2010-05-29 16:59:55 +0000

    Minor output change.  FS#96, reported by Nate Gotz.

--- lib/class/update.class.php
+++ lib/class/update.class.php
@@ -372,7 +372,7 @@ class Update {
 
 		echo "</ul>\n";
 
-		if (!$updated) { echo "<p align=\"center\">No Updates Needed [<a href=\"" . Config::get('web_path') . "\">Return]</a></p>"; }
+		if (!$updated) { echo "<p align=\"center\">No Updates Needed [<a href=\"" . Config::get('web_path') . "\">Return</a>]</p>"; }
 	} // display_update
 
 	/**
