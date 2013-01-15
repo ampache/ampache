a1594882c732a0883fd54a52158b026d35ab8cdc
Paul Arthur <flowerysong00@yahoo.com>
2013-01-15 00:54:19 -0500

    Add Stream_URL class for passing them around

---
+++ lib/class/memory_object.class.php
@@ -0,0 +1,55 @@
+<?php
+/* vim:set tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab: */
+/**
+ *
+ * LICENSE: GNU General Public License, version 2 (GPLv2)
+ * Copyright (c) 2001 - 2011 Ampache.org All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+// A magical class filled with ponies
+
+class memory_object {
+
+	private $_data = array();
+	public $properties;
+
+	public function __construct($data) {
+
+		foreach ($data as $key => $value) {
+			if (in_array($key, $this->properties)) {
+				$this->_data[$key] = $value;
+			}
+		}
+
+	}
+
+	public function __set($name, $value) {
+		if (!in_array($name, $this->properties)) {
+			return false;
+		}
+		$this->_data[$name] = $value;
+	}
+
+	public function __get($name) {
+		if (!in_array($name, $this->properties)) {
+			return false;
+		}
+
+		return isset($this->_data[$name]) ? $this->_data[$name] : null;
+	}
+}
---
+++ lib/class/stream_url.class.php
@@ -0,0 +1,30 @@
+<?php
+/* vim:set tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab: */
+/**
+ *
+ * LICENSE: GNU General Public License, version 2 (GPLv2)
+ * Copyright (c) 2001 - 2011 Ampache.org All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+// A class for passing around an URL and associated data
+
+class Stream_URL extends memory_object {
+
+	public $properties = array('url', 'title', 'author', 'time', 'info_url', 'image_url', 'album', 'type');
+
+}
