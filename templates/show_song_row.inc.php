72a149ab4a688a978a34a41fb1bdbf61f9adf980
John Moore <jcwmoore@gmail.com>
2013-11-19 20:23:35 -0600

    replaced dialog with slide out

--- templates/show_song_row.inc.php
+++ templates/show_song_row.inc.php
@@ -32,7 +32,13 @@
 <td class="cel_artist"><?php echo $song->f_artist_link; ?></td>
 <td class="cel_album"><?php echo $song->f_album_link; ?></td>
 <td class="cel_tags">
-  <a href="#" id="tag_song_<?php echo $song->id?>" onclick="showAddTagDialog(<?php echo $song->id?>, 'song', '<?php echo Config::get('web_path'); ?>')"><?php echo UI::get_icon('add', T_('Add')); ?></a><?php echo $song->f_tags; ?>
+  <span id="tag_song_<?php echo $song->id?>" onclick="showAddTagSlideout('#dialog_tag_item_<?php echo $song->id?>')"><?php echo UI::get_icon('add', T_('Add')); ?></span><?php echo $song->f_tags; ?>
+  <div id="dialog_tag_item_<?php echo $song->id?>" style="display:none;" class="np_cell cel_artist">
+      <div><?php echo T_('Enter tag:')?></div>
+      <div><input type="text" id="dialog_tag_item_tag_name_<?php echo $song->id?>"/></div>
+      <div><input type="button" value="<?php echo T_('Save')?>" onclick="saveTag(<?php echo $song->id?>, 'song', '<?php echo Config::get('web_path'); ?>')" /></div>
+      <div><input type="button" value="<?php echo T_('Close')?>" onclick="closeAddTagSlideout('#dialog_tag_item_<?php echo $song->id?>')" /> </div>
+  </div>
 </td>
 <td class="cel_track"><?php echo $song->f_track; ?></td>
 <td class="cel_time"><?php echo $song->f_time; ?></td>
