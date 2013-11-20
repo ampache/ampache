ead36d91bd9344bca3fd93e3163b571b9067acd9
John Moore <jcwmoore@gmail.com>
2013-11-20 02:10:14 +0000

    allowing the browser to cache album art

--- templates/show_now_playing_row.inc.php
+++ templates/show_now_playing_row.inc.php
@@ -83,7 +83,7 @@
 <div class="np_group" id="np_group_3">
   <div class="np_cell cel_albumart">
       <a href="<?php echo $web_path; ?>/image.php?id=<?php echo $media->album; ?>" onclick="TINY.box.show({image:'<?php echo $web_path; ?>/image.php?id=<?php echo $media->album; ?>',boxid:'frameless',animate:true}); return false;">
-        <img align="middle" src="<?php echo $web_path; ?>/image.php?id=<?php echo $media->album; ?>&amp;thumb=1&amp;sid=<?php echo session_id(); ?>" alt="<?php echo scrub_out($media->f_album_full); ?>" title="<?php echo scrub_out($media->f_album_full); ?>" height="80" width="80" />
+        <img align="middle" src="<?php echo $web_path; ?>/image.php?id=<?php echo $media->album; ?>&amp;thumb=1" alt="<?php echo scrub_out($media->f_album_full); ?>" title="<?php echo scrub_out($media->f_album_full); ?>" height="80" width="80" />
       </a>
   </div>
 </div>
