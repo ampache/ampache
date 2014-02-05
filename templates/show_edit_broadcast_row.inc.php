29c655d76116dff1f74c6cf40d6d6fe9090b5619
Afterster <afterster@gmail.com>
2014-02-05 13:45:17 +0100

    Add broadcast row edit

---
+++ templates/show_edit_broadcast_row.inc.php
@@ -0,0 +1,46 @@
+<?php
+/* vim:set softtabstop=4 shiftwidth=4 expandtab: */
+/**
+ *
+ * LICENSE: GNU General Public License, version 2 (GPLv2)
+ * Copyright 2001 - 2013 Ampache.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+?>
+<div>
+    <form method="post" id="edit_broadcast_<?php echo $broadcast->id; ?>" class="edit_dialog_content">
+        <table class="tabledata" cellspacing="0" cellpadding="0">
+            <tr>
+                <td class="edit_dialog_content_header"><?php echo T_('Name') ?></td>
+                <td><input type="text" name="name" value="<?php echo scrub_out($broadcast->name); ?>" /></td>
+            </tr>
+            <tr>
+                <td class="edit_dialog_content_header"><?php echo T_('Description') ?></td>
+                <td><input type="text" name="description" value="<?php echo scrub_out($broadcast->description); ?>" /></td>
+            </tr>
+            <tr>
+                <td class="edit_dialog_content_header"></td>
+                <td><input type="checkbox" name="private" value="1" <?php echo ($broadcast->is_private) ? 'checked' : ''; ?> /> <?php echo T_('Authentication Required') ?></td>
+            </tr>
+            <tr>
+                <td class="edit_dialog_content_header"><?php echo T_('Genre') ?></td>
+                <td><input type="text" name="edit_tags" id="edit_tags" value="<?php echo Tag::get_display($broadcast->tags); ?>" /></td>
+            </tr>
+        </table>
+        <input type="hidden" name="id" value="<?php echo $broadcast->id; ?>" />
+        <input type="hidden" name="type" value="broadcast_row" />
+    </form>
+</div>
