0b971ab214a19014258eeed1952c4ca647d84274
SUTJael <arnaud.humilier@laposte.net>
2014-05-06 19:57:22 +0200

    Fix #167.

--- templates/show_tagcloud.inc.php
+++ templates/show_tagcloud.inc.php
@@ -23,22 +23,24 @@
 ?>
 <?php Ajax::start_container('tag_filter'); ?>
 <?php foreach ($object_ids as $data) { ?>
-    <div class="tag_button">
-        <span id="click_tag_<?php echo $data['id']; ?>"><?php echo $data['name']; ?></span>
-        <?php echo Ajax::observe('click_tag_' . $data['id'], 'click', Ajax::action('?page=tag&action=add_filter&browse_id=' . $browse2->id . '&tag_id=' . $data['id'], '')); ?>
-    </div>
-    <?php if (Access::check('interface', '50')) { ?>
-    <div class="tag_actions">
-        <ul>
-            <li>
-                <a class="tag_edit" id="<?php echo 'edit_tag_'.$data['id'] ?>" onclick="showEditDialog('tag_row', '<?php echo $data['id'] ?>', '<?php echo 'edit_tag_'.$data['id'] ?>', '<?php echo T_('Tag edit') ?>', 'click_tag_', 'refresh_tag')">
-                    <?php echo UI::get_icon('edit', T_('Edit')); ?>
-                </a>
-            </li>
-            <li>
-                <a class="tag_delete" href="<?php echo AmpConfig::get('ajax_url') ?>?page=tag&action=delete&tag_id=<?php echo $data['id']; ?>" onclick="return confirm('<?php echo T_('Do you really want to delete the tag?'); ?>');"><?php echo UI::get_icon('delete', T_('Delete')); ?></a>
-            </li>
-        </ul>
+    <div class="tag_container">
+        <div class="tag_button">
+            <span id="click_tag_<?php echo $data['id']; ?>"><?php echo $data['name']; ?></span>
+            <?php echo Ajax::observe('click_tag_' . $data['id'], 'click', Ajax::action('?page=tag&action=add_filter&browse_id=' . $browse2->id . '&tag_id=' . $data['id'], '')); ?>
+        </div>
+        <?php if (Access::check('interface', '50')) { ?>
+        <div class="tag_actions">
+            <ul>
+                <li>
+                    <a class="tag_edit" id="<?php echo 'edit_tag_'.$data['id'] ?>" onclick="showEditDialog('tag_row', '<?php echo $data['id'] ?>', '<?php echo 'edit_tag_'.$data['id'] ?>', '<?php echo T_('Tag edit') ?>', 'click_tag_', 'refresh_tag')">
+                        <?php echo UI::get_icon('edit', T_('Edit')); ?>
+                    </a>
+                </li>
+                <li>
+                    <a class="tag_delete" href="<?php echo AmpConfig::get('ajax_url') ?>?page=tag&action=delete&tag_id=<?php echo $data['id']; ?>" onclick="return confirm('<?php echo T_('Do you really want to delete the tag?'); ?>');"><?php echo UI::get_icon('delete', T_('Delete')); ?></a>
+                </li>
+            </ul>
+        </div>
     </div>
     <?php } ?>
 <?php } ?>
