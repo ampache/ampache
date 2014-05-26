41c533780fc2909ec6f6e1197d20618c97476eb4
Afterster <afterster@gmail.com>
2014-05-26 13:18:29 +0200

    Update develop version to 3.7.1-develop

--- lib/init.php
+++ lib/init.php
@@ -65,7 +65,7 @@ if (!empty($link)) {
 
 $results['load_time_begin'] = $load_time_begin;
 /** This is the version.... fluf nothing more... **/
-$results['version']        = '3.7.0';
+$results['version']        = '3.7.1-develop';
 $results['int_config_version']    = '16';
 
 if (!empty($results['force_ssl'])) {
