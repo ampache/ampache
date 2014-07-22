b3f713db1cad581074ebf328673396a7c9c6d6c2
Afterster <afterster@gmail.com>
2014-07-22 06:07:00 +0200

    Fix SubSonic and local https (fix #342)

--- lib/init.php
+++ lib/init.php
@@ -83,7 +83,7 @@ if (empty($results['http_host'])) {
     $results['http_host'] = $_SERVER['HTTP_HOST'];
 }
 if (empty($results['local_web_path'])) {
-    $results['local_web_path'] = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $results['raw_web_path'];
+    $results['local_web_path'] = $http_type . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $results['raw_web_path'];
 }
 $results['web_path'] = $http_type . $results['http_host'] . $results['web_path'];
 $results['http_port'] = (!empty($results['http_port'])) ? $results['http_port'] : $http_port;
