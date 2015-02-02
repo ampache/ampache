6c01c9aa2ed0d9e1f632cbe783cc2ae04c54c9ec
Nick Lanham <nick@afternight.org>
2015-02-02 23:55:02 +0000

    Set http status code from the curl request
    Need to pass through the http status code from the underlying request in order to properly answer with a 206 when skipping bytes.
    Set when we see the HTTP/... status line header.

--- lib/class/subsonic_api.class.php
+++ lib/class/subsonic_api.class.php
@@ -91,6 +91,11 @@ class Subsonic_Api
             if ($rhpart[0] != "Transfer-Encoding") {
                 header($rheader);
             }
+        } else {
+            if (substr($header, 0, 5) === "HTTP/") {
+                // if $header starts with HTTP/ assume it's the status line
+                http_response_code(curl_getinfo($ch,CURLINFO_HTTP_CODE));
+            }
         }
         return strlen($header);
     }
