e6a5d29cd0bc9278cccd6fbc530b24edeaf2d208
Afterster <afterster@gmail.com>
2015-02-02 06:39:32 +0100

    Fixed #705: Web player UI gets messed up when removing the footer statistics

--- templates/footer.inc.php
+++ templates/footer.inc.php
@@ -39,16 +39,16 @@
             <a id="donate" href="//ampache.github.io/donate.html" title="Donate" target="_blank"><?php echo ".:: " . T_('Donate') . " ::."; ?></a> |
         <?php } ?>
             <a id="ampache_link" href="https://github.com/ampache/ampache#readme" target="_blank" title="Copyright © 2001 - 2015 Ampache.org">Ampache <?php echo AmpConfig::get('version'); ?></a>
-            <?php if (AmpConfig::get('show_footer_statistics')) { ?>
+        <?php if (AmpConfig::get('show_footer_statistics')) { ?>
             <br />
             <?php echo T_('Queries:'); ?><?php echo Dba::$stats['query']; ?> <?php echo T_('Cache Hits:'); ?><?php echo database_object::$cache_hit; ?>
-        <?php
-            $load_time_end = microtime(true);
-            $load_time = number_format(($load_time_end - AmpConfig::get('load_time_begin')), 4);
-        ?>
+            <?php
+                $load_time_end = microtime(true);
+                $load_time = number_format(($load_time_end - AmpConfig::get('load_time_begin')), 4);
+            ?>
             | <?php echo T_('Load time:'); ?><?php echo $load_time; ?>
-        </div>
         <?php } ?>
+        </div>
         <?php if (AmpConfig::get('ajax_load') && (!isset($_SESSION['login']) || !$_SESSION['login'])) { ?>
         <div id="webplayer"></div>
         <?php
