35477d0a5ef273bda71107be15f681619243f2bc
Niols <niols@niols.net>
2015-06-06 18:53:38 +0200

    Add UI::find_template method and its allow_php_themes argument

--- lib/class/ui.class.php
+++ lib/class/ui.class.php
@@ -35,6 +35,25 @@ class UI
     }
 
     /**
+     * find_template
+     *
+     * Return the path to the template file wanted. The file can be overwriten
+     * by the theme if it's not a php file, or if it is and if option
+     * allow_php_themes is set to true.
+     */
+    public static function find_template($template)
+    {
+        $path = AmpConfig::get('theme_path') . '/templates/' . $template;
+        $realpath = AmpConfig::get('prefix') . $path;
+        $extension = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+        if(($extension != 'php' || AmpConfig::get('allow_php_themes'))
+           && file_exists($realpath) && is_file($realpath))
+            return $path;
+        else
+            return '/templates/' . $template;
+    }
+
+    /**
      * access_denied
      *
      * Throw an error when they try to do something naughty.
@@ -44,7 +63,7 @@ class UI
         // Clear any buffered crap
         ob_end_clean();
         header("HTTP/1.1 403 $error");
-        require_once AmpConfig::get('prefix') . '/templates/show_denied.inc.php';
+        require_once AmpConfig::get('prefix') . UI::find_template('show_denied.inc.php');
         exit;
     }
 
@@ -57,7 +76,7 @@ class UI
     public static function ajax_include($template)
     {
         ob_start();
-        require AmpConfig::get('prefix') . '/templates/' . $template;
+        require AmpConfig::get('prefix') . UI::find_template('') . $template;
         $output = ob_get_contents();
         ob_end_clean();
 
@@ -274,7 +293,7 @@ END;
      */
     public static function show_header()
     {
-        require_once AmpConfig::get('prefix') . '/templates/header.inc.php';
+        require_once AmpConfig::get('prefix') . UI::find_template('header.inc.php');
     }
 
     /**
@@ -296,7 +315,7 @@ END;
             }
         }
 
-        require_once AmpConfig::get('prefix') . '/templates/footer.inc.php';
+        require_once AmpConfig::get('prefix') . UI::find_template('footer.inc.php');
         if (isset($_REQUEST['profiling'])) {
             Dba::show_profile();
         }
@@ -309,7 +328,7 @@ END;
      */
     public static function show_box_top($title = '', $class = '')
     {
-        require AmpConfig::get('prefix') . '/templates/show_box_top.inc.php';
+        require AmpConfig::get('prefix') . UI::find_template('show_box_top.inc.php');
     }
 
     /**
@@ -319,7 +338,7 @@ END;
      */
     public static function show_box_bottom()
     {
-        require AmpConfig::get('prefix') . '/templates/show_box_bottom.inc.php';
+        require AmpConfig::get('prefix') . UI::find_template('show_box_bottom.inc.php');
     }
 
     public static function show_custom_style()
