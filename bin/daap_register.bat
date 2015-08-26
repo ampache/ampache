afac314ad1437808a5240c8897271555049c93d2
Afterster <afterster@gmail.com>
2015-08-26 18:34:33 +0200

    Fix iTunes 12 browsing Add bin/daap_register.bat script

---
+++ bin/daap_register.bat
@@ -0,0 +1,5 @@
+start dns-sd -R Ampache _http._tcp local 3689
+start dns-sd -R Ampache _daap._tcp local 3689 txtvers=1 "Machine ID"=AC630E6FB504 "iTSh Version"=196619 "Media Kinds Shared"=1 dmv=131082 Version=196620 "Machine Name"=Ampache Password=0 OSsi=0x10313 "Database ID"=416D7061636865 MID=0xAD12C5B9B9D839F2
+start dns-sd -R Ampache _dacp._tcp local 3689
+start dns-sd -R Ampache _touch-able._tcp local 3689 txtvers=1 OSsi=0x10313 CtlN=Ampache Ver=131073 DvSv=2305 DvTy=iTunes DbId=416D7061636865
+start dns-sd -R Ampache _rsp._tcp local 3689
