10e14fc59079bb6e4c4453e39371e8fbbda69090
Afterster <afterster@gmail.com>
2015-08-27 11:05:05 +0200

    Merged mbstring.func_overload warning to one unique line

--- templates/show_test_table.inc.php
+++ templates/show_test_table.inc.php
@@ -170,13 +170,7 @@
     <?php echo debug_result(check_mbstring_func_overload()); ?>
     </td>
     <td>
-    <?php printf(T_('This tests whether PHP %s is set as it may break the ID3 tag support.'), '<a href="http://php.net/manual/en/mbstring.overload.php">mbstring.func_overload</a>'); ?>
-    <?php
-    if (!check_mbstring_func_overload()) {
-        echo "<br />";
-        echo T_('Enabling Ampache ID3 tag write support along with mbstring.func_overload may result in irreversible corruption of your music files.\n\nYOU HAVE BEEN WARNED!');
-    }
-    ?>
+    <?php printf(T_('This tests whether PHP %s is set as it may break the ID3 tag support. This is not stricly necessary, but enabling Ampache ID3 tag write support (disabled by default) along with mbstring.func_overload may result in irreversible corruption of your music files.'), '<a href="http://php.net/manual/en/mbstring.overload.php">mbstring.func_overload</a>'); ?>
     </td>
 </tr>
 <?php
