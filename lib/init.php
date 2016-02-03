cfce5049abb08ab33d335af3c90e4c7bab5c2f8d
Afterster <afterster@gmail.com>
2016-02-03 22:02:23 +0100

    Update version to 3.8.3-develop

--- lib/init.php
+++ lib/init.php
@@ -67,7 +67,7 @@ if (!empty($link)) {
 
 $results['load_time_begin'] = $load_time_begin;
 /** This is the version.... fluf nothing more... **/
-$results['version']            = '3.8.2';
+$results['version']            = '3.8.3-develop';
 $results['int_config_version'] = '33';
 
 if (!empty($results['force_ssl'])) {
