b58b7869aa33f5b107fda38a7984cbf996379a73
Maniac <maniac@case-of.org>
2016-02-12 09:26:04 +0100

    Show artist name as well as album name when hovering images

--- lib/class/art.class.php
+++ lib/class/art.class.php
@@ -1692,7 +1692,7 @@ class Art extends database_object
      */
     public static function display_item($item, $thumb, $link = null)
     {
-        return self::display($item->type ?: strtolower(get_class($item)), $item->id, $item->get_fullname(), $thumb, $link);
+        return self::display($item->type ?: strtolower(get_class($item)), $item->id, "[" . $item->f_artist . "] " . $item->get_fullname(), $thumb, $link);
     }
 
     /**
