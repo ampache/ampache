5e7affb21a48d935e0d22947a0f308bf446e4017
Joel Klinghed <the_jk@yahoo.com>
2016-03-26 18:01:44 +0100

    Make song count in handshake match the number returned from songs
    Songs skip disabled songs so the handshake count should do the same
    Also makes the counts for artists and albums not depend on included
    song rows but on the number of rows in their own tables

--- lib/class/api.class.php
+++ lib/class/api.class.php
@@ -226,12 +226,17 @@ class Api
                 $row        = Dba::fetch_assoc($db_results);
 
                 // Now we need to quickly get the song totals
-                $sql = 'SELECT COUNT(`id`) AS `song`, ' .
-                    'COUNT(DISTINCT(`album`)) AS `album`, ' .
-                    'COUNT(DISTINCT(`artist`)) AS `artist` ' .
-                    'FROM `song`';
+                $sql = "SELECT COUNT(`id`) AS `song` FROM `song` WHERE `song`.`enabled`='1'";
                 $db_results = Dba::read($sql);
-                $counts     = Dba::fetch_assoc($db_results);
+                $song       = Dba::fetch_assoc($db_results);
+
+                $sql = "SELECT COUNT(`id`) AS `album` FROM `album`";
+                $db_results = Dba::read($sql);
+                $album     = Dba::fetch_assoc($db_results);
+
+                $sql = "SELECT COUNT(`id`) AS `artist` FROM `artist`";
+                $db_results = Dba::read($sql);
+                $artist     = Dba::fetch_assoc($db_results);
 
                 // Next the video counts
                 $sql        = "SELECT COUNT(`id`) AS `video` FROM `video`";
@@ -252,9 +257,9 @@ class Api
                     'update'=>date("c",$row['update']),
                     'add'=>date("c",$row['add']),
                     'clean'=>date("c",$row['clean']),
-                    'songs'=>$counts['song'],
-                    'albums'=>$counts['album'],
-                    'artists'=>$counts['artist'],
+                    'songs'=>$song['song'],
+                    'albums'=>$album['album'],
+                    'artists'=>$artist['artist'],
                     'playlists'=>$playlist['playlist'],
                     'videos'=>$vcounts['video'],
                     'catalogs'=>$catalog['catalog']));
