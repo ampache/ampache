14ac1f2a9e4d8e3849e0d724dcb7a2b2792458fb
manuelglez86 <manuel.gonzalez@correo.uady.mx>
2017-06-01 19:45:37 -0500

    Update api.class.php
    The XML api doent have an option to modify the name and type of playlist already created.
    
    parameters:
        name (Playlist name)
        type (Playlist type [public|private])

--- lib/class/api.class.php
+++ lib/class/api.class.php
@@ -630,6 +630,32 @@ class Api
     }
 
     /**
+	 * playlist_edit
+	 * This modifies name and type of playlist
+	 */
+	
+	public static function playlist_edit($input)
+	{
+		$filter = $input['filter']
+		$name = $input['name'];
+        $type = $input['type'];
+		ob_end_clean();
+		$playlist = new Playlist($input['filter']);
+		
+		if (!$playlist->has_access()) {
+            echo XML_Data::error('401', T_('Access denied to this playlist.'));
+        } else {
+            $array = [
+			"name" => $name,
+			"pl_type" => $type,
+			]
+			$playlist->update($array);
+			echo XML_Data::single_string('success');
+        }
+		
+		
+	}
+    /**
      * playlist_delete
      * This delete a playlist
      * @param array $input
