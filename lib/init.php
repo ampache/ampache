b75d93abe05d36282028081568cdb108397d21f8
Ernie D <wagnered@comcast.net>
2018-10-20 22:15:56 -0700

    Corrected release version.

--- lib/init.php
+++ lib/init.php
@@ -68,7 +68,7 @@ if (!empty($link)) {
 $results['load_time_begin'] = $load_time_begin;
 /** This is the version.... fluf nothing more... **/
 
-$results['version']            = '3.8.9-release';
+$results['version']            = '3.9.0-release';
 $results['int_config_version'] = '34';
 
 if (!empty($results['force_ssl'])) {
