66e22241def96449321976012596355c9f92f481
Ernie D <wagnered@comcast.net>
2018-12-04 10:23:02 -0800

    Modified max index length for innodb tables.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use App\Classes\Catalog;
 use App\Models\User;
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         if (!isset($_COOKIE['sidebar_tab'])) {
             setcookie('sidebar_tab', 'home', time() + (30 * 24 * 60 * 60), '/');
         }
@@ -27,6 +30,7 @@ class AppServiceProvider extends ServiceProvider
         if (!isset($_COOKIE['sidebar_state'])) {
             setcookie('sidebar_state', 'expanded', time() + (30 * 24 * 60 * 60), '/');
         }
+        $sb_homeItems                    = array();
         $sb_homeItems['sb_music']        = isset($_COOKIE['sb_music']) ? : 'collapsed';
         $sb_homeItems['sb_video']        = isset($_COOKIE['sb_video']) ? : 'collapsed';
         $sb_homeItems['sb_information']  = isset($_COOKIE['sb_information']) ? : 'collapsed';
