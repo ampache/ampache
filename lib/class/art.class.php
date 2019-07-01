6de2e01e5039d7d871715b5098eeb94d428a2ede
lachlan-00 <lachlan.00@gmail.com>
2019-07-01 21:40:56 +1000

    Web player and db updates

--- lib/class/art.class.php
+++ lib/class/art.class.php
@@ -1749,7 +1749,7 @@ class Art extends database_object
                 $size['width']     = 80;
             break;
             case 5:
-                /* Web Player size */
+                /* Web player size */
                 $size['height'] = 32;
                 $size['width']  = 32;
             break;
@@ -1779,7 +1779,7 @@ class Art extends database_object
                  $size['width']  = 24;
             break;
             case 4:
-                /* Popup Web Player size */
+                /* Popup Web player size */
             case 11:
                 /* Large view browse size */
             case 12:
