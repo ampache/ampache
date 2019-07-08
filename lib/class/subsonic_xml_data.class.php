545f0a22b2def90796d9fa890b42eb3c50358216
lachlan <lachlan.00@gmail.com>
2019-07-08 14:43:17 +1000

    Update subsonic_xml_data.class.php

--- lib/class/subsonic_xml_data.class.php
+++ lib/class/subsonic_xml_data.class.php
@@ -202,7 +202,7 @@ class Subsonic_XML_Data
     {
         $response = self::createResponse($version);
         $response->addAttribute('status', 'failed');
-        debug_event('subsonic_xml_data.class', 'API auth fail ' . $version, 3);
+        debug_event('subsonic_xml_data.class', 'API auth fail', 3);
 
         return $response;
     }
@@ -210,7 +210,7 @@ class Subsonic_XML_Data
     public static function createSuccessResponse($version = '')
     {
         $response = self::createResponse($version);
-        debug_event('subsonic_xml_data.class', 'API auth success: ' . $version, 5);
+        debug_event('subsonic_xml_data.class', 'API auth success', 5);
 
         return $response;
     }
