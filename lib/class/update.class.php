014a6fe8ac8f99de32b532f7a48e3af049552363
lachlan-00 <lachlan.00@gmail.com>
2019-08-14 19:26:04 +1000

    Update update.class.php

--- lib/class/update.class.php
+++ lib/class/update.class.php
@@ -572,12 +572,14 @@ class Update
                          "  This is a cosmetic update and does not affect any operation)<br />";
         $version[]     = array('version' => '400001', 'description' => $update_string);
 
-        $update_string = "IMPORTANT UPDATE NOTES" .
-                         "* Update album disk support to allow 1 instead of 0 by default.<br />" .
-                         "* Add barcode catalog_number and original_year to albums.<br />" .
-                         "* Drop catalog_number from song_data and use album instead.<br />";
+        $update_string = "**IMPORTANT UPDATE NOTES**<br />" .
+                         "This is part of a major update to how Ampache handles Albums,<br />" .
+                         "Artists and data migration during tag updates.<br />" .
+                         " * Update album disk support to allow 1 instead of 0 by default.<br />" .
+                         " * Add barcode catalog_number and original_year to albums.<br />" .
+                         " * Drop catalog_number from song_data and use album instead.<br />";
         $version[]     = array('version' => '400002', 'description' => $update_string);
-        
+
         return $version;
     }
 
