bfebc20fe3ac2dc12b0f879b467a46390caf14a9
lachlan <lachlan.00@gmail.com>
2019-08-14 10:10:21 +1000

    Add Rating::migrate

--- lib/class/artist.class.php
+++ lib/class/artist.class.php
@@ -968,6 +968,7 @@ class Artist extends database_object implements library_item
                 $current_id = $artist_id;
                 Stats::migrate('artist', $this->id, $artist_id);
                 UserActivity::migrate('artist', $this->id, $artist_id);
+                Rating::migrate('artist', $this->id, $artist_id);
                 Art::migrate('artist', $this->id, $artist_id);
                 self::garbage_collection();
             } // end if it changed
