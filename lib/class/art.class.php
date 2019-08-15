94515ce06a3854252bd4e75f34ceb58ab2f886a4
lachlan <lachlan.00@gmail.com>
2019-08-15 22:59:42 +1000

    only fail php-gd check once (#2013)

--- lib/class/art.class.php
+++ lib/class/art.class.php
@@ -206,8 +206,7 @@ class Art extends database_object
             return false;
         }
 
-        $test  = false;
-        $image = false;
+        $test = false;
         // Check to make sure PHP:GD exists.  If so, we can sanity check the image.
         if (function_exists('ImageCreateFromString') && is_string($source)) {
             $test  = true;
