9e86afe881d96a2295b2e7052b93917384327b51
lachlan <lachlan.00@gmail.com>
2019-08-23 09:43:31 +1000

    more debug for errors in migration

--- lib/class/catalog.class.php
+++ lib/class/catalog.class.php
@@ -1723,7 +1723,6 @@ abstract class Catalog extends database_object
         if (self::migrate('artist', $song->artist, $new_song->artist) ||
                 self::migrate('artist', $song->albumartist, $new_song->albumartist) ||
                 self::migrate('album', $song->album, $new_song->album)) {
-            debug_event('catalog.class', 'Set update_time for ' . $song->id . ' due to artist/album changes.', 5);
             Song::update_utime($song->id, $update_time);
         }
         $new_song->title = self::check_title($new_song->title, $new_song->file);
@@ -2630,12 +2629,22 @@ abstract class Catalog extends database_object
     public static function migrate($object_type, $old_object_id, $new_object_id)
     {
         if ($old_object_id != $new_object_id) {
-            debug_event('catalog.class', 'migrate ' . $object_type . ' from ' .  $old_object_id . ' to ' . $new_object_id, 4);
-            Stats::migrate($object_type, $old_object_id, $new_object_id);
-            UserActivity::migrate($object_type, $old_object_id, $new_object_id);
-            Userflag::migrate($object_type, $old_object_id, $new_object_id);
-            Rating::migrate($object_type, $old_object_id, $new_object_id);
-            Art::migrate($object_type, $old_object_id, $new_object_id);
+            debug_event('catalog.class', 'migrate ' . $object_type . ' from ' . $old_object_id . ' to ' . $new_object_id, 4);
+            if (!Stats::migrate($object_type, $old_object_id, $new_object_id)) {
+                debug_event('catalog.class', 'migrate ' . $object_type . ' from ' . $old_object_id . ' to ' . $new_object_id . ' stats migration failed!', 2);
+            }
+            if (!UserActivity::migrate($object_type, $old_object_id, $new_object_id)) {
+                debug_event('catalog.class', 'migrate ' . $object_type . ' from ' . $old_object_id . ' to ' . $new_object_id . ' stats migration failed!', 2);
+            }
+            if (!Userflag::migrate($object_type, $old_object_id, $new_object_id)) {
+                debug_event('catalog.class', 'migrate ' . $object_type . ' from ' . $old_object_id . ' to ' . $new_object_id . ' stats migration failed!', 2);
+            }
+            if (!Rating::migrate($object_type, $old_object_id, $new_object_id)) {
+                debug_event('catalog.class', 'migrate ' . $object_type . ' from ' . $old_object_id . ' to ' . $new_object_id . ' stats migration failed!', 2);
+            }
+            if (!Art::migrate($object_type, $old_object_id, $new_object_id)) {
+                debug_event('catalog.class', 'migrate ' . $object_type . ' from ' . $old_object_id . ' to ' . $new_object_id . ' stats migration failed!', 2);
+            }
 
             return true;
         }
