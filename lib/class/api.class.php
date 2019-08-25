566460547c58ac8ac9322ed5352360c3a9bda85e
lachlan-00 <lachlan.00@gmail.com>
2019-08-25 15:56:24 +1000

    Update api.class.php

--- lib/class/api.class.php
+++ lib/class/api.class.php
@@ -383,7 +383,6 @@ class Api
     public static function artist_albums($input)
     {
         $artist = new Artist($input['filter']);
-
         $albums = $artist->get_albums();
 
         // Set the offset
@@ -465,13 +464,29 @@ class Api
     public static function album_songs($input)
     {
         $album = new Album($input['filter']);
-        $songs = $album->get_songs();
+        $songs = array();
 
         // Set the offset
         XML_Data::set_offset($input['offset']);
         XML_Data::set_limit($input['limit']);
 
         ob_end_clean();
+
+        // songs for all disks
+        if (AmpConfig::get('album_group')) {
+            $disc_ids = $album->get_group_disks_ids();
+            foreach ($disc_ids as $discid) {
+                $disc     = new Album($discid);
+                $allsongs = $disc->get_songs();
+                foreach ($allsongs as $songid) {
+                    $songs[] = $songid;
+                }
+            }
+        } else {
+            // songs for just this disk
+            $songs = $album->get_songs();
+        }
+
         echo XML_Data::songs($songs);
     } // album_songs
 
