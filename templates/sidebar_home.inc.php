9c9b83cfd5eda65904f54a558165d2ad4a4f9029
lachlan <lachlan.00@gmail.com>
2019-08-30 14:04:29 +1000

    Update sidebar_home.inc.php

--- templates/sidebar_home.inc.php
+++ templates/sidebar_home.inc.php
@@ -159,6 +159,7 @@
     </li>
     <?php
     } ?>
+    <li>
         <h4 class="header"><span class="sidebar-header-title" title="<?php echo T_('Information'); ?>"><?php echo T_('Information'); ?></span><img src="<?php echo AmpConfig::get('web_path') . AmpConfig::get('theme_path'); ?>/images/icons/icon_all.png" class="header-img <?php echo ($_COOKIE['sb_info'] == 'collapsed') ? 'collapsed' : 'expanded'; ?>" id="information" alt="<?php echo T_('Expand/Collapse'); ?>" title="<?php echo T_('Expand/Collapse'); ?>" /></h4>
         <ul class="sb3" id="sb_home_info">
             <li id="sb_home_info_recent"><a href="<?php echo $web_path; ?>/stats.php?action=recent"><?php echo T_('Recent'); ?></a></li>
