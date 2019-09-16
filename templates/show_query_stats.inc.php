ab88acb42ec03beb79c0cda7b41086e15507166f
lachlan <lachlan.00@gmail.com>
2019-09-16 16:24:53 +1000

    Update show_query_stats.inc.php

--- templates/show_query_stats.inc.php
+++ templates/show_query_stats.inc.php
@@ -24,10 +24,10 @@
     ?>
     <br />
     <span class="item-count">
-    <?php echo T_('Queries: '); ?><?php echo Dba::$stats['query']; ?> <?php echo T_('Cache Hits: '); ?><?php echo database_object::$cache_hit; ?>
-    <?php
-        $load_time_end         = microtime(true);
-    $load_time                 = number_format(($load_time_end - AmpConfig::get('load_time_begin')), 4); ?>
+    <?php echo T_('Queries: '); ?><?php echo Dba::$stats['query']; ?>
+    | <?php echo T_('Cache Hits: '); ?><?php echo database_object::$cache_hit;
+    $load_time_end = microtime(true);
+    $load_time     = number_format(($load_time_end - AmpConfig::get('load_time_begin')), 4); ?>
     | <?php echo T_('Load Time: '); ?><?php echo $load_time; ?>
     </span>
 <?php
