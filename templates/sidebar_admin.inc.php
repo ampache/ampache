735257d4a144789e4ff70a1c60fcf53f3aae14c0
lachlan-00 <lachlan.00@gmail.com>
2019-09-19 19:37:09 +1000

    Manage Plugins
    needs to differentiate from config

--- templates/sidebar_admin.inc.php
+++ templates/sidebar_admin.inc.php
@@ -62,22 +62,14 @@
   </li>
   <?php Ajax::start_container('browse_filters'); ?>
   <?php Ajax::end_container(); ?>
-  <li><h4 class="header"><span class="sidebar-header-title" title="<?php echo T_('Other Tools'); ?>"><?php echo T_('Other Tools'); ?></span><?php echo UI::get_icon('all', T_('Expand/Collapse'), 'ad_other_tools', 'header-img ' . ((filter_has_var(INPUT_COOKIE, 'sb_ad_other_tools')) ? $_COOKIE['sb_ad_other_tools'] : 'expanded')); ?></h4>
-    <ul class="sb3" id="sb_admin_ot">
-      <li id="sb_admin_ot_Debug"><a href="<?php echo $web_path; ?>/admin/system.php?action=show_debug"><?php echo T_('Ampache Debug'); ?></a></li>
-      <li id="sb_admin_ot_ClearNowPlaying"><a href="<?php echo $web_path; ?>/admin/catalog.php?action=clear_now_playing"><?php echo T_('Clear Now Playing'); ?></a></li>
-      <li id="sb_admin_ot_ExportCatalog"><a href="<?php echo $web_path; ?>/admin/export.php"><?php echo T_('Export Catalog'); ?></a></li>
-      <?php if (AmpConfig::get('sociable')) {
-    ?>
-      <li id="sb_admin_ot_ManageShoutbox"><a href="<?php echo $web_path; ?>/admin/shout.php"><?php echo T_('Manage Shoutbox'); ?></a></li>
-      <?php
-} ?>
-      <?php if (AmpConfig::get('licensing')) {
-        ?>
-      <li id="sb_admin_ot_ManageLicense"><a href="<?php echo $web_path; ?>/admin/license.php"><?php echo T_('Manage Licenses'); ?></a></li>
-      <?php
-    } ?>
-    </ul>
+<ul class="sb2" id="sb_modules">
+    <li><h4 class="header"><span class="sidebar-header-title" title="<?php echo T_('Modules'); ?>"><?php echo T_('Modules'); ?></span><img src="<?php echo AmpConfig::get('web_path') . AmpConfig::get('theme_path'); ?>/images/icons/icon_all.png" class="header-img <?php echo (filter_has_var(INPUT_COOKIE, 'sb_modules')) ? $_COOKIE['sb_modules'] : 'expanded'; ?>" id="modules" alt="<?php echo T_('Expand/Collapse'); ?>" title="<?php echo T_('Expand/Collapse'); ?>" /></h4>
+      <ul class="sb3" id="sb_Modules">
+        <li id="sb_preferences_mo_localplay"><a href="<?php echo $web_path; ?>/admin/modules.php?action=show_localplay"><?php echo T_('Localplay Controllers'); ?></a></li>
+        <li id="sb_preferences_mo_catalog_types"><a href="<?php echo $web_path; ?>/admin/modules.php?action=show_catalog_types"><?php echo T_('Catalog Types'); ?></a></li>
+        <li id="sb_preferences_mo_plugins"><a href="<?php echo $web_path; ?>/admin/modules.php?action=show_plugins"><?php echo T_('Manage Plugins'); ?></a></li>
+      </ul>
+    </li>
   </li>
 <?php if (Access::check('interface', '100')) {
         ?>
