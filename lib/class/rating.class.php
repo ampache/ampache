2c2b10840fb3d7fee6a5d9d4730a061fbdeb653b
lachlan <lachlan.00@gmail.com>
2019-09-24 15:32:38 +1000

    sql fix for get_highest_sql
    use subsonic user input to id user

--- lib/class/rating.class.php
+++ lib/class/rating.class.php
@@ -188,18 +188,18 @@ class Rating extends database_object
     public static function get_highest_sql($type)
     {
         $type = Stats::validate_type($type);
-        $sql  = "SELECT `object_id` as `id`, AVG(`rating`) AS `rating` FROM rating";
+        $sql  = "SELECT `object_id` as `id`, AVG(`rating`) AS `rating`, COUNT(`object_id`) AS `count` FROM rating";
         if (AmpConfig::get('album_group') && $type === 'album') {
             $sql .= " LEFT JOIN `album` on `rating`.`object_id` = `album`.`id` and `rating`.`object_type` = 'album'";
         }
-        $sql .= " WHERE object_type = '" . $type . "'";
+        $sql .= " WHERE `object_type` = '" . $type . "'";
         if (AmpConfig::get('catalog_disable')) {
             $sql .= " AND " . Catalog::get_enable_filter($type, '`object_id`');
         }
         if (AmpConfig::get('album_group') && $type === 'album') {
-            "GROUP BY `album`.`name`, `album`.`album_artist`, `album`.`mbid`, `album`.`year` ORDER BY `rating` DESC";
+            $sql .= " GROUP BY `album`.`name`, `album`.`album_artist`, `album`.`mbid`, `album`.`year` ORDER BY `rating` DESC";
         } else {
-            $sql .= " GROUP BY object_id ORDER BY `rating` DESC ";
+            $sql .= " GROUP BY `object_id` ORDER BY `rating` DESC, `count` DESC ";
         }
         //debug_event('rating.class', 'get_highest_sql ' . $sql, 5);
 
@@ -225,7 +225,7 @@ class Rating extends database_object
 
         /* Select Top objects counting by # of rows */
         $sql = self::get_highest_sql($type);
-        $sql .= "LIMIT $limit";
+        $sql .= " LIMIT $limit";
         $db_results = Dba::read($sql, array($type));
 
         $results = array();
