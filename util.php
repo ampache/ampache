88f798b87844417109ec37c7a9551b6b2a2ea082
lachlan-00 <lachlan.00@gmail.com>
2019-10-05 12:32:31 +1000

    Update util.php

--- util.php
+++ util.php
@@ -1,11 +1,9 @@
 <?php
-/* vim:set tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab: */
+/* vim:set softtabstop=4 shiftwidth=4 expandtab: */
 /**
- * Util
- *
  *
  * LICENSE: GNU General Public License, version 2 (GPLv2)
- * Copyright (c) 2001 - 2011 Ampache.org All Rights Reserved
+ * Copyright 2001 - 2013 Ampache.org
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License v2
@@ -20,10 +18,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * @package	Ampache
- * @copyright	2001 - 2011 Ampache.org
- * @license	http://opensource.org/licenses/gpl-2.0 GPLv2
- * @link	http://www.ampache.org/
  */
 
 require_once 'lib/init.php';
@@ -37,12 +31,12 @@ header("Pragma: no-cache");
 // content of $_SESSION['iframe']['target'] and does a header
 // redirect to that spot!
 if (isset($_SESSION['iframe']['target'])) {
-	$target = $_SESSION['iframe']['target'];
-	unset($_SESSION['iframe']['target']);
-	header("Location: " . $target);
+    $target = $_SESSION['iframe']['target'];
+    unset($_SESSION['iframe']['target']);
+    header("Location: " . $target);
 }
 else {
-	// Prevent the update query as it's pointless
-	define('NO_SESSION_UPDATE','1');
+    // Prevent the update query as it's pointless
+    define('NO_SESSION_UPDATE','1');
 }
 ?>
