935ad850f5a8f87d5a1d2e6a3cfbee7d1cbf38e1
lachlan <lachlan.00@gmail.com>
2019-10-18 10:38:22 +1000

    Update autoupdate.class.php

--- lib/class/autoupdate.class.php
+++ lib/class/autoupdate.class.php
@@ -269,12 +269,13 @@ class AutoUpdate
         echo '<span>' . T_('Update available') . '</span>';
         echo ' (' . self::get_latest_version() . ').<br />';
         $git_branch    = self::is_force_git_branch();
-        $develop_check = self::is_develop() || $git_branch == 'develop';
+        $develop_check = self::is_develop() || $git_branch != '';
         $changelog     = ($git_branch == '') ? 'master' : $git_branch;
+        $zip_name      = ($git_branch == '') ? 'develop' : $git_branch;
 
         echo '<a href="https://github.com/ampache/ampache/' . ($develop_check ? 'compare/' . self::get_current_version() . '...' . self::get_latest_version() : 'blob/'. $changelog .'/docs/CHANGELOG.md') . '" target="_blank">' . T_('View changes') . '</a> ';
         if ($develop_check) {
-            echo ' | <a href="https://github.com/ampache/ampache/archive/develop.zip' . '" target="_blank">' . T_('Download') . '</a>';
+            echo ' | <a href="https://github.com/ampache/ampache/archive/' . $zip_name . '.zip' . '" target="_blank">' . T_('Download') . '</a>';
         } else {
             echo ' | <a href="https://github.com/ampache/ampache/releases/download/' . self::get_latest_version() .
               '/ampache-' . self::get_latest_version() . '_all.zip"' . ' target="_blank">' . T_('Download') . '</a>';
