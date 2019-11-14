de6de1ae794971d4800e0d265bd3f6ccc2aff0d0
lachlan <lachlan.00@gmail.com>
2019-11-14 16:48:28 +1000

    document ldap.class a bit better

--- lib/class/ldap.class.php
+++ lib/class/ldap.class.php
@@ -82,12 +82,14 @@ class LDAP
      *
      * This function is here to return a real array {number} => {field} => {value array}
      * instead of the custom LDAP search results provided by the ldap_* library.
+     * @param array $searchresult
+     * @return array
      */
-    private static function clean_search_results($sr)
+    private static function clean_search_results($searchresult)
     {
         $sr_clean = [];
 
-        foreach (array_filter_key($sr, 'is_int') as $i => $result) {
+        foreach (array_filter_key($searchresult, 'is_int') as $i => $result) {
             $sr_clean[$i] = [];
 
             foreach ($result as $field => $values) {
@@ -111,6 +113,7 @@ class LDAP
      * Note: This does not open a connection. It checks whether
      * the given parameters are plausible and can be used to open a
      * connection as soon as one is needed.
+     * @return resource|false
      */
     private static function connect()
     {
@@ -185,7 +188,9 @@ class LDAP
 
     /**
      * Search for a DN in the LDAP
-     * @return string|array
+     * @param string $filter
+     * @return boolean $only_one_result
+     * @return array
      */
     private static function search($link, $base_dn, $filter, $only_one_result = true)
     {
