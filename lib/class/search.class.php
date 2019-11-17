58d4bdbc1da55664693e6af49882c9984a2d4b5c
lachlan <lachlan.00@gmail.com>
2019-11-18 09:45:10 +1000

    seach text 'is not'

--- lib/class/search.class.php
+++ lib/class/search.class.php
@@ -153,6 +153,12 @@ class Search extends playlist_object
         );
 
         $this->basetypes['text'][] = array(
+            'name' => 'not equal',
+            'description' => T_('is not'),
+            'sql' => '!='
+        );
+
+        $this->basetypes['text'][] = array(
             'name' => 'sounds',
             'description' => T_('sounds like'),
             'sql' => 'SOUNDS LIKE'
@@ -873,7 +879,7 @@ class Search extends playlist_object
         $sql .= ' ' . $limit_sql;
         $sql = trim($sql);
 
-        debug_event('search.class', 'SQL get_items: ' . $sql, 5);
+        //debug_event('search.class', 'SQL get_items: ' . $sql, 5);
         $db_results = Dba::read($sql);
         $results    = array();
         while ($row = Dba::fetch_assoc($db_results)) {
@@ -937,7 +943,7 @@ class Search extends playlist_object
         if ($this->limit > 0) {
             $sql .= " LIMIT " . (string) ($this->limit);
         }
-        debug_event('search.class', 'SQL get_items: ' . $sql, 5);
+        //debug_event('search.class', 'SQL get_items: ' . $sql, 5);
 
         $db_results = Dba::read($sql);
         while ($row = Dba::fetch_assoc($db_results)) {
@@ -1257,7 +1263,7 @@ class Search extends playlist_object
         $where_sql = implode(" $sql_logic_operator ", $where);
 
         foreach ($join['tag'] as $key => $value) {
-            debug_event('search.class', '$join[tag]: ' . $key . " " . $value, 5);
+            //debug_event('search.class', '$join[tag]: ' . $key . " " . $value, 5);
             $table['tag_' . $key] =
                 "LEFT JOIN (" .
                 "SELECT `object_id`, COUNT(`name`) AS `match` " .
@@ -1410,7 +1416,7 @@ class Search extends playlist_object
         $where_sql = implode(" $sql_logic_operator ", $where);
 
         foreach ($join['tag'] as $key => $value) {
-            debug_event('search.class', '$join[tag]: ' . $key . " " . $value, 5);
+            //debug_event('search.class', '$join[tag]: ' . $key . " " . $value, 5);
             $table['tag_' . $key] =
                 "LEFT JOIN (" .
                 "SELECT `object_id`, COUNT(`name`) AS `match` " .
@@ -1703,7 +1709,7 @@ class Search extends playlist_object
         }
         if ($join['tag']) {
             foreach ($join['tag'] as $key => $value) {
-                debug_event('search.class', '$join[tag]: ' . $key . " " . $value, 5);
+                //debug_event('search.class', '$join[tag]: ' . $key . " " . $value, 5);
                 $table['tag_' . $key] =
                     "LEFT JOIN (" .
                     "SELECT `object_id`, COUNT(`name`) AS `match` " .
