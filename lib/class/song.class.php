2c29a1e924d064d7c7422e1fc9e9f9df2cd7de1e
lachlan <lachlan.00@gmail.com>
2019-11-18 13:10:17 +1000

    Update song.class.php

--- lib/class/song.class.php
+++ lib/class/song.class.php
@@ -986,8 +986,6 @@ class Song extends database_object implements media, library_item
      * this checks to see if the current object has been played
      * if not then it sets it to played. In any case it updates stats.
      * @param integer $user
-     * @param string $agent
-     * @param array $location
      * @return boolean
      */
     public function check_play_history($user)
