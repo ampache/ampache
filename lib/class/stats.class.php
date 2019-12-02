2aa1e16124ba2925008198fc2825a42e17fe1b5e
lachlan <lachlan.00@gmail.com>
2019-12-02 13:45:14 +1000

    Update stats.class.php

--- lib/class/stats.class.php
+++ lib/class/stats.class.php
@@ -587,7 +587,7 @@ class Stats
             }
         }
         if ($allow_group_disks && $type == 'album') {
-            $sql .= "GROUP BY $sql_type`, `album`.`prefix`, `album`.`name`, `album`.`album_artist`, `album`.`release_type`, `album`.`mbid`, `album`.`year` ORDER BY `real_atime` DESC ";
+            $sql .= "GROUP BY `$sql_type`, `album`.`prefix`, `album`.`name`, `album`.`album_artist`, `album`.`release_type`, `album`.`mbid`, `album`.`year` ORDER BY `real_atime` DESC ";
         } else {
             $sql .= "GROUP BY `$sql_type` ORDER BY `real_atime` DESC ";
         }
