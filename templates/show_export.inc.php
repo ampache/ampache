87498705f861a956c880299b80483a52db684f29
lachlan <lachlan.00@gmail.com>
2020-01-08 09:29:13 +1000

    Update Copyright Year

--- templates/show_export.inc.php
+++ templates/show_export.inc.php
@@ -4,7 +4,7 @@
 /**
  *
  * LICENSE: GNU Affero General Public License, version 3 (AGPLv3)
- * Copyright 2001 - 2019 Ampache.org
+ * Copyright 2001 - 2020 Ampache.org
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
