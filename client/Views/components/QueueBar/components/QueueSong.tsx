2aa352fb1de957ff8f7128e934be35d377769566
Ashot Nazaryan <contact@hego.co>
2020-01-25 21:43:41 -0800

    QueueBar changes

--- client/Views/components/QueueBar/components/QueueSong.tsx
+++ client/Views/components/QueueBar/components/QueueSong.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Song } from '../../../../logic/Song';
+import { Link } from 'react-router-dom';
 
 interface QueueSongProps {
     song: Song;
@@ -16,7 +17,28 @@ const QueueSong: React.FC<QueueSongProps> = (props) => {
             <div className='imageWrapper'>
                 <img src={props.song.art} />
             </div>
-            <div className='songName'>{props.song.title}</div>
+            <div className='details'>
+                <div className='songName'>{props.song.title}</div>
+                <div className='albumArtist'>
+                    <Link
+                        to={`/artist/${props.song.artist.id}`}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
+                    >
+                        {props.song.artist.name}
+                    </Link>
+                    <span className='gap'>-</span>
+                    <Link
+                        to={`/album/${props.song.album.id}`}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
+                    >
+                        {props.song.album.name}
+                    </Link>
+                </div>
+            </div>
         </li>
     );
 };
