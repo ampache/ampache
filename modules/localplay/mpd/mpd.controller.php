acdf4f348034d351d89c749db33ef36e22123b2e
lachlan <github@dewaard.email>
2020-04-15 18:09:37 +1000

    Revert "default to only mpd instance"
    This reverts commit 79bc4d3a3ac63202b1b70c45853cb83787509f13.

--- modules/localplay/mpd/mpd.controller.php
+++ modules/localplay/mpd/mpd.controller.php
@@ -191,13 +191,10 @@ class AmpacheMpd extends localplay_controller
      */
     public function get_instance($instance = '')
     {
-        $instance = is_numeric($instance) ? $instance : AmpConfig::get('mpd_active');
+        $instance = $instance ? $instance : AmpConfig::get('mpd_active');
         $instance = Dba::escape($instance);
-        $sql      = "SELECT * FROM `localplay_mpd` WHERE `id`= ?";
-        // if you only have one MPD instance just default to that!
-        if (!is_numeric($instance) && count(self::get_instances()) === 1) {
-            $sql = "SELECT * FROM `localplay_mpd`";
-        }
+
+        $sql        = "SELECT * FROM `localplay_mpd` WHERE `id`= ?";
         $db_results = Dba::query($sql, array($instance));
 
         return Dba::fetch_assoc($db_results);
@@ -242,7 +239,7 @@ class AmpacheMpd extends localplay_controller
     /**
      * set_active_instance
      * This sets the specified instance as the 'active' one
-     * @param string $uid
+     * @param $uid
      * @param string $user_id
      * @return boolean
      */
@@ -255,9 +252,8 @@ class AmpacheMpd extends localplay_controller
 
         $user_id = $user_id ? $user_id : Core::get_global('user')->id;
 
-        Preference::update('mpd_active', $user_id, $uid);
-        AmpConfig::set('mpd_active', $uid, true);
-        debug_event('mdp.controller', 'set_active_instance: ' . $uid . ' ' . $user_id, 5);
+        Preference::update('mpd_active', $user_id, (int) ($uid));
+        AmpConfig::set('mpd_active', (int) ($uid), true);
 
         return true;
     } // set_active_instance
