2226c55bb132199f43e451156ab50f6acee49e55
lachlan <github@dewaard.email>
2020-07-02 15:54:12 +1000

    Revert "update_group_from_tags"
    included the wrong files

--- albums.php
+++ albums.php
@@ -72,21 +72,6 @@ switch ($_REQUEST['action']) {
         $target_url    = AmpConfig::get('web_path') . '/albums.php?action=show&amp;album=' . $object_id;
         require_once AmpConfig::get('prefix') . UI::find_template('show_update_items.inc.php');
     break;
-    case 'update_group_from_tags':
-        // Make sure they are a 'power' user at least
-        if (!Access::check('interface', '75')) {
-            UI::access_denied();
-
-            return false;
-        }
-        $album = new Album($_REQUEST['album_id']);
-        $album->format();
-        $catalog_id    = $album->get_catalogs();
-        $type          = 'album';
-        $object_id     = (int) filter_input(INPUT_GET, 'album_id', FILTER_SANITIZE_NUMBER_INT);
-        $target_url    = AmpConfig::get('web_path') . '/albums.php?action=show&amp;album=' . $object_id;
-        require_once AmpConfig::get('prefix') . UI::find_template('show_update_items.inc.php');
-        break;
     case 'set_track_numbers':
         debug_event('albums', 'Set track numbers called.', 5);
 
