ed035480bba9ad7afe295a79609f8ce6db50eb35
lachlan <github@dewaard.email>
2020-07-03 12:41:20 +1000

    make song inserts the update time as well.
    technically it was updated then right?

--- lib/class/song.class.php
+++ lib/class/song.class.php
@@ -424,17 +424,18 @@ class Song extends database_object implements media, library_item
         } else {
             $album_id = (int) ($results['album_id']);
         }
+        $insert_time = time();
 
         $sql = 'INSERT INTO `song` (`catalog`, `file`, `album`, `artist`, ' .
             '`title`, `bitrate`, `rate`, `mode`, `size`, `time`, `track`, ' .
-            '`addition_time`, `year`, `mbid`, `user_upload`, `license`, ' .
+            '`addition_time`, `update_time`, `year`, `mbid`, `user_upload`, `license`, ' .
             '`composer`, `channels`) ' .
             'VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
         $db_results = Dba::write($sql, array(
             $catalog, $file, $album_id, $artist_id,
             $title, $bitrate, $rate, $mode, $size, $time, $track,
-            time(), $year, $track_mbid, $user_upload, $license,
+            $insert_time, $insert_time, $year, $track_mbid, $user_upload, $license,
             $composer, $channels));
 
         if (!$db_results) {
