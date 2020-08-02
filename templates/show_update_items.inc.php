fd0e276c4848296fe9b47a5cf580ba06d1c0b9dc
Shen-Ta Hsieh <ibmibmibm.tw@gmail.com>
2020-08-02 10:48:35 +0800

    change http://www.gnu.org and http://fsf.org to https

--- templates/show_update_items.inc.php
+++ templates/show_update_items.inc.php
@@ -16,7 +16,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 UI::show_box_top(T_('Starting Update from Tags'), 'box box_update_items');
