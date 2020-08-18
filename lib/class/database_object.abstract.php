e92cb6154c32c513b9c07e5fdbf5ac7de81ef5ed
lachlan-00 <github@dewaard.email>
2020-08-18 17:25:36 +1000

    Update database_object.abstract.php
    cast int for object_id

--- lib/class/database_object.abstract.php
+++ lib/class/database_object.abstract.php
@@ -45,19 +45,21 @@ abstract class database_object
      */
     public function get_info($object_id, $table_name = '')
     {
-        $table_name = $table_name ? Dba::escape($table_name) : Dba::escape(strtolower(get_class($this)));
+        $table     = $table_name ? Dba::escape($table_name) : Dba::escape(strtolower(get_class($this)));
+        $object_id = (int) $object_id;
 
         // Make sure we've got a real id
         if ($object_id < 1) {
             return array();
         }
 
-        if (self::is_cached($table_name, $object_id)) {
-            return self::get_from_cache($table_name, $object_id);
+        if (self::is_cached($table, $object_id)) {
+            return self::get_from_cache($table, $object_id);
         }
 
-        $sql        = "SELECT * FROM `$table_name` WHERE `id`='$object_id'";
-        $db_results = Dba::read($sql);
+        $params     = array($object_id);
+        $sql        = "SELECT * FROM `$table` WHERE `id`= ?";
+        $db_results = Dba::read($sql, $params);
 
         if (!$db_results) {
             return array();
@@ -65,7 +67,7 @@ abstract class database_object
 
         $row = Dba::fetch_assoc($db_results);
 
-        self::add_to_cache($table_name, $object_id, $row);
+        self::add_to_cache($table, $object_id, $row);
 
         return $row;
     } // get_info
