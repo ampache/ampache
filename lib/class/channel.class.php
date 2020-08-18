07632c51ce452457b40215c3360a72a267af8d3c
lachlan-00 <github@dewaard.email>
2020-08-18 19:30:48 +1000

    dont insert stats from channels

--- lib/class/channel.class.php
+++ lib/class/channel.class.php
@@ -634,7 +634,6 @@ class Channel extends database_object implements media, library_item
 
                     // End of file, prepare to move on for next call
                     if (feof($this->transcoder['handle'])) {
-                        $this->media->set_played(-1, 'Ampache', array());
                         if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
                             fread($this->transcoder['stderr'], 4096);
                             fclose($this->transcoder['stderr']);
