6614e0c78033978a499d5a74ea20efef417ebca6
lachlan <lachlan.00@gmail.com>
2020-08-31 12:42:59 +1000

    Javascript samesite strict for cookies

--- templates/sidebar.inc.php
+++ templates/sidebar.inc.php
@@ -85,7 +85,7 @@ $(function() {
             if ($header.children(".header-img").hasClass("collapsed")) {
                 sbstate = "collapsed";
             }
-            $.cookie('sb_' + $header.children(".header-img").attr('id'), sbstate, { expires: 30, path: '/'});
+            $.cookie('sb_' + $header.children(".header-img").attr('id'), sbstate, { expires: 30, path: '/; samesite=strict'});
         });
 
     });
