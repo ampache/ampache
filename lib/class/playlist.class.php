edf561b3742329e415d4039ec299ff55699701d5
lachlan-00 <lachlan.00@gmail.com>
2020-09-09 14:10:04 +1000

    Update playlist.class.php

--- lib/class/playlist.class.php
+++ lib/class/playlist.class.php
@@ -715,9 +715,9 @@ class Playlist extends playlist_object
             $count++;
         } // end while results
 
-        $sql = "REPLACE INTO `playlist_data` (`id`, `track`) ";
+        $sql = "REPLACE INTO `playlist_data` (`id`, `track`) VALUES";
         foreach ($results as $data) {
-            $sql .= "VALUES(" . Dba::escape($data['id']) . ", " . Dba::escape($data['track']) . "), ";
+            $sql .= "(" . Dba::escape($data['id']) . ", " . Dba::escape($data['track']) . "), ";
         } // foreach re-ordered results
 
         // do this in one go
