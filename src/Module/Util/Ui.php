d848f69a66bf08fcc502dd9cec1592d04c16a44b
Daniel Jakob <wolverine@usox.org>
2020-12-21 20:55:27 +0100

    Cleanup Ui class

--- src/Module/Util/Ui.php
+++ src/Module/Util/Ui.php
@@ -144,23 +144,11 @@ class Ui implements UiInterface
     public static function clean_utf8($string)
     {
         if ($string) {
-            $clean = preg_replace('/[^\x{9}\x{a}\x{d}\x{20}-\x{d7ff}\x{e000}-\x{fffd}\x{10000}-\x{10ffff}]|[\x{7f}-\x{84}\x{86}-\x{9f}\x{fdd0}-\x{fddf}\x{1fffe}-\x{1ffff}\x{2fffe}-\x{2ffff}\x{3fffe}-\x{3ffff}\x{4fffe}-\x{4ffff}\x{5fffe}-\x{5ffff}\x{6fffe}-\x{6ffff}\x{7fffe}-\x{7ffff}\x{8fffe}-\x{8ffff}\x{9fffe}-\x{9ffff}\x{afffe}-\x{affff}\x{bfffe}-\x{bffff}\x{cfffe}-\x{cffff}\x{dfffe}-\x{dffff}\x{efffe}-\x{effff}\x{ffffe}-\x{fffff}\x{10fffe}-\x{10ffff}]/u',
-                '', $string);
-
-            // Other cleanup regex. Takes too long to process.
-            /* $regex = <<<'END'
-              /
-              (
-              (?: [\x00-\x7F]                 # single-byte sequences   0xxxxxxx
-              |   [\xC0-\xDF][\x80-\xBF]      # double-byte sequences   110xxxxx 10xxxxxx
-              |   [\xE0-\xEF][\x80-\xBF]{2}   # triple-byte sequences   1110xxxx 10xxxxxx * 2
-              |   [\xF0-\xF7][\x80-\xBF]{3}   # quadruple-byte sequence 11110xxx 10xxxxxx * 3
-              ){1,100}                        # ...one or more times
-              )
-              | .                                 # anything else
-              /x
-              END;
-              $clean = preg_replace($regex, '$1', $string); */
+            $clean = preg_replace(
+                '/[^\x{9}\x{a}\x{d}\x{20}-\x{d7ff}\x{e000}-\x{fffd}\x{10000}-\x{10ffff}]|[\x{7f}-\x{84}\x{86}-\x{9f}\x{fdd0}-\x{fddf}\x{1fffe}-\x{1ffff}\x{2fffe}-\x{2ffff}\x{3fffe}-\x{3ffff}\x{4fffe}-\x{4ffff}\x{5fffe}-\x{5ffff}\x{6fffe}-\x{6ffff}\x{7fffe}-\x{7ffff}\x{8fffe}-\x{8ffff}\x{9fffe}-\x{9ffff}\x{afffe}-\x{affff}\x{bfffe}-\x{bffff}\x{cfffe}-\x{cffff}\x{dfffe}-\x{dffff}\x{efffe}-\x{effff}\x{ffffe}-\x{fffff}\x{10fffe}-\x{10ffff}]/u',
+                '',
+                $string
+            );
 
             if ($clean) {
                 return rtrim((string)$clean);
@@ -462,28 +450,6 @@ class Ui implements UiInterface
     }
 
     /**
-     * show_header
-     *
-     * For now this just shows the header template
-     *
-     * @deprecated use non-static version
-     */
-    public static function show_header()
-    {
-        require_once self::find_template('header.inc.php');
-    }
-
-    /**
-     * show_header_tiny
-     *
-     * For now this just shows the header-tiny template
-     */
-    public static function show_header_tiny()
-    {
-        require_once self::find_template('header-tiny.inc.php');
-    }
-
-    /**
      * show
      *
      * Show the requested template file
@@ -501,7 +467,7 @@ class Ui implements UiInterface
     
     public function showHeader(): void
     {
-        static::show_header();
+        require_once self::find_template('header.inc.php');
     }
 
     /**
@@ -568,23 +534,11 @@ class Ui implements UiInterface
     }
 
     /**
-     * show_query_stats
-     *
-     * This shows the bottom of the box
-     *
-     * @deprecated use non-static version
-     */
-    public static function show_query_stats()
-    {
-        require self::find_template('show_query_stats.inc.php');
-    }
-
-    /**
      * This shows the query stats
      */
     public function showQueryStats(): void
     {
-        static::show_query_stats();
+        require self::find_template('show_query_stats.inc.php');
     }
 
     public static function show_custom_style()
