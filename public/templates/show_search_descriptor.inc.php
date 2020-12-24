65163169ca6767438831a75944582d5cc48ffc9f
Daniel Jakob <github@usox.org>
2020-12-24 10:05:41 +0100

    Add missing import

--- public/templates/show_search_descriptor.inc.php
+++ public/templates/show_search_descriptor.inc.php
@@ -20,6 +20,8 @@
  *
  */
 
+use Ampache\Config\AmpConfig;
+
 echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
     <ShortName>Ampache</ShortName>
