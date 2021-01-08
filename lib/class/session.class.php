1ba7678370bc39706e630f95e058da439bf8b8ed
lachlan <lachlan.00@gmail.com>
2021-01-08 12:11:10 +1000

    gen session on a rand string for api key

--- lib/class/session.class.php
+++ lib/class/session.class.php
@@ -203,7 +203,7 @@ class Session
         // Regenerate the session ID to prevent fixation
         switch ($data['type']) {
             case 'api':
-                $key = (isset($data['apikey'])) ? md5(((string) $data['apikey'] . (string) time())) : md5(uniqid((string) rand(), true));
+                $key = (isset($data['apikey'])) ? md5(((string) $data['apikey'] . md5(uniqid((string) rand(), true)))) : md5(uniqid((string) rand(), true));
                 break;
             case 'stream':
                 $key = (isset($data['sid'])) ? $data['sid'] : md5(uniqid((string) rand(), true));
