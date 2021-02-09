d5005830ecba814a63c20f4e26ec37edc25cd1f0
lachlan <lachlan.00@gmail.com>
2021-02-09 16:06:19 +1000

    Update podcast_episode.class.php

--- src/Model/Podcast_Episode.php
+++ src/Model/Podcast_Episode.php
@@ -433,7 +433,7 @@ class Podcast_Episode extends database_object implements Media, library_item, Ga
      * @param string $additional_params
      * @param string $player
      * @param boolean $local
-     * @param boolean $uid
+     * @param string $uid
      * @return string
      */
     public function play_url($additional_params = '', $player = '', $local = false, $uid = false)
