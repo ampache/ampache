ampache/ampache

commit f698dbb34d9c23146ed4d5e0eb5a9cde14f8b8bb

Code documentation and fixes
* need a user for Bookmark::get_bookmarks
* remove redundant parameters
* send user_id from api methods
* URL for podcast_episodes should be a play_url
* rename get_bookmarks_ids and require a User to call
* document xml_data, json_data the same way
* fix api::record_play user
* rename get_info to the abstract values

lib/class/catalog.class.php
@@ -450,17 +450,17 @@ abstract class Catalog extends database_object
 
     /**
      * Get catalog info from table.
-     * @param integer $catalog_id
+     * @param integer $object_id
      * @param string $table
      * @return array
      */
-    public function get_info($catalog_id, $table = 'catalog')
+    public function get_info($object_id, $table_name = 'catalog')
     {
-        $info = parent::get_info($catalog_id, $table);
+        $info = parent::get_info($object_id, $table);
 
         $table      = 'catalog_' . $this->get_type();
         $sql        = "SELECT `id` FROM $table WHERE `catalog_id` = ?";
-        $db_results = Dba::read($sql, array($catalog_id));
+        $db_results = Dba::read($sql, array($object_id));
 
         if ($results = Dba::fetch_assoc($db_results)) {
             $info_type = parent::get_info($results['id'], $table);

lib/class/ApiMethods/BookmarksMethod.php
@@ -52,7 +52,7 @@ final class BookmarksMethod
     public static function bookmarks(array $input)
     {
         $user      = User::get_from_username(Session::username($input['auth']));
-        $bookmarks = Bookmark::get_bookmarks_ids($user);
+        $bookmarks = Bookmark::get_bookmark_ids($user);
         if (empty($bookmarks)) {
             Api::empty('bookmark', $input['api_format']);
 

lib/class/ApiMethods/GetIndexesMethod.php
@@ -120,12 +120,12 @@ final class GetIndexesMethod
             case 'json':
                 JSON_Data::set_offset($input['offset']);
                 JSON_Data::set_limit($input['limit']);
-                echo JSON_Data::indexes($objects, $type, $include);
+                echo JSON_Data::indexes($objects, $type, $user->id, $include);
                 break;
             default:
                 XML_Data::set_offset($input['offset']);
                 XML_Data::set_limit($input['limit']);
-                echo XML_Data::indexes($objects, $type, true, $include);
+                echo XML_Data::indexes($objects, $type, $user->id, true, $include);
         }
         Session::extend($input['auth']);
 

lib/class/ApiMethods/GetSimilarMethod.php
@@ -29,6 +29,7 @@ use Api;
 use JSON_Data;
 use Recommendation;
 use Session;
+use User;
 use XML_Data;
 
 /**
@@ -84,17 +85,18 @@ final class GetSimilarMethod
             return false;
         }
 
+        $user = User::get_from_username(Session::username($input['auth']));
         ob_end_clean();
         switch ($input['api_format']) {
             case 'json':
                 JSON_Data::set_offset($input['offset']);
                 JSON_Data::set_limit($input['limit']);
-                echo JSON_Data::indexes($objects, $type);
+                echo JSON_Data::indexes($objects, $type, $user->id);
                 break;
             default:
                 XML_Data::set_offset($input['offset']);
                 XML_Data::set_limit($input['limit']);
-                echo XML_Data::indexes($objects, $type);
+                echo XML_Data::indexes($objects, $type, $user->id);
         }
         Session::extend($input['auth']);
 

lib/class/ApiMethods/PlaylistGenerateMethod.php
@@ -178,10 +178,10 @@ final class PlaylistGenerateMethod
             case 'index':
                 switch ($input['api_format']) {
                     case 'json':
-                        echo JSON_Data::indexes($song_ids, 'song');
+                        echo JSON_Data::indexes($song_ids, 'song', $user->id);
                         break;
                     default:
-                        echo XML_Data::indexes($song_ids, 'song');
+                        echo XML_Data::indexes($song_ids, 'song', $user->id);
                 }
                 break;
             case 'song':

lib/class/ApiMethods/PodcastCreateMethod.php
@@ -75,13 +75,15 @@ final class PodcastCreateMethod
 
             return false;
         }
+
+        $user = User::get_from_username(Session::username($input['auth']));
         ob_end_clean();
         switch ($input['api_format']) {
             case 'json':
-                echo JSON_Data::podcasts(array($podcast), false, false);
+                echo JSON_Data::podcasts(array($podcast), $user->id, false, false);
                 break;
             default:
-                echo XML_Data::podcasts(array($podcast));
+                echo XML_Data::podcasts(array($podcast), $user->id);
         }
         Catalog::count_table('podcast');
         Session::extend($input['auth']);

lib/class/ApiMethods/PodcastEpisodeMethod.php
@@ -30,6 +30,7 @@ use Api;
 use JSON_Data;
 use Podcast_Episode;
 use Session;
+use User;
 use XML_Data;
 
 /**
@@ -69,13 +70,14 @@ final class PodcastEpisodeMethod
             return false;
         }
 
+        $user = User::get_from_username(Session::username($input['auth']));
         ob_end_clean();
         switch ($input['api_format']) {
             case 'json':
-                echo JSON_Data::podcast_episodes(array($object_id), false, false);
+                echo JSON_Data::podcast_episodes(array($object_id), $user->id, false, false);
                 break;
             default:
-                echo XML_Data::podcast_episodes(array($object_id));
+                echo XML_Data::podcast_episodes(array($object_id), $user->id);
         }
         Session::extend($input['auth']);
 

lib/class/ApiMethods/PodcastEpisodesMethod.php
@@ -30,6 +30,7 @@ use Api;
 use JSON_Data;
 use Podcast;
 use Session;
+use User;
 use XML_Data;
 
 /**
@@ -77,17 +78,18 @@ final class PodcastEpisodesMethod
             return false;
         }
 
+        $user = User::get_from_username(Session::username($input['auth']));
         ob_end_clean();
         switch ($input['api_format']) {
             case 'json':
                 JSON_Data::set_offset($input['offset']);
                 JSON_Data::set_limit($input['limit']);
-                echo JSON_Data::podcast_episodes($items);
+                echo JSON_Data::podcast_episodes($items, $user->id);
                 break;
             default:
                 XML_Data::set_offset($input['offset']);
                 XML_Data::set_limit($input['limit']);
-                echo XML_Data::podcast_episodes($items);
+                echo XML_Data::podcast_episodes($items, $user->id);
         }
         Session::extend($input['auth']);
 

commit 20ddfb94d7f9cbbd3458bd8e44658c150ec4395e

Correct parameter name
Fixes #2745
Regression introduced in: f698dbb

lib/class/catalog.class.php
@@ -456,7 +456,7 @@ abstract class Catalog extends database_object
      */
     public function get_info($object_id, $table_name = 'catalog')
     {
-        $info = parent::get_info($object_id, $table);
+        $info = parent::get_info($object_id, $table_name);
 
         $table      = 'catalog_' . $this->get_type();
         $sql        = "SELECT `id` FROM $table WHERE `catalog_id` = ?";