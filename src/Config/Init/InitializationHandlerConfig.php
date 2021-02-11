ef6315472cc3299c5bb97e6165bab529155c1c3d
lachlan <lachlan.00@gmail.com>
2021-02-11 10:11:01 +1000

    Begin the Ampache5 process

--- src/Config/Init/InitializationHandlerConfig.php
+++ src/Config/Init/InitializationHandlerConfig.php
@@ -33,7 +33,7 @@ use Ampache\Module\Util\EnvironmentInterface;
 final class InitializationHandlerConfig implements InitializationHandlerInterface
 {
     private const VERSION        = 'develop';
-    private const CONFIG_VERSION = '48';
+    private const CONFIG_VERSION = '49';
 
     public const CONFIG_FILE_PATH = __DIR__ . '/../../../config/ampache.cfg.php';
 
