5e7488d1ceee7f217bf0d37546151364a24dbdd0
lachlan <lachlan.00@gmail.com>
2021-02-16 13:18:43 +1000

    fix php errors from a missing upnp device

--- src/Module/Playback/Localplay/Upnp/UPnPPlayer.php
+++ src/Module/Playback/Localplay/Upnp/UPnPPlayer.php
@@ -148,12 +148,17 @@ class UPnPPlayer
     }
 
     /**
-     * @return SimpleXMLElement
+     * GetState
+     *
+     * @return SimpleXMLElement|string
      */
     public function GetState()
     {
         $response    = $this->Device()->instanceOnly('GetTransportInfo');
         $responseXML = simplexml_load_string($response);
+        if (empty($responseXML)) {
+            return '';
+        }
         list($state) = $responseXML->xpath('//CurrentTransportState');
 
         //!!debug_event(self::class, 'GetState = ' . $state, 5);
@@ -399,6 +404,8 @@ class UPnPPlayer
 
     /**
      * GetVolume
+     *
+     * @return SimpleXMLElement|string
      */
     public function GetVolume()
     {
@@ -411,6 +418,9 @@ class UPnPPlayer
         ));
 
         $responseXML  = simplexml_load_string($response);
+        if (empty($responseXML)) {
+            return '';
+        }
         list($volume) = ($responseXML->xpath('//CurrentVolume'));
         debug_event(self::class, 'GetVolume:' . $volume, 5);
 
