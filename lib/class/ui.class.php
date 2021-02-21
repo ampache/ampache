92559a0c5a79554f2667ef132f84c03082a5d181
lachlan <lachlan.00@gmail.com>
2021-02-22 09:14:31 +1000

    copy background poisiont and size from source-changes
    a517aff6e

--- lib/class/ui.class.php
+++ lib/class/ui.class.php
@@ -546,7 +546,7 @@ class UI
     public static function show_custom_style()
     {
         if (AmpConfig::get('custom_login_background')) {
-            echo "<style> body { background-image: url('" . AmpConfig::get('custom_login_background') . "') !important; }</style>";
+            echo "<style> body { background-position: center; background-size: cover; background-image: url('" . AmpConfig::get('custom_login_background') . "') !important; }</style>";
         }
 
         if (AmpConfig::get('custom_login_logo')) {
