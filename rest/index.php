dcb1c132225e6d8c622fb78b3abfdd0b73a7be22
lachlan <lachlan.00@gmail.com>
2021-03-22 18:57:52 +1000

    Update index.php

--- rest/index.php
+++ rest/index.php
@@ -83,6 +83,8 @@ if (!in_array($user, User::get_valid_usernames())) {
     debug_event('rest/index', 'Invalid authentication attempt to Subsonic API for user [' . $user . ']', 3);
     ob_end_clean();
     Subsonic_Api::apiOutput2($f, Subsonic_XML_Data::createError(Subsonic_XML_Data::SSERROR_BADAUTH, 'Invalid authentication attempt to Subsonic API for user [' . $user . ']', $version), $callback);
+
+    return false;
 }
 
 // Check user authentication
