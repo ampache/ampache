32110e03d217ea738e3548f6fa63a931a28e0d08
Daniel Jakob <github@usox.org>
2021-03-22 09:38:01 +0100

    Ensure the album list gets loaded before using it for calculations #2844
    Also repair some formatting issues

--- src/Module/Api/Daap_Api.php
+++ src/Module/Api/Daap_Api.php
@@ -436,8 +436,8 @@ class Daap_Api
                     }
                     $params .= '&transcode_to=' . $type;
                     $className = ObjectTypeToClassNameMapper::map($type);
-                    $media = new $className($object_id);
-                    $url   = $media->play_url($params, 'api', true);
+                    $media     = new $className($object_id);
+                    $url       = $media->play_url($params, 'api', true);
                     self::follow_stream($url);
 
                     return false;
