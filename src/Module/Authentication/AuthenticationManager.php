3c12ef26c70a50010e10fc7d2f459e7253ccc604
lachlan <lachlan.00@gmail.com>
2021-03-23 13:03:57 +1000

    be a bit more stringent with tokens

--- src/Module/Authentication/AuthenticationManager.php
+++ src/Module/Authentication/AuthenticationManager.php
@@ -91,11 +91,11 @@ final class AuthenticationManager implements AuthenticationManagerInterface
     ): array {
         // subsonic token auth with apikey
         if (strlen((string)$token) && strlen((string)$salt) && strlen((string)$username)) {
-            $sql        = 'SELECT `apikey` FROM `user` WHERE `username` = ?';
+            $sql        = 'SELECT `apikey`, `username` FROM `user` WHERE `username` = ?';
             $db_results = Dba::read($sql, [$username]);
             $row        = Dba::fetch_assoc($db_results);
             $hash_token = hash('md5', ($row['apikey'] . $salt));
-            if ($token == $hash_token) {
+            if ($token == $hash_token && $row['username'] == $username && isset($row['apikey'])) {
                 return [
                     'success' => true,
                     'type' => 'api',
