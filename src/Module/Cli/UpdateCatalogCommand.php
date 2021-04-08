13541dd01de430f86a73379ac1dc6403b14b7362
Daniel Jakob <wolverine@usox.org>
2021-04-08 21:25:19 +0200

    Buffer the catalog actions separate and print the buffer #2848

--- src/Module/Cli/UpdateCatalogCommand.php
+++ src/Module/Cli/UpdateCatalogCommand.php
@@ -25,22 +25,17 @@ declare(strict_types=1);
 namespace Ampache\Module\Cli;
 
 use Ahc\Cli\Input\Command;
-use Ampache\Config\ConfigContainerInterface;
 use Ampache\Module\Catalog\Update\UpdateCatalogInterface;
 
 final class UpdateCatalogCommand extends Command
 {
-    private ConfigContainerInterface $configContainer;
-
     private UpdateCatalogInterface $updateCatalog;
 
     public function __construct(
-        ConfigContainerInterface $configContainer,
         UpdateCatalogInterface $updateCatalog
     ) {
         parent::__construct('run:updateCatalog', T_('Perform catalog actions for all files of a catalog'));
 
-        $this->configContainer = $configContainer;
         $this->updateCatalog   = $updateCatalog;
 
         $this
