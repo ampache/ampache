b161ce0bb2c9ecb39bf719c006e63eb27e7be518
lachlan <lachlan.00@gmail.com>
2021-05-26 16:55:48 +1000

    garbage collect bad user_Activity

--- src/Repository/UserActivityRepository.php
+++ src/Repository/UserActivityRepository.php
@@ -116,6 +116,7 @@ final class UserActivityRepository implements UserActivityRepositoryInterface
         } else {
             foreach ($types as $type) {
                 Dba::write("DELETE FROM `user_activity` WHERE `object_type` = '$type' AND `user_activity`.`object_id` NOT IN (SELECT `$type`.`id` FROM `$type`);");
+                Dba::write("DELETE FROM `user_activity` WHERE `object_type` IN ('album', 'artist');");
             }
         }
     }
