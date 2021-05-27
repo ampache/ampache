0a79d30fcded9e6eb6c3accafbf27c7abd42fe6e
lachlan <lachlan.00@gmail.com>
2021-05-27 12:58:39 +1000

    wrong item type on time update

--- src/Repository/Model/Catalog.php
+++ src/Repository/Model/Catalog.php
@@ -1812,7 +1812,7 @@ abstract class Catalog extends database_object
                 foreach ($libitem->get_child_ids() as $album_id) {
                     $album_tags = self::getSongTags('album', $album_id);
                     Tag::update_tag_list(implode(',', $album_tags), 'album', $album_id, false);
-                    static::getAlbumRepository()->updateTime($libitem);
+                    static::getAlbumRepository()->updateTime($album_id);
                 }
                 $tags = self::getSongTags('artist', $libitem->id);
                 Tag::update_tag_list(implode(',', $tags), 'artist', $libitem->id, false);
