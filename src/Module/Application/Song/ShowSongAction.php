2711120685ebe0a49cf0f1c1e9e17b7bc756eba5
lachlan <lachlan.00@gmail.com>
2021-09-01 05:57:04 +1000

    scrub and display fixed

--- src/Module/Application/Song/ShowSongAction.php
+++ src/Module/Application/Song/ShowSongAction.php
@@ -83,7 +83,7 @@ final class ShowSongAction implements ApplicationActionInterface
             $this->ui->showBoxTop(
                 sprintf(
                     T_('%s Details'),
-                    $song->f_title
+                    scrub_out($song->f_title)
                 ),
                 'box box_song_details'
             );
