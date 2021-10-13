ca74ff9d456e67f17691bb2aab2eb8400d0d9f1b
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2021-10-13 16:17:16 +1100

    Category browser revamp (#3043)

--- public/templates/show_mashup_browse_form.inc.php
+++ public/templates/show_mashup_browse_form.inc.php
@@ -5,29 +5,27 @@ use Ampache\Repository\Model\Video;
 use Ampache\Repository\VideoRepositoryInterface;
 
 global $dic;
+
 $videoRepository = $dic->get(VideoRepositoryInterface::class);
-$web_path        = AmpConfig::get('web_path'); ?>
+$web_path        = AmpConfig::get('web_path');
+$filter_str      = (string) filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES); ?>
+
 <h3 class="box-title"><?php echo T_('Dashboards'); ?></h3>
-<table class="tabledata">
-    <tr id="browse_location">
-        <td>
-            <a href="<?php echo $web_path; ?>/mashup.php?action=album"><?php echo T_('Albums'); ?></a>
-        </td>
-        <td>
-            <a href="<?php echo $web_path; ?>/mashup.php?action=artist"><?php echo T_('Artists'); ?></a>
-        </td>
-        <td>
-            <a href="<?php echo $web_path; ?>/mashup.php?action=playlist"><?php echo T_('Playlists'); ?></a>
-        </td>
-        <?php if (AmpConfig::get('podcast')) { ?>
-            <td>
-                <a href="<?php echo $web_path; ?>/mashup.php?action=podcast_episode"><?php echo T_('Podcast Episodes'); ?></a>
-            </td>
-        <?php }
-        if (AmpConfig::get('allow_video') && $videoRepository->getItemCount(Video::class)) { ?>
-            <td>
-                <a href="<?php echo $web_path; ?>/mashup.php?action=video"><?php echo T_('Videos'); ?></a>
-            </td>
-        <?php } ?>
-    </tr>
-</table>
+
+<div class="browse_location">
+    <a class="category <?php echo ($filter_str == 'album') ? 'current' : '' ?>" href="<?php echo $web_path; ?>/mashup.php?action=album">
+        <?php echo T_('Albums'); ?>
+    </a>
+    <a class="category <?php echo ($filter_str == 'artist') ? 'current' : '' ?>" href="<?php echo $web_path; ?>/mashup.php?action=artist">
+        <?php echo T_('Artists'); ?>
+    </a>
+    <a class="category <?php echo ($filter_str == 'playlist') ? 'current' : '' ?>" href="<?php echo $web_path; ?>/mashup.php?action=playlist">
+        <?php echo T_('Playlists'); ?>
+    </a>
+    <?php if (AmpConfig::get('podcast')) { ?>
+        <a class="category <?php echo ($filter_str == 'podcast_episode') ? 'current' : '' ?>" href="<?php echo $web_path; ?>/mashup.php?action=podcast_episode"><?php echo T_('Podcast Episodes'); ?></a>
+    <?php }
+    if (AmpConfig::get('allow_video') && $videoRepository->getItemCount(Video::class)) { ?>
+        <a class="category <?php echo ($filter_str == 'video') ? 'current' : '' ?>" href="<?php echo $web_path; ?>/mashup.php?action=video"><?php echo T_('Videos'); ?></a>
+    <?php } ?>
+</div>
