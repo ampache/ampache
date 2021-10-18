39414635ae24888bac7856266050ebf7db5415e8
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2021-10-18 11:50:28 +1100

    Add Top Tracks and Similar Songs to Artist pages
    * Top tracks are base on play history
    * Similar tracks requires a last.fm key to get similar objects

--- public/templates/show_artist.inc.php
+++ public/templates/show_artist.inc.php
@@ -236,11 +236,13 @@ if (AmpConfig::get('sociable') && $owner_id > 0) {
     <div id="tabs_container">
         <ul id="tabs">
             <li class="tab_active"><a href="#albums"><?php echo T_('Albums'); ?></a></li>
+            <li><a id="top_tracks_link" href="#top_tracks"><?php echo T_('Top Tracks'); ?></a></li>
 <?php if ($use_wanted) { ?>
             <li><a id="missing_albums_link" href="#missing_albums"><?php echo T_('Missing Albums'); ?></a></li>
 <?php } ?>
 <?php if ($show_similar) { ?>
             <li><a id="similar_artist_link" href="#similar_artist"><?php echo T_('Similar Artists'); ?></a></li>
+            <li><a id="similar_songs_link" href="#similar_songs"><?php echo T_('Similar Songs'); ?></a></li>
 <?php } ?>
 <?php if ($use_label) { ?>
             <li><a id="labels_link" href="#labels"><?php echo T_('Labels'); ?></a></li>
@@ -267,6 +269,13 @@ if (AmpConfig::get('sociable') && $owner_id > 0) {
         $browse->store();
     } ?>
         </div>
+        <?php echo Ajax::observe('top_tracks_link', 'click', Ajax::action('?page=index&action=top_tracks&artist=' . $artist->id, 'top_tracks')); ?>
+        <div id="top_tracks" class="tab_content">
+            <?php Ui::show_box_top(null, 'info-box');
+            echo T_('Loading...');
+            Ui::show_box_bottom(); ?>
+        </div>
+
 <?php if ($use_wanted) {
         echo Ajax::observe('missing_albums_link', 'click', Ajax::action('?page=index&action=wanted_missing_albums&artist=' . $artist->id, 'missing_albums')); ?>
         <div id="missing_albums" class="tab_content">
@@ -282,6 +291,12 @@ if (AmpConfig::get('sociable') && $owner_id > 0) {
         <?php Ui::show_box_top(T_('Similar Artists'), 'info-box');
         echo T_('Loading...');
         Ui::show_box_bottom(); ?>
+        </div>
+        <?php echo Ajax::observe('similar_songs_link', 'click', Ajax::action('?page=index&action=similar_songs&artist=' . $artist->id, 'similar_songs')); ?>
+        <div id="similar_songs" class="tab_content">
+            <?php Ui::show_box_top(null, 'info-box');
+            echo T_('Loading...');
+            Ui::show_box_bottom(); ?>
         </div>
 <?php
     } ?>
---
+++ public/templates/show_top_tracks.inc.php
@@ -0,0 +1,138 @@
+<?php
+/* vim:set softtabstop=4 shiftwidth=4 expandtab: */
+/**
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright 2001 - 2020 Ampache.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+use Ampache\Config\AmpConfig;
+use Ampache\Gui\GuiFactoryInterface;
+use Ampache\Gui\TalFactoryInterface;
+use Ampache\Repository\Model\Artist;
+use Ampache\Repository\Model\Rating;
+use Ampache\Repository\Model\Song;
+use Ampache\Repository\Model\User;
+use Ampache\Repository\Model\Userflag;
+use Ampache\Module\Authorization\Access;
+use Ampache\Module\Authorization\GatekeeperFactoryInterface;
+
+/** @var Ampache\Repository\Model\Browse $browse */
+/** @var array $object_ids */
+/** @var array $hide_columns */
+/** @var string $argument_param */
+/** @var string $limit_threshold */
+/** @var Artist $artist */
+
+$web_path     = AmpConfig::get('web_path');
+$show_ratings = User::is_registered() && (AmpConfig::get('ratings'));
+$hide_genres  = AmpConfig::get('hide_genres');
+$is_table     = true;
+// hide columns you don't always need
+$hide_artist  = in_array('cel_artist', $hide_columns);
+$hide_album   = in_array('cel_album', $hide_columns);
+$hide_year    = in_array('cel_year', $hide_columns);
+$hide_drag    = in_array('cel_drag', $hide_columns);
+$show_license = AmpConfig::get('licensing') && AmpConfig::get('show_license');
+?>
+<table id="top_tracks_<?php echo $artist->id; ?>" class="tabledata striped-rows">
+    <thead>
+    <tr class="th-top">
+        <th class="cel_play essential"></th>
+        <th class="cel_song essential persist"><?php echo T_('Song Title'); ?></th>
+        <th class="cel_add essential"></th>
+        <?php if (!$hide_artist) { ?>
+            <th class="cel_artist optional"><?php echo T_('Song Artist'); ?></th>
+        <?php } ?>
+        <?php if (!$hide_album) { ?>
+            <th class="cel_album essential"><?php echo T_('Album'); ?></th>
+        <?php } ?>
+        <?php if (!$hide_year) { ?>
+            <th class="cel_year"><?php echo T_('Year'); ?></th>
+        <?php } ?>
+        <?php if (!$hide_genres) { ?>
+            <th class="cel_tags optional"><?php echo T_('Genres'); ?></th>
+        <?php } ?>
+        <th class="cel_time optional"><?php echo T_('Time'); ?></th>
+        <?php if (AmpConfig::get('show_played_times')) { ?>
+            <th class="cel_counter optional"><?php echo T_('# Played'); ?></th>
+            <?php
+        } ?>
+        <?php if (AmpConfig::get('show_skipped_times')) { ?>
+            <th class="cel_counter optional"><?php echo T_('# Skipped'); ?></th>
+            <?php
+        } ?>
+        <?php if ($show_ratings) { ?>
+            <th class="cel_ratings optional"><?php echo T_('Rating'); ?></th>
+            <?php if (AmpConfig::get('ratings')) {
+                Rating::build_cache('song', $object_ids);
+                Userflag::build_cache('song', $object_ids);
+            }
+        } ?>
+        <th class="cel_action essential"><?php echo T_('Actions'); ?></th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php global $dic;
+        $talFactory = $dic->get(TalFactoryInterface::class);
+        $guiFactory = $dic->get(GuiFactoryInterface::class);
+        $gatekeeper = $dic->get(GatekeeperFactoryInterface::class)->createGuiGatekeeper();
+
+        foreach ($object_ids as $song_id) {
+            $libitem = new Song($song_id, $limit_threshold);
+            $libitem->format(); ?>
+
+            <tr id="song_<?php echo $libitem->id; ?>">
+                <?php
+                if ($libitem->enabled || Access::check('interface', 50)) {
+                    $content = $talFactory->createTalView()
+                        ->setContext('USER_IS_REGISTERED', User::is_registered())
+                        ->setContext('USING_RATINGS', User::is_registered() && (AmpConfig::get('ratings')))
+                        ->setContext('SONG', $guiFactory->createSongViewAdapter($gatekeeper, $libitem))
+                        ->setContext('CONFIG', $guiFactory->createConfigViewAdapter())
+                        ->setContext('ARGUMENT_PARAM', '')
+                        ->setContext('IS_TABLE_VIEW', $is_table)
+                        ->setContext('IS_SHOW_TRACK', (!empty($argument) && $is_table))
+                        ->setContext('IS_SHOW_LICENSE', $show_license)
+                        ->setContext('IS_HIDE_GENRE', $hide_genres)
+                        ->setContext('IS_HIDE_ARTIST', $hide_artist)
+                        ->setContext('IS_HIDE_ALBUM', $hide_album)
+                        ->setContext('IS_HIDE_YEAR', $hide_year)
+                        ->setContext('IS_HIDE_DRAG', (empty($argument) || $hide_drag))
+                        ->setTemplate('song_row.xhtml')
+                        ->render();
+
+                    echo $content;
+                } ?>
+            </tr>
+            <?php
+        } ?>
+
+        <?php if (!count($object_ids)) { ?>
+            <tr>
+                <td colspan="100"><span class="nodata"><?php echo T_('No song found'); ?></span></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<script>
+    var index = 1;
+    var indexes = $("#top_tracks_<?php echo $artist->id; ?> .cel_play_content").each(function() {
+        $(this).html(index++);
+    });
+</script>
