b27d855e215a45c85507771d65ff0314267f3d7c
lachlan <lachlan.00@gmail.com>
2021-10-23 06:41:47 +1000

    php-cs

--- src/Repository/UserActivityRepository.php
+++ src/Repository/UserActivityRepository.php
@@ -115,7 +115,7 @@ final class UserActivityRepository implements UserActivityRepositoryInterface
             }
         } else {
             foreach ($types as $type) {
-                Dba::write("DELETE FROM `user_activity` WHERE `object_type` = ? AND `user_activity`.`object_id` NOT IN (SELECT `$type`.`id` FROM `$type`);", array($type))
+                Dba::write("DELETE FROM `user_activity` WHERE `object_type` = ? AND `user_activity`.`object_id` NOT IN (SELECT `$type`.`id` FROM `$type`);", array($type));
             }
             // accidental plays
             Dba::write("DELETE FROM `user_activity` WHERE `object_type` IN ('album', 'artist') AND `action` = 'play';");
