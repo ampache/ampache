1b31d83b85ccc540ac370daa214863afa78fa631
lachlan <lachlan.00@gmail.com>
2021-10-29 18:29:05 +1000

    missing user_id cast

--- src/Repository/Model/Search.php
+++ src/Repository/Model/Search.php
@@ -658,6 +658,7 @@ class Search extends playlist_object
      */
     private function artist_types()
     {
+        $user_id = $this->search_user->id ?? 0;
         $t_artist_data = T_('Artist Data');
         $this->type_text('title', T_('Name'), $t_artist_data);
         $this->type_numeric('yearformed', T_('Year Formed'), 'numeric', $t_artist_data);
@@ -713,6 +714,7 @@ class Search extends playlist_object
      */
     private function album_types()
     {
+        $user_id = $this->search_user->id ?? 0;
         $t_album_data = T_('Album Data');
         $this->type_text('title', T_('Title'), $t_album_data);
         $this->type_text('artist', T_('Album Artist'), $t_album_data);
