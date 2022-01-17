85ebc09a35cd6131b692af479b8386f70cdcf977
lachlan <lachlan.00@gmail.com>
2022-01-17 17:29:49 +1000

    shouldn't to that for no reason

--- src/Repository/Model/Userflag.php
+++ src/Repository/Model/Userflag.php
@@ -203,7 +203,6 @@ class Userflag extends database_object
         }
         if ($this->type == 'album' && AmpConfig::get('album_group')) {
             $album = new Album($this->id);
-            $album->get_group_disks_ids();
             $album_array = $album->get_group_disks_ids();
             self::set_flag_for_group($flagged, $album_array, $user_id);
 
