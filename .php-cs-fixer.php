4a1d1028add99a542a0f243e7e9db882f0e6674c
lachlan <lachlan.00@gmail.com>
2022-02-09 11:20:20 +1000

    add php-cs-fixer 3.x.x config
    bump fixer version to 2.19

---
+++ .php-cs-fixer.php
@@ -0,0 +1,37 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude(['vendor', 'lib/components', 'foam', 'ample', 'client', 'newclient'])
+    ->in('public/')
+    ->in('src/')
+    ->in('tests/')
+;
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+        '@PSR2' => true,
+        'binary_operator_spaces' => ['operators' => ['=' => 'align']],
+        'blank_line_after_namespace' => true,
+        'blank_line_before_statement' => ['statements' => ['declare', 'return']],
+        'braces' => ['allow_single_line_closure' => true],
+        'concat_space' => ['spacing' => 'one'],
+        'constant_case' => true,
+        'elseif' => true,
+        'encoding' => true,
+        'full_opening_tag' => true,
+        'line_ending' => true,
+        'method_argument_space' => false,
+        'no_break_comment' => false,
+        'no_trailing_whitespace' => true,
+        'no_whitespace_in_blank_line' => true,
+        'single_blank_line_at_eof' => true,
+        'strict_param' => false,
+        'visibility_required' => false,
+        'visibility_required' => true,
+     ])
+   ->setIndent("    ")
+   ->setUsingCache(false)
+   ->setFinder($finder)
+   ->setLineEnding("\n")
+;
+
