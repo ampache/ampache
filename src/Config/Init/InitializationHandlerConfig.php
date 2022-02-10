b9f0e1ad19c14affff5cc02953210724d42fb04c
lachlan <lachlan.00@gmail.com>
2022-02-11 09:51:11 +1000

    5.2.1

--- src/Config/Init/InitializationHandlerConfig.php
+++ src/Config/Init/InitializationHandlerConfig.php
@@ -32,7 +32,7 @@ use Ampache\Module\Util\EnvironmentInterface;
 
 final class InitializationHandlerConfig implements InitializationHandlerInterface
 {
-    private const VERSION        = '5.2.1-develop';
+    private const VERSION        = '5.2.1-release';
     private const CONFIG_VERSION = '59';
 
     public const CONFIG_FILE_PATH = __DIR__ . '/../../../config/ampache.cfg.php';
