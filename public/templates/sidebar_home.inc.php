9efdd7eeb1cca311f7a54dfbbc4d32ec3e6385b5
lachlan <lachlan.00@gmail.com>
2022-04-13 15:05:44 +1000

    move it under artists if you have both

--- public/templates/sidebar_home.inc.php
+++ public/templates/sidebar_home.inc.php
@@ -65,11 +65,11 @@ $web_path        = AmpConfig::get('web_path'); ?>
         <ul class="sb3" id="sb_browse_music">
             <li id="sb_home_browse_music_songTitle"><a href="<?php echo $web_path; ?>/browse.php?action=song"><?php echo $t_songs ?></a></li>
             <li id="sb_home_browse_music_album"><a href="<?php echo $web_path; ?>/browse.php?action=album"><?php echo $t_albums; ?></a></li>
-            <?php if ($showAlbumArtist || (!$showArtist)) { ?>
-                <li id="sb_home_browse_music_artist"><a href="<?php echo $web_path; ?>/browse.php?action=album_artist"><?php echo $t_a_artists; ?></a></li>
-            <?php } ?>
             <?php if ($showArtist) { ?>
                 <li id="sb_home_browse_music_artist"><a href="<?php echo $web_path; ?>/browse.php?action=artist"><?php echo $t_artists; ?></a></li>
+            <?php } ?>
+            <?php if ($showAlbumArtist || (!$showArtist)) { ?>
+                <li id="sb_home_browse_music_artist"><a href="<?php echo $web_path; ?>/browse.php?action=album_artist"><?php echo $t_a_artists; ?></a></li>
             <?php } ?>
             <?php if ($allowLabel) { ?>
                 <li id="sb_home_browse_music_label"><a href="<?php echo $web_path; ?>/browse.php?action=label"><?php echo $t_labels ?></a></li>
