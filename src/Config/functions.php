a13bc7c4a5e48bfcdd66d3e03673f083b3f70a20
lachlan <lachlan.00@gmail.com>
2022-04-28 13:57:43 +1000

    date_default_timezone_get()
    get server timezone for get_datetime

--- src/Config/functions.php
+++ src/Config/functions.php
@@ -450,7 +450,7 @@ function get_datetime($time, $date_format = 'short', $time_format = 'short', $ov
 
     // get your locale and set the date based on that, unless you have 'custom_datetime set'
     $locale = AmpConfig::get('lang', 'en_US');
-    $format = new IntlDateFormatter($locale, $date_type, $time_type, null, null, $pattern);
+    $format = new IntlDateFormatter($locale, $date_type, $time_type, date_default_timezone_get(), null, $pattern);
 
     return $format->format($time);
 }
