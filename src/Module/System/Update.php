4f86e96e96bb3b29a094855afa94df58b62935d6
troycarpenter <troy@carpenter.cx>
2022-06-13 14:06:12 +0000

    Changed to Update.php per review.

--- src/Module/System/Update.php
+++ src/Module/System/Update.php
@@ -753,7 +753,7 @@ class Update
         $version[]     = array('version' => '540002', 'description' => $update_string);
 
         $update_string = "* Add new tables to support catalog access feature<br />* Update user profiles to support catalog access feature<br />* Add all catalogs to DEFAULT group on upgrade<br />* Remove obsolete user_catalog table<br><br>NOTE: If you have private catalogs configured, you will need to create a special catalog filter for that user.";
-        $version[]     = array('version' => '540003', 'description' => $update_string);
+        $version[]     = array('version' => '550001', 'description' => $update_string);
 
         return $version;
     }
@@ -4488,66 +4488,12 @@ class Update
         return $retval;
     }
 
-    /** update_54003
+    /** update_550001
      *
      * Add new tables and modify others to support catalog access feature
      *
      */
-    public static function update_540003(): bool
-    {
-        $retval     = true;
-        $collation  = (AmpConfig::get('database_collation', 'utf8mb4_unicode_ci'));
-        $charset    = (AmpConfig::get('database_charset', 'utf8mb4'));
-        $engine     = ($charset == 'utf8mb4') ? 'InnoDB' : 'MYISAM';
-
-        // Add the new catalog_access_group table
-        $sql = "CREATE TABLE IF NOT EXISTS `catalog_access_group` (`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,`name` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,PRIMARY KEY (`id`),UNIQUE KEY `name` (`name`)) ENGINE=$engine DEFAULT CHARSET=$charset COLLATE=$collation;";
-        $retval &= (Dba::write($sql) !== false);
-
-        // Add the new default_catalog_access_group table to prevent deletion of the default group
-        $sql = "CREATE TABLE `default_catalog_access_group` (`default_catalog_access_group_id` int(11) UNSIGNED PRIMARY KEY,FOREIGN KEY (default_catalog_access_group_id) references catalog_access_group (id));";
-        $retval &= (Dba::write($sql) !== false);
-
-        // Add the new catalog_access table
-        $sql = "CREATE TABLE IF NOT EXISTS `catalog_access` (`access_group_id` int(11) UNSIGNED NOT NULL,`catalog_id` int(11) UNSIGNED NOT NULL,`enabled` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,FOREIGN KEY (`access_group_id`) REFERENCES catalog_access_group(`id`) ON DELETE CASCADE, FOREIGN KEY (`catalog_id`) REFERENCES catalog(`id`) ON DELETE CASCADE) ENGINE=$engine DEFAULT CHARSET=$charset COLLATE=$collation;";
-        $retval &= (Dba::write($sql) !== false);
-
-        // Add the default group
-        $sql = "INSERT INTO `catalog_access_group` (`id`, `name`) VALUES (1, 'DEFAULT');";
-        $retval &= (Dba::write($sql) !== false);
-
-        // Add the default group into the protecdtion table
-        $sql = "INSERT INTO `default_catalog_access_group` (`default_catalog_access_group_id`) VALUES (1);";
-        $retval &= (Dba::write($sql) !== false);
-
-        // Add the default access group to the user table and add TRIGGER to reset access to DEFAULT if current access group is deleted
-        $sql = "ALTER TABLE user ADD catalog_access_group INT(11) UNSIGNED NOT NULL DEFAULT 1;";
-        $retval &= (Dba::write($sql) !== false);
-        $sql = "CREATE TRIGGER default_user_catalog_access AFTER DELETE ON catalog_access_group FOR EACH ROW UPDATE user SET catalog_access_group=1 WHERE catalog_access_group = OLD.id;";
-        $retval &= (Dba::write($sql) !== false);
-
-        // Drop user_catalog table
-        $sql = "DROP TABLE IF EXISTS `user_catalog`;";
-        $retval &= (Dba::write($sql) !== false);
-
-        // Enable all catalogs in the DEFAULT profile initially.  Need to get a list of all catalog IDs and add them to the user_catalog_access table as enabled.
-        $sql        = "SELECT id FROM catalog;";
-        $db_results = Dba::read($sql);
-        while ($row = Dba::fetch_assoc($db_results)) {
-            $catalog = $row['id'];
-            $sql     = "INSERT INTO `catalog_access` (`access_group_id`, `catalog_id`, `enabled`) VALUES (1, $catalog, 1);";
-            $retval &= (Dba::write($sql) !== false);
-        }
-
-        return $retval;
-    }
-
-    /** update_530003
-     *
-     * Add new tables and modify others to support catalog access feature
-     *
-     */
-    public static function update_540003(): bool
+    public static function update_550001(): bool
     {
         $retval     = true;
         $collation  = (AmpConfig::get('database_collation', 'utf8mb4_unicode_ci'));
