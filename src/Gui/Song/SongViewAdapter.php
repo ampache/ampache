07201e8646c6b8f43c09fb2618c4a15b54767390
lachlan <lachlan.00@gmail.com>
2022-06-21 10:41:37 +1000

    Update SongViewAdapter.php

--- src/Gui/Song/SongViewAdapter.php
+++ src/Gui/Song/SongViewAdapter.php
@@ -349,12 +349,12 @@ final class SongViewAdapter implements SongViewAdapterInterface
         $songprops = [];
 
         $songprops[T_('Title')]         = scrub_out($this->song->title);
-        $songprops[T_('Track')]         = $this->song->track;
         $songprops[T_('Song Artist')]   = $this->song->get_f_artist_link();
         $songprops[T_('Album Artist')]  = $this->song->get_f_albumartist_link();
         $songprops[T_('Album')]         = $this->song->f_album_link;
         $songprops[T_('Composer')]      = scrub_out($this->song->composer);
         $songprops[T_('Genres')]        = $this->song->f_tags;
+        $songprops[T_('Track')]         = $this->song->track;
         $songprops[T_('Year')]          = $this->song->year;
         $songprops[T_('Original Year')] = scrub_out($this->song->get_album_original_year($this->song->album));
         $songprops[T_('Length')]        = scrub_out($this->song->f_time);
