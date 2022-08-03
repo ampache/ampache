20643892ea36f70b762c0e764bbe85f843f57f00
lachlan <lachlan.00@gmail.com>
2022-08-03 13:05:15 +1000

    catalog array should be ints

--- src/Module/Application/Admin/Catalog/ShowDeleteCatalogAction.php
+++ src/Module/Application/Admin/Catalog/ShowDeleteCatalogAction.php
@@ -54,7 +54,7 @@ final class ShowDeleteCatalogAction implements ApplicationActionInterface
         if ($gatekeeper->mayAccess(AccessLevelEnum::TYPE_INTERFACE, AccessLevelEnum::LEVEL_MANAGER) === false) {
             throw new AccessDeniedException();
         }
-        $catalogs = isset($_REQUEST['catalogs']) ? filter_var_array($_REQUEST['catalogs'], FILTER_SANITIZE_STRING) : array();
+        $catalogs = isset($_REQUEST['catalogs']) ? filter_var_array($_REQUEST['catalogs'], FILTER_SANITIZE_NUMBER_INT) : array();
 
         $this->ui->showHeader();
 
