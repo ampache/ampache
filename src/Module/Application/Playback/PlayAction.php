407f3b43b8419f0508e729f4c12a63491a7d2d6f
lachlan <lachlan.00@gmail.com>
2022-10-07 07:38:01 +1000

    check ssid for user
    surprised that this hasn't come up before

--- src/Module/Application/Playback/PlayAction.php
+++ src/Module/Application/Playback/PlayAction.php
@@ -248,11 +248,15 @@ final class PlayAction implements ApplicationActionInterface
                 $user_authenticated = true;
             }
         }
+        // try the session ID as well
+        if ($user == null) {
+            $user = User::get_from_username(Session::username($session_id));
+        }
 
         $session_name = AmpConfig::get('session_name');
         // Identify the user according to it's web session
         // We try to avoid the generic 'Ampache User' as much as possible
-        if (array_key_exists($session_name, $_COOKIE) && Session::exists('interface', $_COOKIE[$session_name])) {
+        if ($user == null && array_key_exists($session_name, $_COOKIE) && Session::exists('interface', $_COOKIE[$session_name])) {
             Session::check();
             $user = (array_key_exists('username', $_SESSION['userdata']))
                 ? User::get_from_username($_SESSION['userdata']['username'])
