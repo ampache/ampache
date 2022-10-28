16eb541e7ef58f040c75300c9ce40da498fd5cea
lachlan <lachlan.00@gmail.com>
2022-10-28 16:35:32 +1000

    in_array

--- src/Module/Api/Method/Api4/CatalogAction4Method.php
+++ src/Module/Api/Method/Api4/CatalogAction4Method.php
@@ -101,7 +101,7 @@ final class CatalogAction4Method
             } elseif ($catalog_media_type == 'podcast') {
                 Catalog::update_mapping('podcast');
                 Catalog::update_mapping('podcast_episode');
-            } elseif (!in_array($catalog_media_type, array('clip', 'tvshow', 'movie', 'personal_video'))) {
+            } elseif (in_array($catalog_media_type, array('clip', 'tvshow', 'movie', 'personal_video'))) {
                 Catalog::update_mapping('video');
             }
             Catalog::update_counts();
--- src/Module/Api/Method/CatalogActionMethod.php
+++ src/Module/Api/Method/CatalogActionMethod.php
@@ -102,7 +102,7 @@ final class CatalogActionMethod
             } elseif ($catalog_media_type == 'podcast') {
                 Catalog::update_mapping('podcast');
                 Catalog::update_mapping('podcast_episode');
-            } elseif (!in_array($catalog_media_type, array('clip', 'tvshow', 'movie', 'personal_video'))) {
+            } elseif (in_array($catalog_media_type, array('clip', 'tvshow', 'movie', 'personal_video'))) {
                 Catalog::update_mapping('video');
             }
             Catalog::update_counts();
