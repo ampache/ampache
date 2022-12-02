288366d15f74b89792fd9cfc64a1067565a56a7b
lachlan <lachlan.00@gmail.com>
2022-12-02 14:13:16 +1000

    system pref's can't be applied to all
    they only exist for the -1 (System) user

--- public/templates/show_preference_box.inc.php
+++ public/templates/show_preference_box.inc.php
@@ -29,8 +29,8 @@ use Ampache\Module\Util\UiInterface;
 
 /** @var UiInterface $ui */
 /** @var array<string, mixed> $preferences */
-
-$is_admin = (Access::check('interface', 100) && (array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'admin')) ?>
+print_r($preferences);
+$is_admin = ($preferences['title'] !== 'System' && (Access::check('interface', 100) && (array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'admin'))) ?>
 <h4><?php echo T_($preferences['title']); ?></h4>
 <table class="tabledata striped-rows">
 <colgroup>
