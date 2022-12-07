ca01386fc19b5edee10501df55bda583e3daa8a6
lachlan <lachlan.00@gmail.com>
2022-12-07 10:25:58 +1000

    Update Playlist5Method.php

--- src/Module/Api/Method/Api5/Playlist5Method.php
+++ src/Module/Api/Method/Api5/Playlist5Method.php
@@ -68,7 +68,7 @@ final class Playlist5Method
 
             return false;
         }
-        if (!$playlist->type == 'public' && (!$playlist->has_access($user) && !Access::check('interface', 100, $user))) {
+        if (!$playlist->type == 'public' && (!$playlist->has_access($user->id) && !Access::check('interface', 100, $user->id))) {
             Api5::error(T_('Require: 100'), '4742', self::ACTION, 'account', $input['api_format']);
 
             return false;
