7c76500eb12f15e2cd634193d0a02da71965cb88
lachlan <lachlan.00@gmail.com>
2022-12-07 10:26:12 +1000

    Update PlaylistMethod.php

--- src/Module/Api/Method/PlaylistMethod.php
+++ src/Module/Api/Method/PlaylistMethod.php
@@ -69,7 +69,7 @@ final class PlaylistMethod
 
             return false;
         }
-        if (!$playlist->type == 'public' && (!$playlist->has_access($user) && !Access::check('interface', 100, $user))) {
+        if (!$playlist->type == 'public' && (!$playlist->has_access($user->id) && !Access::check('interface', 100, $user->id))) {
             Api::error(T_('Require: 100'), '4742', self::ACTION, 'account', $input['api_format']);
 
             return false;
