d046883766cf24712b84ccd55f959b42e55b10eb
lachlan <lachlan.00@gmail.com>
2022-12-20 11:22:39 +1000

    use the unchanged size/bitrate for songs
    transcoded values will be a separate element in api6

--- src/Module/Api/Json_Data.php
+++ src/Module/Api/Json_Data.php
@@ -927,6 +927,8 @@ class Json_Data
             $user_rating = $rating->get_user_rating($user->getId());
             $flag        = new Userflag($song_id, 'song');
             $art_url     = Art::url($song->album, 'album', $_REQUEST['auth']);
+            $songMime    = $song->mime;
+            $songBitrate = $song->bitrate;
             $play_url    = $song->play_url('', 'api', false, $user->id);
             $playlist_track++;
 
@@ -953,10 +955,10 @@ class Json_Data
             $ourArray['playlisttrack']         = $playlist_track;
             $ourArray['time']                  = (int)$song->time;
             $ourArray['year']                  = (int)$song->year;
-            $ourArray['bitrate']               = (int)$song->bitrate;
+            $ourArray['bitrate']               = (int)$songBitrate;
             $ourArray['rate']                  = (int)$song->rate;
             $ourArray['mode']                  = $song->mode;
-            $ourArray['mime']                  = $song->mime;
+            $ourArray['mime']                  = $songMime;
             $ourArray['url']                   = $play_url;
             $ourArray['size']                  = (int)$song->size;
             $ourArray['mbid']                  = $song->mbid;
@@ -1076,6 +1078,7 @@ class Json_Data
             $rating      = new Rating($song->id, 'song');
             $user_rating = $rating->get_user_rating($user->getId());
             $art_url     = Art::url($song->album, 'album', $_REQUEST['auth']);
+            $songMime    = $song->mime;
             $play_url    = $song->play_url('', 'api', false, $user->id);
 
             array_push($JSON, array(
@@ -1086,7 +1089,7 @@ class Json_Data
                 "genre" => self::genre_array($song->tags),
                 "track" => (int) $song->track,
                 "time" => (int) $song->time,
-                "mime" => $song->mime,
+                "mime" => $songMime,
                 "url" => $play_url,
                 "size" => (int) $song->size,
                 "art" => $art_url,
