f55061ca81af2fde68496cbc321d1cb2b5497eec
lachlan <lachlan.00@gmail.com>
2023-01-04 13:44:48 +1000

    filter action to just special_chars

--- public/templates/show_alphabet_form.inc.php
+++ public/templates/show_alphabet_form.inc.php
@@ -26,5 +26,5 @@ use Ampache\Config\AmpConfig;
 <form style="display:inline;" name="f" method="get" action="<?php echo AmpConfig::get('web_path') . "/$action"; ?>" enctype="multipart/form-data">
     <label for="match" accesskey="S"><?php echo $text; ?></label>
     <input type="text" id="match" name="match" value="<?php echo $match; ?>" />
-    <input type="hidden" name="action" value="<?php echo scrub_out(filter_input(INPUT_GET, 'action', FILTER_SANITIZE_URL)); ?>">
+    <input type="hidden" name="action" value="<?php echo scrub_out(filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS)); ?>">
 </form>
--- public/templates/show_browse_form.inc.php
+++ public/templates/show_browse_form.inc.php
@@ -28,7 +28,7 @@ global $dic;
 
 $videoRepository = $dic->get(VideoRepositoryInterface::class);
 $web_path        = AmpConfig::get('web_path');
-$filter_str      = (string) filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_NO_ENCODE_QUOTES);
+$filter_str      = (string) filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
 $showAlbumArtist = AmpConfig::get('show_album_artist');
 $showArtist      = AmpConfig::get('show_artist'); ?>
 
