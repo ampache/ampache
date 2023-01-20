6ba50868c00886bf1bf266ba780ab17dc80cc206
lachlan <lachlan.00@gmail.com>
2023-01-20 11:32:13 +1000

    Update Catalog.php

--- src/Repository/Model/Catalog.php
+++ src/Repository/Model/Catalog.php
@@ -1106,10 +1106,10 @@ abstract class Catalog extends database_object
                 $join = 'AND';
             }
             if ($user_id == -1) {
-                $sql .= "$join `id` IN (SELECT `catalog_id` FROM `catalog_filter_group_map` WHERE `enabled` = 1 AND `group_id` = 0)";
+                $sql .= "$join `id` IN (SELECT `catalog_id` FROM `catalog_filter_group_map` WHERE `enabled` = 1 AND `group_id` = 0) ";
             }
         }
-        $sql .= "ORDER BY `name`";
+        $sql .= "ORDER BY `name`;";
         //debug_event(self::class, 'get_catalogs ' . $sql, 5);
         $db_results = Dba::read($sql, $params);
         $results    = array();
