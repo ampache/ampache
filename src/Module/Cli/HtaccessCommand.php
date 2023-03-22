d5652381e357c7441510bc53ecddc48819112c0c
lachlan <lachlan.00@gmail.com>
2023-03-22 14:05:16 +1000

    Add command to recreate htaccess files using bin/installer

---
+++ src/Module/Cli/HtaccessCommand.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * vim:set softtabstop=4 shiftwidth=4 expandtab:
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright 2001 - 2022 Ampache.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Ampache\Module\Cli;
+
+use Ahc\Cli\Input\Command;
+use Ampache\Config\AmpConfig;
+use Ampache\Config\ConfigContainerInterface;
+use Ampache\Module\System\AmpError;
+use Ampache\Module\System\InstallationHelperInterface;
+
+final class HtaccessCommand extends Command
+{
+    private ConfigContainerInterface $configContainer;
+
+    private InstallationHelperInterface $installationHelper;
+
+    public function __construct(
+        ConfigContainerInterface $configContainer,
+        InstallationHelperInterface $installationHelper
+    ) {
+        parent::__construct('htaccess', T_('Create .htaccess files'));
+
+        $this->configContainer = $configContainer;
+
+        $this
+            ->option('-e|--execute', T_('Execute the update'), 'boolval', false)
+            ->usage('<bold>  htaccess -e</end> <comment> ## ' . T_('Recreate Ampache .htaccess files') . '</end><eol/>');
+        $this->installationHelper = $installationHelper;
+    }
+
+    public function execute(): void
+    {
+        $interactor = $this->io();
+        $execute    = $this->values()['execute'] === true;
+
+        // Make sure we have all the required information
+        if (!$execute) {
+            $this->showHelp();
+
+            return;
+        }
+        $htaccess_play_file = __DIR__ . '/../../../public/play/.htaccess';
+        $htaccess_rest_file = __DIR__ . '/../../../public/rest/.htaccess';
+
+        // create the files
+        if (!$this->installationHelper->install_rewrite_rules($htaccess_play_file, $this->configContainer->get('web_path'), false)) {
+            $interactor->error(
+                T_('Failed to write config file') . ": " . $htaccess_play_file,
+                true
+            );
+            $interactor->error(
+                AmpError::get('general'),
+                true
+            );
+
+            return;
+        }
+        if (!$this->installationHelper->install_rewrite_rules($htaccess_rest_file, $this->configContainer->get('web_path'), false)) {
+            $interactor->error(
+                T_('Failed to write config file') . ": " . $htaccess_rest_file,
+                true
+            );
+            $interactor->error(
+                AmpError::get('general'),
+                true
+            );
+
+            return;
+        }
+        // successfully created htaccess files
+        $interactor->white(T_('Success'), true);
+    }
+}
