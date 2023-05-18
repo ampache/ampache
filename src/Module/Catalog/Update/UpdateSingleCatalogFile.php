9b72720db40098315ccce708581e7c8668207467
lachlan <lachlan.00@gmail.com>
2023-05-18 11:43:06 +1000

    catalog doesn't need to be there

--- src/Module/Catalog/Update/UpdateSingleCatalogFile.php
+++ src/Module/Catalog/Update/UpdateSingleCatalogFile.php
@@ -110,8 +110,7 @@ final class UpdateSingleCatalogFile extends AbstractCatalogUpdater implements Up
                 );
                 if ($media->id && $verificationMode == 1) {
                     // Verify Existing files
-                    $catalog = $media->catalog;
-                    $info    = Catalog::update_media_from_tags($media);
+                    $info = Catalog::update_media_from_tags($media);
                     if (array_key_exists('element', $info) && is_array($info['element']) && !empty($info['element'])) {
                         // update counts after adding/verifying
                         Album::update_table_counts();
