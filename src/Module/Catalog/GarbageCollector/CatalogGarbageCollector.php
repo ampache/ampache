7da0d18f52b154ff3cda1e6f9b7239874691683d
lachlan <lachlan.00@gmail.com>
2023-06-29 17:48:08 +1000

    Update CatalogGarbageCollector.php

--- src/Module/Catalog/GarbageCollector/CatalogGarbageCollector.php
+++ src/Module/Catalog/GarbageCollector/CatalogGarbageCollector.php
@@ -27,6 +27,7 @@ use Ampache\Module\Util\Recommendation;
 use Ampache\Repository\Model\Art;
 use Ampache\Repository\Model\Artist;
 use Ampache\Repository\Model\Catalog;
+use Ampache\Repository\Model\database_object;
 use Ampache\Repository\Model\Label;
 use Ampache\Repository\Model\Metadata\Repository\Metadata;
 use Ampache\Repository\Model\Metadata\Repository\MetadataField;
