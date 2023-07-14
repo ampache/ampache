ef61b692994752663f0c2177ce0473e0d76ee91d
lachlan <lachlan.00@gmail.com>
2023-07-14 14:15:11 +1000

    Update InitializationHandlerConfig.php
    add squashed to repo

--- src/Config/Init/InitializationHandlerConfig.php
+++ src/Config/Init/InitializationHandlerConfig.php
@@ -36,7 +36,7 @@ final class InitializationHandlerConfig implements InitializationHandlerInterfac
     private const CONFIG_VERSION = '67';
 
     public const CONFIG_FILE_PATH = __DIR__ . '/../../../config/ampache.cfg.php';
-    public const STRUCTURE        = 'public';  // Project release is using either the public html folder or squashed structure
+    public const STRUCTURE        = 'squashed';  // Project release is using either the public html folder or squashed structure
 
     private EnvironmentInterface $environment;
 
