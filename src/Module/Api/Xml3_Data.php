ad88fbb5e005277cde12896d00608085e1a14f26
lachlan <lachlan.00@gmail.com>
2023-07-27 12:54:48 +1000

    remove superfluous auth on image urls

--- src/Module/Api/Xml3_Data.php
+++ src/Module/Api/Xml3_Data.php
@@ -336,7 +336,7 @@ class Xml3_Data
             $tag_string  = self::tags_string($artist->tags);
 
             // Build the Art URL, include session
-            $art_url = AmpConfig::get('web_path') . '/image.php?object_id=' . $artist_id . '&object_type=artist&auth=' . scrub_out($_REQUEST['auth']);
+            $art_url = AmpConfig::get('web_path') . '/image.php?object_id=' . $artist_id . '&object_type=artist';
 
             // Handle includes
             if (in_array("albums", $include)) {
@@ -392,7 +392,7 @@ class Xml3_Data
             $user_rating = $rating->get_user_rating($user->getId());
 
             // Build the Art URL, include session
-            $art_url = AmpConfig::get('web_path') . '/image.php?object_id=' . $album->id . '&object_type=album&auth=' . scrub_out($_REQUEST['auth']);
+            $art_url = AmpConfig::get('web_path') . '/image.php?object_id=' . $album->id . '&object_type=album';
 
             $string .= "<album id=\"" . $album->id . "\">\n\t<name><![CDATA[" . $album->name . "]]></name>\n";
 
