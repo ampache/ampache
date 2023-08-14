6568ffd3dc41d397eff7c21bceaee5020a01d90b
lachlan <lachlan.00@gmail.com>
2023-08-15 09:31:32 +1000

    i think this should always look

--- src/Module/System/AutoUpdate.php
+++ src/Module/System/AutoUpdate.php
@@ -253,7 +253,7 @@ class AutoUpdate
      */
     public static function is_update_available($force = false)
     {
-        if (!$force && (!self::lastcheck_expired() || !AmpConfig::get('autoupdate'))) {
+        if (!$force && (!AmpConfig::get('autoupdate'))) {
             return AmpConfig::get('autoupdate_lastversion_new');
         }
         $time = time();
