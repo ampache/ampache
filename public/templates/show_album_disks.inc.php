1b00bfc1a103f45ef8b41bf93f2eb477f06a1c77
lachlan <lachlan.00@gmail.com>
2023-10-13 11:15:29 +1000

    enable album sort links again when split by group
    i THINK this is okay now

--- public/templates/show_album_disks.inc.php
+++ public/templates/show_album_disks.inc.php
@@ -65,13 +65,12 @@ $cel_album   = ($is_table) ? "cel_album" : 'grid_album';
 $cel_artist  = ($is_table) ? "cel_artist" : 'grid_artist';
 $cel_tags    = ($is_table) ? "cel_tags" : 'grid_tags';
 $cel_counter = ($is_table) ? "cel_counter" : 'grid_counter';
-// TODO: enable album sort links again when you can work out how to fix it
-$album_link  = ($group_release) ? $album_text : Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=name', $album_text, 'album_disk_sort_name');
-$artist_link = ($group_release) ? $artist_text :  Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=album_artist', $artist_text, 'album_disk_sort_artist');
-$songs_link  = ($group_release) ? $songs_text :  Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=song_count', $songs_text, 'album_disk_sort_song_count');
-$year_link   = ($group_release) ? $year_text :  Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . $year_sort, $year_text, 'album_disk_sort_year');
-$count_link  = ($group_release) ? $count_text :  Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=total_count', $count_text, 'album_disk_sort_total_count');
-$rating_link = ($group_release) ? $rating_text :  Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=rating', $rating_text, 'album_disk_sort_rating');
+$album_link  = Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=name', $album_text, 'album_disk_sort_name');
+$artist_link = Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=album_artist', $artist_text, 'album_disk_sort_artist');
+$songs_link  = Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=song_count', $songs_text, 'album_disk_sort_song_count');
+$year_link   = Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . $year_sort, $year_text, 'album_disk_sort_year');
+$count_link  = Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=total_count', $count_text, 'album_disk_sort_total_count');
+$rating_link = Ajax::text('?page=browse&action=set_sort&browse_id=' . $browse->id . '&sort=rating', $rating_text, 'album_disk_sort_rating');
 
 if ($browse->is_show_header()) {
     require Ui::find_template('list_header.inc.php');
