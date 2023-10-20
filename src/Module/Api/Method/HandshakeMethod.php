4bb8fa5b6436b5483c48dfef9b148a4ae508ddad
lachlan <lachlan.00@gmail.com>
2023-10-20 15:33:42 +1000

    Update HandshakeMethod.php
    not from the request

--- src/Module/Api/Method/HandshakeMethod.php
+++ src/Module/Api/Method/HandshakeMethod.php
@@ -69,7 +69,7 @@ final class HandshakeMethod
         if (empty($passphrase)) {
             $passphrase = Core::get_post('auth');
         }
-        $username = trim((string) ($input['user'] ?? Session::username($_REQUEST['auth'])));
+        $username = trim((string) ($input['user'] ?? Session::username($passphrase)));
         $user_ip  = Core::get_user_ip();
         // set the version to the old string for old api clients
         $version      = (isset($input['version'])) ? $input['version'] : Api::$version;
