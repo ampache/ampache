2b5e8d06ba2f6a72d46184e198455805b9421434
lachlan <lachlan.00@gmail.com>
2023-11-23 13:20:50 +1000

    Update phpstan-baseline.neon

--- src/Repository/Model/Song.php
+++ src/Repository/Model/Song.php
@@ -2195,7 +2195,7 @@ class Song extends database_object implements Media, library_item, GarbageCollec
         }
 
         $media_name = $this->get_stream_name() . "." . $this->type;
-        $media_name = preg_replace("/[^a-zA-Z0-9\. ]+/", "-", $media_name);
+        $media_name = (string)preg_replace("/[^a-zA-Z0-9\. ]+/", "-", $media_name);
         $media_name = (AmpConfig::get('stream_beautiful_url'))
             ? urlencode($media_name)
             : rawurlencode($media_name);
