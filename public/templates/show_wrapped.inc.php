9b1da84f224c107816c53e3093fc7126d55a08c9
lachlan <lachlan.00@gmail.com>
2023-12-13 17:13:29 +1000

    Update show_wrapped.inc.php
    no grid for songs yet, we should put album art there though to make that happen

--- public/templates/show_wrapped.inc.php
+++ public/templates/show_wrapped.inc.php
@@ -69,7 +69,6 @@ $browse     = new Browse();
 $browse->set_type('song');
 $browse->set_use_filters(false);
 $browse->set_show_header(false);
-$browse->set_grid_view(false, false);
 $browse->set_mashup(true);
 $browse->show_objects($object_ids);
 Ui::show_box_bottom();
