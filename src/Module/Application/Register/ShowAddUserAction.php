1b00e77af7fbfdf1ebda29fb2e612282c83852a4
Daniel Jakob <github@usox.org>
2023-12-22 10:46:53 +0100

    Use a dedicated renderer to show the registration agreement
    And some tests :)

--- src/Module/Application/Register/ShowAddUserAction.php
+++ src/Module/Application/Register/ShowAddUserAction.php
@@ -30,8 +30,9 @@ use Ampache\Config\ConfigurationKeyEnum;
 use Ampache\Module\Application\ApplicationActionInterface;
 use Ampache\Module\Application\Exception\AccessDeniedException;
 use Ampache\Module\Authorization\GuiGatekeeperInterface;
+use Ampache\Module\User\Registration\RegistrationAgreementRendererInterface;
 use Ampache\Module\Util\Mailer;
-use Ampache\Module\Util\Ui;
+use Ampache\Module\Util\UiInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -41,10 +42,18 @@ final class ShowAddUserAction implements ApplicationActionInterface
 
     private ConfigContainerInterface $configContainer;
 
+    private RegistrationAgreementRendererInterface $registrationAgreementRenderer;
+
+    private UiInterface $ui;
+
     public function __construct(
-        ConfigContainerInterface $configContainer
+        ConfigContainerInterface $configContainer,
+        RegistrationAgreementRendererInterface $registrationAgreementRenderer,
+        UiInterface $ui
     ) {
-        $this->configContainer = $configContainer;
+        $this->configContainer               = $configContainer;
+        $this->registrationAgreementRenderer = $registrationAgreementRenderer;
+        $this->ui                            = $ui;
     }
 
     /**
@@ -78,7 +87,13 @@ final class ShowAddUserAction implements ApplicationActionInterface
             );
             require_once __DIR__ . '/../../Util/Captcha/init.php';
         }
-        require_once Ui::find_template('show_user_registration.inc.php');
+
+        $this->ui->show(
+            'show_user_registration.inc.php',
+            [
+                'registrationAgreementRenderer' => $this->registrationAgreementRenderer,
+            ]
+        );
 
         return null;
     }
