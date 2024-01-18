106d84f3724b92bd195c086f28964dbcd27602d6
lachlan <lachlan.00@gmail.com>
2024-01-18 12:57:30 +1000

    More user->isnew checks

--- src/Module/Playback/Stream.php
+++ src/Module/Playback/Stream.php
@@ -639,11 +639,17 @@ class Stream
         while ($row = Dba::fetch_assoc($db_results)) {
             $className = ObjectTypeToClassNameMapper::map($row['object_type']);
             /** @var Song|Video $media */
-            $media     = new $className($row['object_id']);
+            $media = new $className($row['object_id']);
+            if ($media->isNew()) {
+                continue;
+            }
             if (($user_id === 0 || (int)$row['user'] == $user_id) && Catalog::has_access($media->getCatalogId(), (int)$row['user'])) {
                 $client = new User($row['user']);
-                $media->format();
                 $client->format();
+                if ($client->isNew()) {
+                    continue;
+                }
+                $media->format();
                 $results[] = array(
                     'media' => $media,
                     'client' => $client,
