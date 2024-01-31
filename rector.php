5d0773ae3eb69409501494e575bedc1d68f818ac
Daniel Jakob <github@usox.org>
2024-01-31 16:30:29 +0100

    Update rector to 0.19
    Also adapt to the new fluent config scheme

--- rector.php
+++ rector.php
@@ -10,27 +10,22 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->importNames();
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
+    ])
+    ->withImportNames()
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class
+    ])
+    ->withSets([
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
         SetList::DEAD_CODE,
         LevelSetList::UP_TO_PHP_74,
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         FlipTypeControlToUseExclusiveTypeRector::class,
         StaticClosureRector::class,
         SymplifyQuoteEscapeRector::class,
     ]);
-};
