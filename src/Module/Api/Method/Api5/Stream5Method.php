e89483c9b01c1c23694436ebfe5bd2c2ed660de9
lachlan <lachlan.00@gmail.com>
2024-02-08 19:09:35 +1000

    stream methods using the user_update input

--- src/Module/Api/Method/Api5/Stream5Method.php
+++ src/Module/Api/Method/Api5/Stream5Method.php
@@ -64,7 +64,7 @@ final class Stream5Method
         $type      = (string) $input['type'];
         $object_id = (int) $input['id'];
 
-        $maxBitRate    = (int)($input['maxBitRate'] ?? 0);
+        $maxBitRate    = (int)($input['bitrate'] ?? 0);
         $format        = $input['format'] ?? null; // mp3, flv or raw
         $transcode_to  = $format && $format != 'raw';
         $timeOffset    = $input['offset'] ?? null;
