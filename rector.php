15c203cb44c8f17f2a9e556de17d01ec0e973242
Daniel Jakob <github@usox.org>
2024-02-09 14:59:32 +0100

    Upgrade rector to its first major version

--- rector.php
+++ rector.php
@@ -7,8 +7,6 @@ use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -18,12 +16,8 @@ return RectorConfig::configure()
     ->withRules([
         InlineConstructorDefaultToPropertyRector::class
     ])
-    ->withSets([
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_74,
-    ])
+    ->withPhpSets(php74: true)
+    ->withPreparedSets(deadCode: true, codeQuality: true, codingStyle: true)
     ->withSkip([
         FlipTypeControlToUseExclusiveTypeRector::class,
         StaticClosureRector::class,
