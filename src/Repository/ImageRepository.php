dd30926d780cb288919cc560e30e83d52284dd9d
Daniel Jakob <github@usox.org>
2024-02-12 20:26:07 +0100

    Add ImageRepository

---
+++ src/Repository/ImageRepository.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * vim:set softtabstop=3 shiftwidth=4 expandtab:
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright Ampache.org, 2001-2023
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Ampache\Repository;
+
+use Ampache\Module\Database\DatabaseConnectionInterface;
+use Generator;
+use PDO;
+
+/**
+ * Manages database access related to images (`Art`)
+ *
+ * Tables: `image``,
+ */
+final class ImageRepository implements ImageRepositoryInterface
+{
+    private DatabaseConnectionInterface $connection;
+
+    public function __construct(
+        DatabaseConnectionInterface $connection
+    ) {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Get the object details for the art table
+     */
+    public function getRawImage(
+        int $objectId,
+        string $objectType,
+        string $size,
+        string $mimeType
+    ): ?string {
+        $result = $this->connection->fetchOne(
+            'SELECT `image` FROM `image` WHERE `object_id` = ? AND `object_type` = ? AND `size` = ? AND `mime` = ?',
+            [
+                $objectId,
+                $objectType,
+                $size,
+                $mimeType
+            ]
+        );
+
+        if ($result === false) {
+            return null;
+        }
+
+        return (string) $result;
+    }
+
+    /**
+     * Get the object details for the art table
+     *
+     * @return Generator<array{id: int, object_id: int, object_type: string, size: string, mime: string}>
+     */
+    public function findAllHavingImage(): Generator
+    {
+        $result = $this->connection->query(
+            'SELECT `id`, `object_id`, `object_type`, `size`, `mime` FROM `image` WHERE `image` IS NOT NULL',
+        );
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            yield [
+                'id' => (int) $row['id'],
+                'object_id' => (int) $row['object_id'],
+                'object_type' => $row['object_type'],
+                'size' => (string) $row['size'],
+                'mime' => (string) $row['mime'],
+            ];
+        }
+    }
+
+    /**
+     * Clear the image column (if you have the image on disk)
+     */
+    public function deleteImage(int $imageId): void
+    {
+        $this->connection->query(
+            'UPDATE `image` SET `image` = NULL WHERE `id` = ?',
+            [
+                $imageId
+            ]
+        );
+    }
+}
---
+++ tests/Repository/ImageRepositoryTest.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * vim:set softtabstop=3 shiftwidth=4 expandtab:
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright Ampache.org, 2001-2023
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Ampache\Repository;
+
+use Ampache\Module\Database\DatabaseConnectionInterface;
+use PDO;
+use PDOStatement;
+use PHPUnit\Framework\TestCase;
+
+class ImageRepositoryTest extends TestCase
+{
+    private DatabaseConnectionInterface $connection;
+
+    private ImageRepository $subject;
+
+    protected function setUp(): void
+    {
+        $this->connection = $this->createMock(DatabaseConnectionInterface::class);
+
+        $this->subject = new ImageRepository(
+            $this->connection,
+        );
+    }
+
+    public function testGetRawImageReturnsNullIfNotExisting(): void
+    {
+        $objectId   = 666;
+        $objectType = 'some-type';
+        $size       = 'some-size';
+        $mimeType   = 'some-mimetype';
+
+        $this->connection->expects(static::once())
+            ->method('fetchOne')
+            ->with(
+                'SELECT `image` FROM `image` WHERE `object_id` = ? AND `object_type` = ? AND `size` = ? AND `mime` = ?',
+                [
+                    $objectId,
+                    $objectType,
+                    $size,
+                    $mimeType
+                ]
+            )
+            ->willReturn(false);
+
+        static::assertNull(
+            $this->subject->getRawImage($objectId, $objectType, $size, $mimeType)
+        );
+    }
+
+    public function testGetRawImageReturnsImage(): void
+    {
+        $objectId   = 666;
+        $objectType = 'some-type';
+        $size       = 'some-size';
+        $mimeType   = 'some-mimetype';
+        $result     = 'some-result';
+
+        $this->connection->expects(static::once())
+            ->method('fetchOne')
+            ->with(
+                'SELECT `image` FROM `image` WHERE `object_id` = ? AND `object_type` = ? AND `size` = ? AND `mime` = ?',
+                [
+                    $objectId,
+                    $objectType,
+                    $size,
+                    $mimeType
+                ]
+            )
+            ->willReturn($result);
+
+        static::assertSame(
+            $result,
+            $this->subject->getRawImage($objectId, $objectType, $size, $mimeType)
+        );
+    }
+
+    public function testFindAllHavingImage(): void
+    {
+        $result = $this->createMock(PDOStatement::class);
+
+        $rowId      = 666;
+        $objectId   = 42;
+        $objectType = 'some-type';
+        $size       = 'some-size';
+        $mimeType   = 'some-mimetype';
+
+        $this->connection->expects(static::once())
+            ->method('query')
+            ->with('SELECT `id`, `object_id`, `object_type`, `size`, `mime` FROM `image` WHERE `image` IS NOT NULL')
+            ->willReturn($result);
+
+        $result->expects(static::exactly(2))
+            ->method('fetch')
+            ->with(PDO::FETCH_ASSOC)
+            ->willReturn(
+                [
+                    'id' => (string) $rowId,
+                    'object_id' => (string) $objectId,
+                    'object_type' => $objectType,
+                    'size' => $size,
+                    'mime' => $mimeType,
+                ],
+                false
+            );
+
+        static::assertSame(
+            [[
+                'id' => $rowId,
+                'object_id' => $objectId,
+                'object_type' => $objectType,
+                'size' => $size,
+                'mime' => $mimeType,
+            ]],
+            iterator_to_array($this->subject->findAllHavingImage())
+        );
+    }
+
+    public function testDeleteImageDeletes(): void
+    {
+        $imageId = 666;
+
+        $this->connection->expects(static::once())
+            ->method('query')
+            ->with(
+                'UPDATE `image` SET `image` = NULL WHERE `id` = ?',
+                [
+                    $imageId
+                ]
+            );
+
+        $this->subject->deleteImage($imageId);
+    }
+}
