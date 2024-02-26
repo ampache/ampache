ampache/ampache

commit 0d1ece0866a0ad510a349028e09b330eddbee02d

?: => ??

src/Module/Util/Ui.php
@@ -275,7 +275,7 @@ class Ui implements UiInterface
             $name       = $name[0];
         }
 
-        $title    = $title ?: T_(ucfirst($name));
+        $title    = $title ?? T_(ucfirst($name));
         $icon_url = self::_find_icon($name);
         $icontype = pathinfo($icon_url, PATHINFO_EXTENSION);
         if (isset($hover_name)) {
@@ -300,7 +300,7 @@ class Ui implements UiInterface
                 $svgicon->addAttribute('id', $id_attrib);
             }
 
-            $class_attrib = ($class_attrib) ?: 'icon icon-' . $name;
+            $class_attrib = ($class_attrib) ?? 'icon icon-' . $name;
             $svgicon->addAttribute('class', $class_attrib);
 
             $tag = explode("\n", $svgicon->asXML(), 2)[1];
@@ -371,7 +371,7 @@ class Ui implements UiInterface
             $name       = $name[0];
         }
 
-        $title = $title ?: ucfirst($name);
+        $title = $title ?? ucfirst($name);
 
         $image_url = self::_find_image($name);
         $imagetype = pathinfo($image_url, PATHINFO_EXTENSION);
@@ -399,7 +399,7 @@ class Ui implements UiInterface
                 $svgimage->addAttribute('id', $id_attrib);
             }
 
-            $class_attrib = ($class_attrib) ?: 'image image-' . $name;
+            $class_attrib = ($class_attrib) ?? 'image image-' . $name;
             $svgimage->addAttribute('class', $class_attrib);
 
             $tag = explode("\n", $svgimage->asXML(), 2)[1];
@@ -554,7 +554,7 @@ class Ui implements UiInterface
             echo "<style>#loginPage #headerlogo, #registerPage #headerlogo { background-image: url('" . AmpConfig::get('custom_login_logo') . "') !important; }</style>";
         }
 
-        $favicon = AmpConfig::get('custom_favicon', false) ?: AmpConfig::get('web_path') . "/favicon.ico";
+        $favicon = AmpConfig::get('custom_favicon', false) ?? AmpConfig::get('web_path') . "/favicon.ico";
         echo "<link rel='shortcut icon' href='" . $favicon . "' />\n";
     }
 

public/templates/show_album_group_disks.inc.php
@@ -47,7 +47,7 @@ $web_path = AmpConfig::get('web_path');
 $f_album_name = $album->get_artist_fullname();
 $f_name       = $album->get_fullname(false, true);
 $title        = ($album->album_artist > 0)
-    ? scrub_out($f_name) . '&nbsp;-&nbsp;' . (($album->get_f_artist_link()) ?: '')
+    ? scrub_out($f_name) . '&nbsp;-&nbsp;' . (($album->get_f_artist_link()) ?? '')
     : scrub_out($f_name);
 
 $current_user         = Core::get_global('user');

public/templates/show_box_top.inc.php
@@ -20,7 +20,7 @@
  *
  */
 
-$class = $class ?: 'box'; ?>
+$class = $class ?? 'box'; ?>
 
 <div class="<?php echo $class; ?>">
   <div class="box-inside">

public/templates/show_ip_history.inc.php
@@ -58,10 +58,10 @@ use Ampache\Module\Util\Ui;
 <?php foreach ($history as $data) { ?>
 <tr>
     <td class="cel_date">
-        <?php echo get_datetime((int) $data['date']); ?>
+        <?php echo get_datetime((int)($data['date'] ?? 0)); ?>
     </td>
     <td class="cel_ipaddress">
-        <?php echo (inet_ntop($data['ip'])) ?: T_('Invalid'); ?>
+        <?php echo(inet_ntop($data['ip'])) ?? T_('Invalid'); ?>
     </td>
 </tr>
 <?php } ?>

public/templates/show_recently_played.inc.php
@@ -77,7 +77,7 @@ foreach ($data as $row) {
     if ($is_allowed_recent) {
         // add the time if you've allowed it
         if ($is_allowed_time) {
-            $interval = (int) (time() - $row['date']);
+            $interval = (int) (time() - ($row['date'] ?? 0));
 
             if ($interval < 60) {
                 $time_string = sprintf(nT_('%d second ago', '%d seconds ago', $interval), $interval);

public/templates/show_recently_skipped.inc.php
@@ -76,7 +76,7 @@ foreach ($data as $row) {
     if ($is_allowed_recent) {
         // add the time if you've allowed it
         if ($is_allowed_time) {
-            $interval = (int) (time() - $row['date']);
+            $interval = (int) (time() - ($row['date'] ?? 0));
 
             if ($interval < 60) {
                 $time_string = sprintf(nT_('%d second ago', '%d seconds ago', $interval), $interval);

public/templates/show_recently_skipped.inc_user.php
@@ -74,7 +74,7 @@ foreach ($data as $row) {
     if ($is_allowed_recent) {
         // add the time if you've allowed it
         if ($is_allowed_time) {
-            $interval = (int) (time() - $row['date']);
+            $interval = (int) (time() - ($row['date'] ?? 0));
 
             if ($interval < 60) {
                 $time_string = sprintf(nT_('%d second ago', '%d seconds ago', $interval), $interval);

public/templates/show_rules.inc.php
@@ -37,7 +37,7 @@ if (isset($playlist)) {
 }
 $logic_operator = strtolower($logic_operator); ?>
 <script src="<?php echo AmpConfig::get('web_path'); ?>/lib/javascript/search.js"></script>
-<script src="<?php echo AmpConfig::get('web_path'); ?>/lib/javascript/search-data.php?type=<?php echo $currentType ?: 'song'; ?>"></script>
+<script src="<?php echo AmpConfig::get('web_path'); ?>/lib/javascript/search-data.php?type=<?php echo $currentType ?? 'song'; ?>"></script>
 
 <?php Ui::show_box_top(T_('Rules') . "...", 'box box_rules'); ?>
 <table class="tabledata">

commit 0dc023906d3e992d1aee35d093259422061335ed

Really show default favicon by default
Since the default value of the `custom_favicon` is an empty string and
the use of the the default value `false`, the result of
`AmpConfig::get('custom_favicon', false)` will never be unset.

Using the `??` operator always returns the `custom_favicon` setting,
even if its NULL or an empty string. This causes the default favicon to
never being shown even in the absence of the `custon_favicon` setting.

This issue has been introduced in commit 0d1ece0866 .

Change `??` to `?:` to use the value of the `custom_favicon` setting
only if it a non-empty string (`false`, `NULL` and `""` would evaluate
to false in the test).

src/Module/Util/Ui.php
@@ -529,7 +529,7 @@ class Ui implements UiInterface
             echo "<style>#loginPage #headerlogo, #registerPage #headerlogo { background-image: url('" . AmpConfig::get('custom_login_logo') . "') !important; }</style>";
         }
 
-        $favicon = AmpConfig::get('custom_favicon', false) ?? AmpConfig::get('web_path') . "/favicon.ico";
+        $favicon = AmpConfig::get('custom_favicon', false) ?: AmpConfig::get('web_path') . "/favicon.ico";
         echo "<link rel='shortcut icon' href='" . $favicon . "' />\n";
     }
 