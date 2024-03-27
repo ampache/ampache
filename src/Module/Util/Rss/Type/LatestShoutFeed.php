cbb5f6baed9ee30091d483ab4779436766c8d30e
lachlan <lachlan.00@gmail.com>
2024-03-27 13:24:49 +1000

    softtabstop=4

--- src/Module/Util/Rss/Type/LatestShoutFeed.php
+++ src/Module/Util/Rss/Type/LatestShoutFeed.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * vim:set softtabstop=3 shiftwidth=4 expandtab:
+ * vim:set softtabstop=4 shiftwidth=4 expandtab:
  *
  * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
  * Copyright Ampache.org, 2001-2023
