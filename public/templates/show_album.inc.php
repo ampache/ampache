1cdc390662506409df939b4f2fe63eee8de0ccef
lachlan <lachlan.00@gmail.com>
2024-04-08 13:46:30 +1000

    global may not be set after loading form other pages
    Adding art raised an error

--- public/templates/show_album.inc.php
+++ public/templates/show_album.inc.php
@@ -35,6 +35,7 @@ use Ampache\Module\Util\Rss\Type\RssFeedTypeEnum;
 use Ampache\Module\Util\Ui;
 use Ampache\Module\Util\Upload;
 use Ampache\Module\Util\ZipHandlerInterface;
+use Ampache\Module\System\Core;
 use Ampache\Repository\Model\Album;
 use Ampache\Repository\Model\Art;
 use Ampache\Repository\Model\Browse;
@@ -49,11 +50,12 @@ global $dic;
 
 /** @var bool $isAlbumEditable */
 /** @var ContainerInterface $dic */
-/** @var User $current_user */
+/** @var User|null $current_user */
 
-$zipHandler = $dic->get(ZipHandlerInterface::class);
-$batch_dl   = Access::check_function(AccessFunctionEnum::FUNCTION_BATCH_DOWNLOAD);
-$zip_album  = $batch_dl && $zipHandler->isZipable('album');
+$current_user = Core::get_global('user');
+$zipHandler   = $dic->get(ZipHandlerInterface::class);
+$batch_dl     = Access::check_function(AccessFunctionEnum::FUNCTION_BATCH_DOWNLOAD);
+$zip_album    = $batch_dl && $zipHandler->isZipable('album');
 // Title for this album
 $web_path = (string)AmpConfig::get('web_path', '');
 
@@ -196,7 +198,7 @@ if (Access::check(AccessTypeEnum::INTERFACE, AccessLevelEnum::USER)) {
             </li>
             <?php }
     }
-if (($owner_id > 0 && $owner_id == $current_user->getId()) || Access::check(AccessTypeEnum::INTERFACE, AccessLevelEnum::CONTENT_MANAGER)) {
+if (($owner_id > 0 && $owner_id == $current_user?->getId()) || Access::check(AccessTypeEnum::INTERFACE, AccessLevelEnum::CONTENT_MANAGER)) {
     $saveorder = T_('Save Track Order');
     if (AmpConfig::get('statistical_graphs') && is_dir(__DIR__ . '/../../vendor/szymach/c-pchart/src/Chart/')) { ?>
             <li>
