0dda1a1522b512a81cf933d8ab7f2e1d231de231
lachlan <lachlan.00@gmail.com>
2024-04-17 11:56:38 +1000

    add an error icon fallback

--- src/Module/Util/Ui.php
+++ src/Module/Util/Ui.php
@@ -415,7 +415,9 @@ class Ui implements UiInterface
             $filesearch = glob(__DIR__ . '/../../../public/' . $path . 'icon_' . $name . '.{svg,png}', GLOB_BRACE);
         }
 
-        $filename = pathinfo($filesearch[0], PATHINFO_BASENAME);
+        $filename = (is_array($filesearch))
+            ? pathinfo($filesearch[0], PATHINFO_BASENAME)
+            : '/images/icon_error.svg'; // fall back to error icon if icon is missing
         $url      = AmpConfig::get('web_path') . $path . $filename;
         // cache the url so you don't need to keep searching
         self::$_icon_cache[$name] = $url;
