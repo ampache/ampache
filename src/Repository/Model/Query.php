b5ba552ecae3a9955eef3c9445b15d484636e211
lachlan <lachlan.00@gmail.com>
2024-05-09 11:10:04 +1000

    missing type from smartlist sort

--- src/Repository/Model/Query.php
+++ src/Repository/Model/Query.php
@@ -279,29 +279,30 @@ class Query
             'date',
             'last_update',
             'name',
+            'rating',
             'type',
             'user',
             'username',
-            'rating',
             'user_flag'
         ),
         'playlist_search' => array(
             'date',
             'last_update',
             'name',
+            'rating',
             'type',
             'user',
             'username',
-            'rating',
             'user_flag'
         ),
         'smartplaylist' => array(
             'date',
             'last_update',
             'name',
+            'rating',
+            'type',
             'user',
             'username',
-            'rating',
             'user_flag'
         ),
         'shoutbox' => array(
@@ -2418,8 +2419,8 @@ class Query
                 switch ($field) {
                     case 'date':
                     case 'last_update':
-                    case 'type':
                     case 'name':
+                    case 'type':
                     case 'user':
                     case 'username':
                         $sql = "`search`.`$field`";
