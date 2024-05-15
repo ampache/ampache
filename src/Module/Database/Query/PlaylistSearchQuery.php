fb738e5457af73e66ac811be092838a25859b26d
lachlan <lachlan.00@gmail.com>
2024-05-15 12:18:13 +1000

    add to the other playlist queries

--- src/Module/Database/Query/PlaylistSearchQuery.php
+++ src/Module/Database/Query/PlaylistSearchQuery.php
@@ -35,6 +35,7 @@ final class PlaylistSearchQuery implements QueryInterface
         'alpha_match',
         'exact_match',
         'playlist_type',
+        'playlist_user',
         'regex_match',
         'regex_not_match',
         'smartlist',
@@ -125,6 +126,9 @@ final class PlaylistSearchQuery implements QueryInterface
                 break;
             case 'smartlist':
                 $filter_sql = " `playlist`.`id` LIKE 'smart_%' AND ";
+                break;
+            case 'playlist_user':
+                $filter_sql = " `playlist`.`user` = " . (int)$value . " AND ";
                 break;
             case 'playlist_type':
                 $user_id = (!empty(Core::get_global('user')) && Core::get_global('user')->id > 0)
