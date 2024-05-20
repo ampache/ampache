4c4d9bc8cc3d6ff039287a6a84be17d1b2f4f95b
lachlan <lachlan.00@gmail.com>
2024-05-20 17:54:13 +1000

    cs:fix

--- src/Module/Api/Method/Api5/Playlists5Method.php
+++ src/Module/Api/Method/Api5/Playlists5Method.php
@@ -71,7 +71,7 @@ final class Playlists5Method
             $browse->set_type('playlist');
         }
 
-        $browse->set_sort('name','ASC');
+        $browse->set_sort('name', 'ASC');
 
         $method = (array_key_exists('exact', $input) && (int)$input['exact'] == 1) ? 'exact_match' : 'alpha_match';
         $browse->set_api_filter($method, $input['filter'] ?? '');
