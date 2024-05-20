59ef8ff1d0705f6f61d6ac75766e5030808ee77e
fufroma <fufroma@users.noreply.github.com>
2024-05-21 00:21:52 +0200

    Fix  #3954 - Add indexes to object_count (#3955)

--- src/Module/System/Update/Versions.php
+++ src/Module/System/Update/Versions.php
@@ -277,6 +277,7 @@ final class Versions
         600068 => Migration\V6\Migration600068::class,
         600069 => Migration\V6\Migration600069::class,
         600070 => Migration\V6\Migration600070::class,
+        600071 => Migration\V6\Migration600071::class,
     ];
 
     /**
