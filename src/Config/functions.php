1611883e0261e599bfa0e050c9ba367249a220bb
lachlan <lachlan.00@gmail.com>
2024-05-27 11:12:36 +1000

    Revert "Rework of get_themes() to let PHP handle most of the work"
    This reverts commit c41c943164032aad51bba64e59df02a72bb4c26e.

--- src/Config/functions.php
+++ src/Config/functions.php
@@ -1191,21 +1191,25 @@ function nT_($original, $plural, $value): string
  */
 function get_themes(): array
 {
-    $lst_files = glob(__DIR__ . '/../../public/themes/*/theme.cfg.php');
+    /* Open the themes dir and start reading it */
+    $handle = opendir(__DIR__ . '/../../public/themes');
 
-    $results=array();
-    foreach ($lst_files as $file){
-        debug_event('themes', "Checking $file", 5);
-        $cfg = get_theme($file);
-        if (is_null($cfg)) {
-            debug_event('themes', "Warning: them $file is empty", 5);
-            continue;
-	}
+    if (!is_resource($handle)) {
+        debug_event('themes', 'Failed to open /themes directory', 2);
 
-        $theme = basename(dirname($file)); // Get last dirname (name of the theme)
-        $results[$theme]=$cfg;
+        return array();
     }
 
+    $results = array();
+    while (($file = readdir($handle)) !== false) {
+        if ((string) $file !== '.' && (string) $file !== '..') {
+            debug_event('themes', "Checking $file", 5);
+            $cfg = get_theme($file);
+            if ($cfg !== null) {
+                $results[$cfg['name']] = $cfg;
+            }
+        }
+    } // end while directory
     // Sort by the theme name
     ksort($results);
 
