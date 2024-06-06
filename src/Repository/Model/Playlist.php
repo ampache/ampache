3d3c7a97ed0cd7500b1648243d21081174f2c0b4
lachlan <lachlan.00@gmail.com>
2024-06-06 13:02:36 +1000

    put params on debug where available

--- src/Repository/Model/Playlist.php
+++ src/Repository/Model/Playlist.php
@@ -153,7 +153,7 @@ class Playlist extends playlist_object
             }
         }
         $sql .= "ORDER BY `name`";
-        //debug_event(self::class, 'get_playlists query: ' . $sql, 5);
+        //debug_event(self::class, 'get_playlists query: ' . $sql . ' ' . print_r($params, true), 5);
 
         $db_results = Dba::read($sql, $params);
         $results    = array();
@@ -193,7 +193,7 @@ class Playlist extends playlist_object
             $params[] = $user_id;
         }
         $sql .= "ORDER BY `name`";
-        //debug_event(self::class, 'get_playlists query: ' . $sql, 5);
+        //debug_event(self::class, 'get_playlists query: ' . $sql . ' ' . print_r($params, true), 5);
 
         $db_results = Dba::read($sql, $params);
         $results    = array();
@@ -363,7 +363,7 @@ class Playlist extends playlist_object
         }
         $sql .= "ORDER BY `playlist_data`.`track`";
         $db_results = Dba::read($sql, $params);
-        // debug_event(__CLASS__, "get_songs(): " . $sql, 5);
+        // debug_event(__CLASS__, "get_songs(): " . $sql . ' ' . print_r($params, true), 5);
 
         while ($row = Dba::fetch_assoc($db_results)) {
             $results[] = (int)$row['object_id'];
