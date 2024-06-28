115e570deb527f37aed29dc86c4793c032f0016f
lachlan <lachlan.00@gmail.com>
2024-06-28 14:39:54 +1000

    copyright 2024

--- public/themes/reborn/theme.cfg.php
+++ public/themes/reborn/theme.cfg.php
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;
 ;<?php exit(); ?>;
 ;;;;;;;;;;;;;;;;;;
-; Copyright Ampache.org, 2001-2023
+; Copyright Ampache.org, 2001-2024
 ;
 ; This program is free software; you can redistribute it and/or
 ; modify it under the terms of the GNU General Public License v2
