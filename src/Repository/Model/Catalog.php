55241b0aa77f57a8e9d7f0499012aedfcba07ff3
lachlan <lachlan.00@gmail.com>
2024-07-01 15:23:07 +1000

    just use the list tables instead of the map

--- src/Repository/Model/Catalog.php
+++ src/Repository/Model/Catalog.php
@@ -3243,25 +3243,10 @@ abstract class Catalog extends database_object
         self::set_update_info('time', $time);
         self::set_update_info('size', $size);
 
-        // count Artists
-        $sql        = "SELECT COUNT(DISTINCT(`artist_id`)) FROM `artist_map`;";
-        $db_results = Dba::read($sql);
-        $row        = Dba::fetch_row($db_results);
-        self::set_update_info('artist', (int)($row[0] ?? 0));
-
-        // count Albums
-        $sql        = "SELECT COUNT(DISTINCT(`album_id`)) FROM `album_map`;";
-        $db_results = Dba::read($sql);
-        $row        = Dba::fetch_row($db_results);
-        self::set_update_info('album', (int)($row[0] ?? 0));
-
-        // album_disk counts
-        $sql        = "SELECT COUNT(DISTINCT `album_disk`.`id`) AS `count` FROM `album_disk` LEFT JOIN `album` ON `album_disk`.`album_id` = `album`.`id` LEFT JOIN `catalog` ON `catalog`.`id` = `album`.`catalog` LEFT JOIN `artist_map` ON `artist_map`.`object_id` = `album`.`id` WHERE `artist_map`.`object_type` = 'album' AND `catalog`.`enabled` = '1';";
-        $db_results = Dba::read($sql);
-        $row        = Dba::fetch_row($db_results);
-        self::set_update_info('album_disk', (int)($row[0] ?? 0));
-
         $list_tables = [
+            'artist',
+            'album',
+            'album_disk',
             'search',
             'playlist',
             'live_stream',
