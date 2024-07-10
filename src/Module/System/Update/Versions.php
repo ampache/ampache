709429beaf72401f3027d4b50fa2a7fb0b78b5bb
lachlan <lachlan.00@gmail.com>
2024-07-10 11:38:27 +1000

    Migration700007
    Add a `collaborate` to playlist table to allow other users to add songs to the list

--- src/Module/System/Update/Versions.php
+++ src/Module/System/Update/Versions.php
@@ -284,6 +284,7 @@ final class Versions
         700004 => Migration\V7\Migration700004::class,
         700005 => Migration\V7\Migration700005::class,
         700006 => Migration\V7\Migration700006::class,
+        700007 => Migration\V7\Migration700007::class,
     ];
 
     /**
