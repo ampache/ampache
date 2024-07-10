5474515cb1c2277d8de4adc00cbea78a23f24ba2
lachlan <lachlan.00@gmail.com>
2024-07-10 20:08:12 +1000

    Update Migration700009.php

--- src/Module/System/Update/Migration/V7/Migration700009.php
+++ src/Module/System/Update/Migration/V7/Migration700009.php
@@ -24,7 +24,6 @@ declare(strict_types=1);
 
 namespace Ampache\Module\System\Update\Migration\V7;
 
-use Ampache\Config\AmpConfig;
 use Ampache\Module\Authorization\AccessLevelEnum;
 use Ampache\Module\System\Update\Migration\AbstractMigration;
 
