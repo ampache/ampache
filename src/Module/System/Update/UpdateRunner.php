63a063cd9153e0d442813ff55dd43895d230c685
lachlan <lachlan.00@gmail.com>
2024-07-10 16:15:42 +1000

    add new downgrades to rollback

--- src/Module/System/Update/UpdateRunner.php
+++ src/Module/System/Update/UpdateRunner.php
@@ -86,6 +86,24 @@ final class UpdateRunner implements UpdateRunnerInterface
         // Prevent the script from timing out, which could be bad
         set_time_limit(0);
 
+        if ($currentVersion >= 700008) {
+            if (Dba::read('SELECT SUM(`user_id`) FROM `user_playlist_map`;')) {
+                // Migration700008
+                if (!Dba::write('DROP TABLE IF EXISTS `user_playlist_map`;')) {
+                    throw new UpdateFailedException();
+                }
+            }
+        }
+
+        if ($currentVersion >= 700007) {
+            if (Dba::read('SELECT SUM(`collaborate`) from `playlist`;')) {
+                // Migration700007
+                if (!Dba::write("ALTER TABLE `playlist` DROP COLUMN `collaborate`;")) {
+                    throw new UpdateFailedException();
+                }
+            }
+        }
+
         if ($currentVersion >= 700006) {
             // Migration700006
             if (!Preference::insert('home_recently_played_all', 'Show all media types in Recently Played', '1', 25, 'bool', 'interface', 'home', true)) {
