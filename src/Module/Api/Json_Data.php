fd5a7b786df9ab6725486703078221d33189e3b9
lachlan <lachlan.00@gmail.com>
2024-07-16 11:44:40 +1000

    add md5 to playlist objects

--- src/Module/Api/Json_Data.php
+++ src/Module/Api/Json_Data.php
@@ -920,8 +920,12 @@ class Json_Data
                         "playlisttrack" => $objects['track'],
                     ];
                 }
+
+                // hash the results
+                $md5 = md5(serialize($playlisttracks));
             } else {
                 $items = (int)($playitem_total ?? 0);
+                $md5   = null;
             }
 
             $rating          = new Rating($playlist->id, $object_type);
@@ -943,7 +947,8 @@ class Json_Data
                 "has_art" => $playlist->has_art(),
                 "flag" => (bool)$flag->get_flag($user->getId()),
                 "rating" => $user_rating,
-                "averagerating" => $rating->get_average_rating()
+                "averagerating" => $rating->get_average_rating(),
+                "md5" => $md5
             ];
         } // end foreach
 
