4ec9cefd0146c84f39f7f0166e51e431a0adc949
lachlan <lachlan.00@gmail.com>
2024-07-18 11:01:27 +1000

    video should not return count

--- src/Module/Api/Method/VideoMethod.php
+++ src/Module/Api/Method/VideoMethod.php
@@ -71,7 +71,7 @@ final class VideoMethod
                 echo Json_Data::videos(array($object_id), $user, true, false);
                 break;
             default:
-                echo Xml_Data::videos(array($object_id), $user);
+                echo Xml_Data::videos(array($object_id), $user, false);
         }
 
         return true;
