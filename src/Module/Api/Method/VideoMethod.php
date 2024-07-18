7726315617f0905bdaedfe438950e1d5b8125e3d
lachlan <lachlan.00@gmail.com>
2024-07-18 11:04:13 +1000

    Revert "video should not return count"
    This reverts commit 7535076862128f0d4b8146dc224e6effcba20a7c.

--- src/Module/Api/Method/VideoMethod.php
+++ src/Module/Api/Method/VideoMethod.php
@@ -71,7 +71,7 @@ final class VideoMethod
                 echo Json_Data::videos([$object_id], $user, true, false);
                 break;
             default:
-                echo Xml_Data::videos([$object_id], $user, false);
+                echo Xml_Data::videos([$object_id], $user);
         }
 
         return true;
