2106423ea9aa8b0b377541f8647cf8c90a72b267
lachlan <lachlan.00@gmail.com>
2024-07-25 15:52:46 +1000

    Update Search.php
    put waveform at the end

--- src/Repository/Model/Search.php
+++ src/Repository/Model/Search.php
@@ -733,11 +733,6 @@ class Search extends playlist_object
             $this->_add_type_select('license', T_('Music License'), 'boolean_numeric', $licenses, $t_file_data);
         }
 
-        // can't read the file from the db if it's stored on the disk
-        if (!AmpConfig::get('album_art_store_disk')) {
-            $this->_add_type_boolean('waveform', T_('Waveform'), 'boolean', $t_file_data);
-        }
-
         $this->_add_type_numeric('recent_added', T_('Recently added'), 'recent_added', $t_file_data);
         $this->_add_type_numeric('recent_updated', T_('Recently updated'), 'recent_updated', $t_file_data);
         $this->_add_type_boolean('possible_duplicate', T_('Possible Duplicate'), 'is_true', $t_file_data);
@@ -756,6 +751,11 @@ class Search extends playlist_object
 
         if ($catalogs !== []) {
             $this->_add_type_select('catalog', T_('Catalog'), 'boolean_numeric', $catalogs, $t_file_data);
+        }
+
+        // can't read the file from the db if it's stored on the disk
+        if (!AmpConfig::get('album_art_store_disk')) {
+            $this->_add_type_boolean('waveform', T_('Waveform'), 'boolean', $t_file_data);
         }
 
         $t_musicbrainz = T_('MusicBrainz');
