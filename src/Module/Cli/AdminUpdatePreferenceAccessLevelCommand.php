18e49198f9405f7dc8f53400cc2fad311a4d2b90
lachlan <lachlan.00@gmail.com>
2024-10-02 16:22:01 +1000

    add cli command admin:updatePreferenceAccessLevel
    force setting prefereces to a user level or reset to default to allow public sites to keep locked down

---
+++ src/Module/Cli/AdminUpdatePreferenceAccessLevelCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * vim:set softtabstop=4 shiftwidth=4 expandtab:
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright Ampache.org, 2001-2024
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Ampache\Module\Cli;
+
+use Ahc\Cli\Application;
+use Ahc\Cli\Input\Command;
+use Ahc\Cli\IO\Interactor;
+use Ampache\Repository\Model\Preference;
+
+final class AdminUpdatePreferenceAccessLevelCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct('admin:updatePreferenceAccessLevel', T_('Update Preference Access Level'));
+        $this
+            ->option(
+                '-l|--level',
+                T_('Access Level') . " ('default' | 'guest' | 'user' | 'content_manager' | 'manager' | 'admin')",
+                'strval',
+                'default'
+            )
+            ->usage('<bold>  admin:updatePreferenceAccessLevel --level admin</end> <comment> ## ' . T_('Update Preference Access Level') . '<eol/>');
+    }
+
+    public function execute(): void
+    {
+        /* @var Application|Interactor $interactor */
+        $interactor = $this->app()?->io();
+        if (!$interactor) {
+            return;
+        }
+        $level = $this->values()['level'] === false;
+
+        if (Preference::set_level($level)) {
+            $interactor->ok(
+                "\n" . T_('Updated'),
+                true
+            );
+        } else {
+            $interactor->error(
+                "\n" . T_('Error'),
+                true
+            );
+        }
+    }
+}
