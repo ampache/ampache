82ff80ad157a827bf7ab8dace637a00e5b52e701
lachlan <lachlan.00@gmail.com>
2024-12-12 16:46:05 +1000

    client public dir not needed

--- public/index.php
+++ public/index.php
@@ -5,23 +5,23 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ampache -- Debug Page</title>
-        <link rel="icon" href="./public/client/favicon.ico">
-        <link rel="stylesheet" href="./public/client/lib/components/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./public/client/lib/components/bootstrap/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="./public/client/templates/install.css" type="text/css" media="screen">
+        <link rel="icon" href="./client/favicon.ico">
+        <link rel="stylesheet" href="./client/lib/components/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./client/lib/components/bootstrap/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="./client/templates/install.css" type="text/css" media="screen">
     </head>
     <body>
         <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="container" style="height: 70px;">
                 <a class="navbar-brand" href="#">
-                    <img src="./public/client/images/ampache-dark.png" title="Ampache" alt="Ampache">
+                    <img src="./client/images/ampache-dark.png" title="Ampache" alt="Ampache">
                     Ampache :: For the Love of Music                </a>
             </div>
         </div>
         <div id="guts" class="container" role="main">
             <div class="jumbotron" style="margin-top: 70px">
                 <h1>Warning</h1>
-                <p>The Ampache website root has changed to <a href="./client" target="_blank">./public</a></p>
+                <p>The Ampache website root has changed to <a href="./client" target="_blank">./client</a></p>
             </div>
             <div class="alert alert-danger">
                 <p>This version of Ampache does not include anything in the web root to allow you to use your own client</p>
