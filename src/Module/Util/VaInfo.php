37d4026b16f60c2a1974e608fc184ed012bc5fb6
lachlan <lachlan.00@gmail.com>
2025-01-15 11:57:21 +1000

    ensure int on rating

--- src/Module/Util/VaInfo.php
+++ src/Module/Util/VaInfo.php
@@ -1271,7 +1271,7 @@ final class VaInfo implements VaInfoInterface
                         $rating_user = (int) $this->configContainer->get(ConfigurationKeyEnum::RATING_FILE_TAG_USER);
                     }
 
-                    $parsed['rating'][$rating_user] = floor($data[0] * 5 / 100);
+                    $parsed['rating'][$rating_user] = floor(((int)$data[0]) * 5 / 100);
                     break;
                 default:
                     // look for set ratings using email address
@@ -1284,7 +1284,7 @@ final class VaInfo implements VaInfoInterface
                             array_map('trim', $items)[1] ?? ''
                         );
                         if ($rating_user !== null) {
-                            $parsed['rating'][$rating_user->id] = floor($data[0] * 5 / 100);
+                            $parsed['rating'][$rating_user->id] = floor(((int)$data[0]) * 5 / 100);
                         }
                     }
                     $parsed[strtolower($tag)] = $data[0];
@@ -1534,7 +1534,7 @@ final class VaInfo implements VaInfoInterface
                 ) {
                     if ($user instanceof User) {
                         // Ratings are out of 255; scale it
-                        $parsed['rating'][$user->id] = $popm['rating'] / 255 * 5;
+                        $parsed['rating'][$user->id] = ((int)$popm['rating']) / 255 * 5;
                     }
                     continue;
                 }
@@ -1544,7 +1544,7 @@ final class VaInfo implements VaInfoInterface
                     $rating_user = (int) $this->configContainer->get(ConfigurationKeyEnum::RATING_FILE_TAG_USER);
                 }
 
-                $parsed['rating'][$rating_user] = $popm['rating'] / 255 * 5;
+                $parsed['rating'][$rating_user] = ((int)$popm['rating']) / 255 * 5;
             }
         }
 
