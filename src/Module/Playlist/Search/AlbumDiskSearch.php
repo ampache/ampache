62e5186e19d99c8fbb90e823d977668d4071825b
lachlan-00 <lachlan.00@gmail.com>
2025-01-21 16:22:18 +1000

    search rules missing parameter

--- src/Module/Playlist/Search/AlbumDiskSearch.php
+++ src/Module/Playlist/Search/AlbumDiskSearch.php
@@ -84,6 +84,7 @@ final class AlbumDiskSearch implements SearchInterface
                     } else {
                         $where[] = "`album`.`" . $rule[0] . "` $operator_sql ?";
                     }
+                    $parameters[] = $input;
                     break;
                 case 'catalog':
                 case 'year':
