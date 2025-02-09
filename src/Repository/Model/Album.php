59e3971d3ccb7adb768c456797a74465df61bb05
lachlan <lachlan.00@gmail.com>
2025-02-09 20:44:08 +1000

    f_year preoperties not used/used once
    useless memory hogs

--- src/Repository/Model/Album.php
+++ src/Repository/Model/Album.php
@@ -128,12 +128,6 @@ class Album extends database_object implements library_item, CatalogItemInterfac
     /** @var null|string $f_link */
     public $f_link;
 
-    /** @var null|string $f_year */
-    public $f_year;
-
-    /** @var null|string $f_year_link */
-    public $f_year_link;
-
     /** @var null|string $f_release_type */
     public $f_release_type;
 
@@ -515,14 +509,6 @@ class Album extends database_object implements library_item, CatalogItemInterfac
         $this->get_f_link();
         $this->get_artist_fullname();
         $this->get_f_parent_link();
-
-        if ($this->year === 0) {
-            $this->f_year = "N/A";
-        } else {
-            $web_path          = AmpConfig::get_web_path();
-            $year              = $this->year;
-            $this->f_year_link = sprintf('<a href="%s/search.php?type=album&action=search&limit=0rule_1=year&rule_1_operator=2&rule_1_input=', $web_path) . $year . "\">" . $year . "</a>";
-        }
     }
 
     /**
