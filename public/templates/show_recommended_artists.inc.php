81c84a98df0836ae4986eaf5d02419158ec57bba
lachlan <lachlan.00@gmail.com>
2025-02-10 17:28:18 +1000

    catalog_id is not valid for artists who can have many

--- public/templates/show_recommended_artists.inc.php
+++ public/templates/show_recommended_artists.inc.php
@@ -84,7 +84,7 @@ $directplay_limit     = AmpConfig::get('direct_play_limit');
 
 /* Foreach through every artist that has been passed to us */
 foreach ($object_ids as $artist_id) {
-    $libitem = new Artist($artist_id, $_SESSION['catalog']);
+    $libitem = new Artist($artist_id);
     if ($libitem->isNew()) {
         continue;
     }
