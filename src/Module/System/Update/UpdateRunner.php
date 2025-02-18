45c327849a738b3ea747cf02904ca07580569d86
lachlan <lachlan.00@gmail.com>
2025-02-18 10:05:52 +1000

    downgrade 721001

--- src/Module/System/Update/UpdateRunner.php
+++ src/Module/System/Update/UpdateRunner.php
@@ -86,6 +86,16 @@ final class UpdateRunner implements UpdateRunnerInterface
         // Prevent the script from timing out, which could be bad
         set_time_limit(0);
 
+
+        if ($currentVersion >= 721001) {
+            // Migration\V7\Migration721001
+            if (
+                !Preference::delete('show_playlist_media_parent')
+            ) {
+                throw new UpdateFailedException();
+            }
+        }
+
         if ($currentVersion >= 720001) {
             // Migration\V7\Migration720001
             if (
