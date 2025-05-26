ff29d8d81b53644e2a715964f82a7324a9d4c90b
lachlan <lachlan.00@gmail.com>
2025-05-26 16:58:25 +1000

    Update Art.php

--- src/Repository/Model/Art.php
+++ src/Repository/Model/Art.php
@@ -1178,7 +1178,7 @@ class Art extends database_object
                     : '';
             $url = AmpConfig::get_web_path() . '/image.php?' . $actionStr . 'object_id=' . $uid . '&object_type=' . scrub_out($type);
             if ($thumb !== null) {
-                $url .= '&thumb=' . $thumb;
+                $url .= '&thumb=' . $thumb; // @todo convert thumb links to size links after a period of timie to allow conversion of rules
             }
 
             if ($extension !== '' && $extension !== '0') {
