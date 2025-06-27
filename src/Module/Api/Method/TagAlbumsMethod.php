d32c49470478505601c7bf03df09c67f968aa75f
lachlan <lachlan.00@gmail.com>
2025-06-27 12:22:54 +1000

    add back in deprecated warnings for tag methods.
    this should help people when using blanket scripts

---
+++ src/Module/Api/Method/TagAlbumsMethod.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=0);
+
+/**
+ * vim:set softtabstop=4 shiftwidth=4 expandtab:
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright Ampache.org, 2001-2024
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Ampache\Module\Api\Method;
+
+use Ampache\Module\Api\Api;
+use Ampache\Module\Api\Exception\ErrorCodeEnum;
+use Ampache\Repository\Model\User;
+
+/**
+ * Class TagAlbumsMethod
+ */
+final class TagAlbumsMethod
+{
+    public const ACTION = 'tag_albums';
+
+    /**
+     * tag_albums
+     * MINIMUM_API_VERSION=380001
+     *
+     * This returns the albums associated with the genre in question
+     *
+     * @param array<string, mixed> $input
+     * @param User $user
+     */
+    public static function tag_albums(array $input, User $user): void
+    {
+        unset($user);
+        Api::error(T_('Deprecated'), ErrorCodeEnum::DEPRECATED, self::ACTION, 'removed', $input['api_format']);
+    }
+}
