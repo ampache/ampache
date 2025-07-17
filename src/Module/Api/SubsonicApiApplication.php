081622b6d7ba11564817111a3707823bd56f7fdd
lachlan <lachlan.00@gmail.com>
2025-07-17 19:31:41 +1000

    Update SubsonicApiApplication.php

--- src/Module/Api/SubsonicApiApplication.php
+++ src/Module/Api/SubsonicApiApplication.php
@@ -267,6 +267,64 @@ final class SubsonicApiApplication implements ApiApplicationInterface
             ? array_diff(get_class_methods(Subsonic_Api::class), Subsonic_Api::SYSTEM_LIST)
             : [];
 
+        // We do not use $_GET because of multiple parameters with the same name
+        $query_string = (string)($_SERVER['QUERY_STRING'] ?? '');
+        // Trick to avoid $HTTP_RAW_POST_DATA
+        $postdata = file_get_contents("php://input");
+        if (!empty($postdata)) {
+            $query_string .= '&' . $postdata;
+        }
+        $query = explode('&', $query_string);
+        $input = [];
+        foreach ($query as $param) {
+            $decname  = false;
+            $decvalue = false;
+            if (strpos((string)$param, '=')) {
+                [$name, $value] = explode('=', $param);
+                $decname        = urldecode($name);
+                $decvalue       = urldecode($value);
+            }
+            if ($decname && $decvalue !== false && $decvalue !== '') {
+                // workaround for clementine/Qt5 bug
+                // see https://github.com/clementine-player/Clementine/issues/6080
+                $matches = [];
+                if ($decname == "id" && preg_match('/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/', $decvalue, $matches)) {
+                    $calc = (
+                        (((int)$matches[1]) << 24) +
+                        (((int)$matches[2]) << 16) +
+                        (((int)$matches[3]) << 8) +
+                        ((int)$matches[4])
+                    );
+                    if ($calc) {
+                        $this->logger->notice(
+                            "Got id parameter $decvalue, which looks like an IP address. This is a known bug in some players, rewriting it to $calc",
+                            [LegacyLogger::CONTEXT_TYPE => self::class]
+                        );
+                        $decvalue = $calc;
+                    } else {
+                        $this->logger->warning(
+                            "Got id parameter $decvalue, which looks like an IP address. Recalculation of the correct id failed, though",
+                            [LegacyLogger::CONTEXT_TYPE => self::class]
+                        );
+                    }
+                }
+
+                if (array_key_exists($decname, $input)) {
+                    if (is_array($input[$decname]) === false) {
+                        $oldvalue          = $input[$decname];
+                        $input[$decname]   = [];
+                        $input[$decname][] = $oldvalue;
+                    }
+                    $input[$decname][] = $decvalue;
+                } else {
+                    $input[$decname] = $decvalue;
+                }
+            }
+        }
+
+        //$this->logger->debug(print_r($input, true), [LegacyLogger::CONTEXT_TYPE => self::class]);
+        //$this->logger->debug(print_r(apache_request_headers(), true), [LegacyLogger::CONTEXT_TYPE => self::class]);
+
         // Call your function if it's valid
         if (
             $os_methods !== [] &&
