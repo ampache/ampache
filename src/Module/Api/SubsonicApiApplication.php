d51132e1aceaca8a75867c93a5ed1592b8b7af3a
lachlan <lachlan.00@gmail.com>
2025-07-17 19:27:37 +1000

    post in values but not in initial auth
    bit annoying

--- src/Module/Api/SubsonicApiApplication.php
+++ src/Module/Api/SubsonicApiApplication.php
@@ -83,17 +83,18 @@ final class SubsonicApiApplication implements ApiApplicationInterface
         );
 
         $query = $request->getQueryParams();
+        $post  = (array)$request->getParsedBody();
 
         //$this->logger->debug(print_r($query, true), [LegacyLogger::CONTEXT_TYPE => self::class]);
         //$this->logger->debug(print_r(apache_request_headers(), true), [LegacyLogger::CONTEXT_TYPE => self::class]);
 
-        $action = strtolower($query['ssaction'] ?? '');
+        $action = strtolower($post['ssaction'] ?? $query['ssaction'] ?? '');
         // Compatibility reason
         if (empty($action)) {
-            $action = strtolower($query['action'] ?? '');
+            $action = strtolower($post['action'] ?? $query['action'] ?? '');
         }
 
-        $format = (string)($query['f'] ?? 'xml');
+        $format = (string)($post['f'] ?? $query['f'] ?? 'xml');
 
         // Set the correct default headers
         self::_setHeaders($action, $format, (string)AmpConfig::get('site_charset', 'UTF-8'));
@@ -114,20 +115,20 @@ final class SubsonicApiApplication implements ApiApplicationInterface
         $subsonic_legacy = AmpConfig::get('subsonic_legacy', true); // force this for the moment to always use subsonic
 
         // Authenticate the user with preemptive HTTP Basic authentication first
-        $userName = $query['PHP_AUTH_USER'] ?? '';
+        $userName = $post['PHP_AUTH_USER'] ?? $query['PHP_AUTH_USER'] ?? '';
         if (empty($userName)) {
-            $userName = $query['u'] ?? '';
+            $userName = $post['u'] ?? $query['u'] ?? '';
         }
-        $password = $query['PHP_AUTH_PW'] ?? '';
+        $password = $post['PHP_AUTH_PW'] ?? $query['PHP_AUTH_PW'] ?? '';
         if (empty($password)) {
-            $password = $query['p'] ?? '';
+            $password = $post['p'] ?? $query['p'] ?? '';
         }
 
 
-        $token     = $query['t'] ?? '';
-        $salt      = $query['s'] ?? '';
-        $version   = $query['v'] ?? '';
-        $clientapp = $query['c'] ?? '';
+        $token     = $post['t'] ?? $query['t'] ?? '';
+        $salt      = $post['s'] ?? $query['s'] ?? '';
+        $version   = $post['v'] ?? $query['v'] ?? '';
+        $clientapp = $post['c'] ?? $query['c'] ?? '';
 
         if (!isset($_SERVER['HTTP_USER_AGENT'])) {
             $_SERVER['HTTP_USER_AGENT'] = $clientapp;
@@ -265,64 +266,6 @@ final class SubsonicApiApplication implements ApiApplicationInterface
         $methods = ($subsonic_legacy)
             ? array_diff(get_class_methods(Subsonic_Api::class), Subsonic_Api::SYSTEM_LIST)
             : [];
-
-        // We do not use $_GET because of multiple parameters with the same name
-        $query_string = (string)($_SERVER['QUERY_STRING'] ?? '');
-        // Trick to avoid $HTTP_RAW_POST_DATA
-        $postdata = file_get_contents("php://input");
-        if (!empty($postdata)) {
-            $query_string .= '&' . $postdata;
-        }
-        $query = explode('&', $query_string);
-        $input = [];
-        foreach ($query as $param) {
-            $decname  = false;
-            $decvalue = false;
-            if (strpos((string)$param, '=')) {
-                [$name, $value] = explode('=', $param);
-                $decname        = urldecode($name);
-                $decvalue       = urldecode($value);
-            }
-            if ($decname && $decvalue !== false && $decvalue !== '') {
-                // workaround for clementine/Qt5 bug
-                // see https://github.com/clementine-player/Clementine/issues/6080
-                $matches = [];
-                if ($decname == "id" && preg_match('/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/', $decvalue, $matches)) {
-                    $calc = (
-                        (((int)$matches[1]) << 24) +
-                        (((int)$matches[2]) << 16) +
-                        (((int)$matches[3]) << 8) +
-                        ((int)$matches[4])
-                    );
-                    if ($calc) {
-                        $this->logger->notice(
-                            "Got id parameter $decvalue, which looks like an IP address. This is a known bug in some players, rewriting it to $calc",
-                            [LegacyLogger::CONTEXT_TYPE => self::class]
-                        );
-                        $decvalue = $calc;
-                    } else {
-                        $this->logger->warning(
-                            "Got id parameter $decvalue, which looks like an IP address. Recalculation of the correct id failed, though",
-                            [LegacyLogger::CONTEXT_TYPE => self::class]
-                        );
-                    }
-                }
-
-                if (array_key_exists($decname, $input)) {
-                    if (is_array($input[$decname]) === false) {
-                        $oldvalue          = $input[$decname];
-                        $input[$decname]   = [];
-                        $input[$decname][] = $oldvalue;
-                    }
-                    $input[$decname][] = $decvalue;
-                } else {
-                    $input[$decname] = $decvalue;
-                }
-            }
-        }
-
-        //$this->logger->debug(print_r($input, true), [LegacyLogger::CONTEXT_TYPE => self::class]);
-        //$this->logger->debug(print_r(apache_request_headers(), true), [LegacyLogger::CONTEXT_TYPE => self::class]);
 
         // Call your function if it's valid
         if (
