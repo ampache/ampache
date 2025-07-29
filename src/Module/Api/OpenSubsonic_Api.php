3168f7d0eddc81c7467a2fc3866158d57c762fdc
Alan <fufroma@gmail.com>
2025-07-29 18:44:42 +0200

    Fix share album on Subsonic API

--- src/Module/Api/OpenSubsonic_Api.php
+++ src/Module/Api/OpenSubsonic_Api.php
@@ -1329,11 +1329,13 @@ class OpenSubsonic_Api
             return;
         }
 
-        $object = self::getAmpacheObject((string)$sub_id);
-        if (!$object instanceof library_item) {
-            self::_errorOutput($input, self::SSERROR_DATA_NOTFOUND, __FUNCTION__);
+        if (!is_array($sub_id)) {
+            $object = self::getAmpacheObject((string)$sub_id);
+            if (!$object instanceof library_item) {
+                self::_errorOutput($input, self::SSERROR_DATA_NOTFOUND, __FUNCTION__);
 
-            return;
+                return;
+            }
         }
 
         $description = $input['description'] ?? null;
@@ -1342,13 +1344,18 @@ class OpenSubsonic_Api
             $expire_days  = (isset($input['expires']))
                 ? Share::get_expiry(((int)filter_var($input['expires'], FILTER_SANITIZE_NUMBER_INT)) / 1000)
                 : $share_expire;
-            $object_type = self::getAmpacheType($sub_id);
+            if (is_array($sub_id)) {
+                $object_type = self::getAmpacheType($sub_id[0]);
+            } else {
+                $object_type = self::getAmpacheType($sub_id);
+            }
             if (is_array($sub_id) && $object_type === 'song') {
                 debug_event(self::class, 'createShare: sharing song list (album)', 5);
                 $song_id     = self::getAmpacheId($sub_id[0]);
                 $tmp_song    = new Song($song_id);
                 $sub_id      = self::getAlbumSubId($tmp_song->album);
                 $object_type = 'album';
+                $object      = new Album($tmp_song->album);
             }
             debug_event(self::class, 'createShare: sharing ' . $object_type . ' ' . $sub_id, 4);
             if (
