02df33bb910e101a373be692d1e9637de2ceeafb
lachlan <lachlan.00@gmail.com>
2025-08-25 16:18:30 +1000

    Revert "keep the same sort on form links for genre pages"
    This reverts commit cfee5c32a7e975a111e4e373630577b568f923b1.

--- public/templates/show_form_genre.inc.php
+++ public/templates/show_form_genre.inc.php
@@ -11,11 +11,6 @@ global $dic;
 
 $web_path = AmpConfig::get_web_path();
 
-/** @var string $countOrder */
-$countString = ($countOrder === 'name')
-    ? '&sort=count'
-    : '';
-
 $videoRepository = $dic->get(VideoRepositoryInterface::class);
 $filter_str      = $type ?? (string) filter_input(INPUT_GET, 'type', FILTER_SANITIZE_SPECIAL_CHARS);
 $albumString     = (AmpConfig::get('album_group'))
@@ -23,22 +18,22 @@ $albumString     = (AmpConfig::get('album_group'))
     : 'album_disk'; ?>
 
 <div class="category_options">
-    <a class="category <?php echo ($filter_str == 'song') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=song<?php echo $countString; ?>">
+    <a class="category <?php echo ($filter_str == 'song') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=song">
         <?php echo T_('Songs'); ?>
     </a>
-    <a class="category <?php echo ($filter_str == 'album' || $filter_str == 'album_disk') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=<?php echo $albumString . $countString; ?>">
+    <a class="category <?php echo ($filter_str == 'album' || $filter_str == 'album_disk') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=<?php echo $albumString; ?>">
         <?php echo T_('Albums'); ?>
     </a>
-    <a class="category <?php echo ($filter_str == 'artist' || $filter_str == 'album_artist') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=artist<?php echo $countString; ?>">
+    <a class="category <?php echo ($filter_str == 'artist' || $filter_str == 'album_artist') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=artist">
         <?php echo T_('Artists'); ?>
     </a>
     <?php if (AmpConfig::get('allow_video') && $videoRepository->getItemCount()) { ?>
-        <a class="category <?php echo ($filter_str == 'video') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=video<?php echo $countString; ?>">
+        <a class="category <?php echo ($filter_str == 'video') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=video">
             <?php echo T_('Videos'); ?>
         </a>
     <?php } ?>
     <?php if (Access::check(AccessTypeEnum::INTERFACE, AccessLevelEnum::CONTENT_MANAGER) && Tag::get_merged_count() > 0) { ?>
-    <a class="category <?php echo ($filter_str == 'tag_hidden') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=tag_hidden<?php echo $countString; ?>">
+    <a class="category <?php echo ($filter_str == 'tag_hidden') ? 'current' : ''; ?>" href="<?php echo $web_path; ?>/browse.php?action=tag&type=tag_hidden">
         <?php echo T_('Hidden'); ?>
     </a>
     <?php } ?>
