a8b20c95b8302834f2fbea7b1f76c56e81e64f35
lachlan <lachlan.00@gmail.com>
2026-01-22 16:32:32 +1000

    Revert this ASAP as it's massive
    debug_level 6

--- src/Module/Util/VaInfo.php
+++ src/Module/Util/VaInfo.php
@@ -374,6 +374,8 @@ final class VaInfo implements VaInfoInterface
         }
 
         $this->_get_plugin_tags();
+        
+        debug_event(self::class, "this->tags: " . print_r($this->tags, true), 6); // TODO: remove THIS IS MASSIVE
     }
 
     /**
