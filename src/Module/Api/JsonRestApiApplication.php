94a538de059e3738c56eeaea1395b989a7db6b1f
lachlan <lachlan.00@gmail.com>
2026-05-11 19:42:28 +1000

    Update JsonRestApiApplication.php

--- src/Module/Api/JsonRestApiApplication.php
+++ src/Module/Api/JsonRestApiApplication.php
@@ -81,9 +81,9 @@ final class JsonRestApiApplication implements ApiApplicationInterface
         // normalize input actions (REST paths)
         $action = $this->apiHandler->normalizeAction((string)$input['action'], $type, isset($input['filter']));
         $action = match (strtoupper($request->getMethod())) {
-            'DELETE' => $type . '_delete',
-            'PATCH' => $type . '_edit',
-            'PUT' => $type . '_create',
+            'DELETE' => $action . '_delete',
+            'PATCH' => $action . '_edit',
+            'PUT' => $action . '_create',
             default => $action,
         };
 
@@ -92,7 +92,7 @@ final class JsonRestApiApplication implements ApiApplicationInterface
             'api_format' => 'json'
         ];
 
-        if ($type !== null) {
+        if ($type !== null && $type !== '') {
             $parameters['type'] = $type;
         }
 
