238c42a34ef4fd3213f23893f072a0903a3b2d3a
lachlan <lachlan.00@gmail.com>
2026-05-12 10:41:13 +1000

    Update PodcastSearch.php
    no join

--- src/Module/Playlist/Search/PodcastSearch.php
+++ src/Module/Playlist/Search/PodcastSearch.php
@@ -190,6 +190,9 @@ final class PodcastSearch implements SearchInterface
                         ? "LEFT JOIN (SELECT `object_id`, `object_type`, `user` FROM `user_flag` WHERE `user_flag`.`object_type` = '" . $my_type . "' AND `user_flag`.`user` = " . $search_user_id . " GROUP BY `object_id`, `object_type`, `user`) AS `my_flagged__" . $my_type . "_" . $search_user_id . "` ON `" . $my_type . "`.`$column` = `my_flagged__" . $my_type . "_" . $search_user_id . "`.`object_id` AND `my_flagged__" . $my_type . "_" . $search_user_id . "`.`object_type` = '" . $my_type . "'"
                         : "";
                     $where[] = "`my_flagged__" . $my_type . "_" . $search_user_id . "`.`object_id` $operator_sql";
+                    if ($my_type == 'podcast_episode') {
+                        $join['podcast_episode'] = true;
+                    }
                     break;
                 case 'played':
                     $where[]                 = "`podcast_episode`.`played` = '$operator_sql'";
