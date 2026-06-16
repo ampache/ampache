52eb700ebd22b0e23021f922f11f9f8d3334ce69
lachlan <lachlan.00@gmail.com>
2026-06-16 13:30:05 +1000

    add blank folder images, reduce folder main page

--- public/templates/show_folder.inc.php
+++ public/templates/show_folder.inc.php
@@ -75,7 +75,6 @@ if ($directplay_limit > 0) {
 <?php Ui::show_box_top($title, 'info-box'); ?>
 
 <div class="item_right_info">
-<?php Art::display('folder', $folder->id, scrub_out($f_name), ['width' => 384, 'height' => 384], null, true, false); ?>
 </div>
 <?php if (User::is_registered() && AmpConfig::get('ratings')) { ?>
     <span id="rating_<?php echo $folder->id; ?>_folder">
@@ -94,131 +93,7 @@ sprintf(nT_('%d time', '%d times', $folder->total_count), $folder->total_count);
 </div>
 <?php } ?>
 
-<?php
-$owner_id = $folder->get_user_owner();
-if (AmpConfig::get('sociable') && !empty($owner_id)) {
-    $owner = new User($owner_id); ?>
-<div class="item_uploaded_by">
-    <?php echo T_('Uploaded by'); ?> <?php echo $owner->get_f_link(); ?>
-</div>
-<?php
-} ?>
-
 <div id="information_actions">
-    <h3><?php echo T_('Actions'); ?></h3>
-    <ul>
-        <?php if ($show_direct_play) {
-            $play     = T_('Play');
-            $playnext = T_('Play next');
-            $playlast = T_('Play last'); ?>
-        <li>
-            <?php echo Ajax::button_with_text('?page=stream&action=directplay&object_type=folder&object_id=' . $folder->id, 'play_circle', $play, 'directplay_full_' . $folder->id); ?>
-        </li>
-            <?php if (Stream_Playlist::check_autoplay_next()) { ?>
-        <li>
-            <?php echo Ajax::button_with_text('?page=stream&action=directplay&object_type=folder&object_id=' . $folder->id . '&playnext=true', 'menu_open', $playnext, 'nextplay_folder_' . $folder->id); ?>
-        </li>
-            <?php } ?>
-            <?php if (Stream_Playlist::check_autoplay_append()) { ?>
-        <li>
-            <?php echo Ajax::button_with_text('?page=stream&action=directplay&object_type=folder&object_id=' . $folder->id . '&append=true', 'low_priority', $playlast, 'addplay_folder_' . $folder->id); ?>
-        </li>
-            <?php } ?>
-        <?php
-        } ?>
-
-        <?php if ($show_playlist_add) {
-            $addtotemp  = T_('Add to Temporary Playlist');
-            $randtotemp = T_('Random to Temporary Playlist');
-            $addtoexist = T_('Add to playlist'); ?>
-        <li>
-            <?php echo Ajax::button_with_text('?action=basket&type=folder&id=' . $folder->id, 'add_circle', $addtotemp, 'play_full_' . $folder->id); ?>
-        </li>
-        <li>
-            <?php echo Ajax::button_with_text('?action=basket&type=folder_random&id=' . $folder->id, 'shuffle', $randtotemp, 'play_random_' . $folder->id); ?>
-        </li>
-        <li>
-            <a id="<?php echo 'add_to_playlist_' . $folder->id; ?>" onclick="showPlaylistDialog(event, 'folder', '<?php echo $folder->id; ?>')">
-                <?php echo Ui::get_material_symbol('playlist_add', $addtoexist);
-            echo $addtoexist; ?>
-            </a>
-        </li>
-        <?php
-        } ?>
-        <?php if (AmpConfig::get('use_rss')) { ?>
-        <li>
-            <?php echo Ui::getRssLink(
-                RssFeedTypeEnum::LIBRARY_ITEM,
-                $current_user,
-                T_('RSS Feed'),
-                ['object_type' => 'folder', 'object_id' => (string)$folder->id]
-            ); ?>
-        </li>
-        <?php } ?>
-        <?php if (!AmpConfig::get('use_auth') || $access25) { ?>
-            <?php if (AmpConfig::get('sociable')) {
-                $postshout = "&nbsp;" . T_('Post Shout'); ?>
-            <li>
-                <a href="<?php echo $web_path; ?>/shout.php?action=show_add_shout&type=folder&id=<?php echo $folder->id; ?>">
-                    <?php echo Ui::get_material_symbol('comment', $postshout);
-                echo $postshout; ?>
-                </a>
-            </li>
-            <?php
-            } ?>
-        <?php } ?>
-    <?php if ($access25 && AmpConfig::get('share')) { ?>
-            <li>
-                <?php echo Share::display_ui('folder', $folder->id); ?>
-            </li>
-        <?php } else {
-            $link = "&nbsp;" . T_('Link'); ?>
-        <li>
-            <a href="<?php echo $folder->get_link(); ?>" target=_blank>
-                <?php echo Ui::get_material_symbol('open_in_new', $link);
-            echo $link; ?>
-            </a>
-        </li>
-    <?php } ?>
-        <?php if ((!empty($owner_id) && $owner_id == $current_user?->getId()) || $access50) {
-            if (AmpConfig::get('statistical_graphs') && is_dir(__DIR__ . '/../../vendor/szymach/c-pchart/src/Chart/')) { ?>
-            <li>
-                <a href="<?php echo $web_path; ?>/stats.php?action=graph&object_type=folder&object_id=<?php echo $folder->id; ?>">
-                    <?php echo Ui::get_material_symbol('bar_chart', T_('Graphs'));
-                echo T_('Graphs'); ?>
-                </a>
-            </li>
-        <?php } ?>
-        <li>
-            <a href="javascript:NavigateTo('<?php echo $web_path; ?>/folders.php?action=update_from_tags&folder=<?php echo $folder->id; ?>');" onclick="return confirm('<?php echo T_('Do you really want to update from tags?'); ?>');">
-                <?php echo Ui::get_material_symbol('sync_alt', T_('Update from tags'));
-            echo "&nbsp;" . T_('Update from tags'); ?>
-            </a>
-        </li>
-        <?php
-        } ?>
-        <?php
-            if ($zip_folder) {
-                $download = "&nbsp;" . T_('Download'); ?>
-        <li>
-            <a class="nohtml" href="<?php echo $web_path; ?>/batch.php?action=folder&id=<?php echo $folder->id; ?>" rel="nofollow">
-                <?php echo Ui::get_material_symbol('folder_zip', $download); ?>
-                <?php echo $download; ?>
-            </a>
-        </li>
-        <?php
-            } ?>
-        <?php if (Catalog::can_remove($folder)) {
-            $delete = T_('Delete'); ?>
-        <li>
-            <a id="<?php echo 'delete_folder_' . $folder->id; ?>" href="<?php echo $web_path; ?>/folders.php?action=delete&folder_id=<?php echo $folder->id; ?>">
-                <?php echo Ui::get_material_symbol('close', $delete); ?>
-                <?php echo $delete; ?>
-            </a>
-        </li>
-        <?php
-        } ?>
-    </ul>
 </div>
 <?php Ui::show_box_bottom(); ?>
 <div id="additional_information">
