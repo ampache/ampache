b54835c99a702d9ca10797f4930bfe8ebb227b90
lachlan <lachlan.00@gmail.com>
2026-06-19 16:01:10 +1000

    sorts missing

--- src/Module/Database/Query/FolderQuery.php
+++ src/Module/Database/Query/FolderQuery.php
@@ -54,9 +54,11 @@ final class FolderQuery implements QueryInterface
         'last_count',
         'last_update',
         'name',
+        'object_count',
         'rand',
         'rating',
         'title',
+        'total_count',
         'type',
         'user_flag_rating',
         'user_flag',
@@ -182,23 +184,25 @@ final class FolderQuery implements QueryInterface
             case 'int_id':
             case 'last_count':
             case 'last_update':
+            case 'object_count':
+            case 'total_count':
             case 'type':
             case 'user':
                 $sql = sprintf('`folder`.`%s`', $field);
                 break;
             case 'rating':
                 $sql = sprintf('`rating`.`rating` %s, `rating`.`date`', $order);
-                $query->set_join_and_and('LEFT', "`rating`", "`rating`.`object_id`", "`folder`.`int_id`", "`rating`.`object_type`", "'folder'", "`rating`.`user`", (string)$query->user_id, 100);
+                $query->set_join_and_and('LEFT', "`rating`", "`rating`.`object_id`", "`folder`.`object_id`", "`rating`.`object_type`", "'folder'", "`rating`.`user`", (string)$query->user_id, 100);
                 break;
             case 'user_flag':
             case 'userflag':
                 $sql = "`user_flag`.`date`";
-                $query->set_join_and_and('LEFT', "`user_flag`", "`user_flag`.`object_id`", "`folder`.`int_id`", "`user_flag`.`object_type`", "'folder'", "`user_flag`.`user`", (string)$query->user_id, 100);
+                $query->set_join_and_and('LEFT', "`user_flag`", "`user_flag`.`object_id`", "`folder`.`object_id`", "`user_flag`.`object_type`", "'folder'", "`user_flag`.`user`", (string)$query->user_id, 100);
                 break;
             case 'user_flag_rating':
                 $sql = sprintf('`user_flag`.`date` %s, `rating`.`rating` %s, `rating`.`date`', $order, $order);
-                $query->set_join_and_and('LEFT', "`user_flag`", "`user_flag`.`object_id`", "`folder`.`int_id`", "`user_flag`.`object_type`", "'folder'", "`user_flag`.`user`", (string)$query->user_id, 100);
-                $query->set_join_and_and('LEFT', "`rating`", "`rating`.`object_id`", "`folder`.`int_id`", "`rating`.`object_type`", "'folder'", "`rating`.`user`", (string)$query->user_id, 100);
+                $query->set_join_and_and('LEFT', "`user_flag`", "`user_flag`.`object_id`", "`folder`.`object_id`", "`user_flag`.`object_type`", "'folder'", "`user_flag`.`user`", (string)$query->user_id, 100);
+                $query->set_join_and_and('LEFT', "`rating`", "`rating`.`object_id`", "`folder`.`object_id`", "`rating`.`object_type`", "'folder'", "`rating`.`user`", (string)$query->user_id, 100);
                 break;
             default:
                 $sql = '';
