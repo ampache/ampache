b44ca5a79a19315bebcb0bcfb1ac412df7c26307
lachlan <lachlan.00@gmail.com>
2026-06-24 08:47:20 +1000

    always count on garbage if doing all items

--- src/Module/Catalog/Update/UpdateCatalog.php
+++ src/Module/Catalog/Update/UpdateCatalog.php
@@ -325,7 +325,7 @@ final class UpdateCatalog extends AbstractCatalogUpdater implements UpdateCatalo
             Catalog::clean_empty_albums();
             Album::update_album_artist();
             Catalog::update_counts();
-            if ($scanFolders && empty($catalogName)) {
+            if (empty($catalogName)) {
                 $catalog?->count_scan_folders($interactor);
             }
             $interactor->info(
