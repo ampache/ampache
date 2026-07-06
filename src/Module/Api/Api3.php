ba7eba443de30095e76bcc553a4864efc6b6c42d
lachlan <lachlan.00@gmail.com>
2026-07-07 08:31:26 +1000

    Update Api3.php

--- src/Module/Api/Api3.php
+++ src/Module/Api/Api3.php
@@ -21,7 +21,7 @@ declare(strict_types=0);
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
-*/
+ */
 
 namespace Ampache\Module\Api;
 
