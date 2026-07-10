34e9da4546e6ce677c7250f97b3ffa7acab6661f
lachlan <lachlan.00@gmail.com>
2026-07-10 19:59:16 +1000

    Update SongRepository.php

--- src/Repository/SongRepository.php
+++ src/Repository/SongRepository.php
@@ -269,9 +269,8 @@ final readonly class SongRepository implements SongRepositoryInterface
             Album::check_album_map($song->album, 'song', $song_artist_id);
         }
 
-        Dba::write("DELETE FROM `artist_map` WHERE `artist_map`.`object_type` = 'album' AND `artist_map`.`object_id` IN (SELECT `id` FROM `album` WHERE `album_artist` IS NULL);", [], true);
-        Dba::write("DELETE FROM `artist_map` WHERE `artist_map`.`object_type` = 'album' AND `artist_map`.`object_id` NOT IN (SELECT `album` FROM `song`);", [], true);
-        Dba::write("DELETE FROM `artist_map` WHERE `artist_map`.`object_type` = 'song' AND `artist_map`.`object_id` NOT IN (SELECT `id` FROM `song`);", [], true);
+        Dba::write("DELETE FROM `artist_map` WHERE `artist_map`.`object_type` = 'album' AND `artist_map`.`object_id` = ? AND (EXISTS (SELECT 1 FROM `album` WHERE `album`.`id` = ? AND `album`.`album_artist` IS NULL) OR NOT EXISTS (SELECT 1 FROM `song` WHERE `song`.`album` = ?));", [$song->album, $song->album, $song->album], true);
+        Dba::write("DELETE FROM `artist_map` WHERE `artist_map`.`object_type` = 'song' AND `artist_map`.`object_id` = ?;", [$song->id], true);
     }
 
     /**
