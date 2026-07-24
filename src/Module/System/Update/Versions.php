d045c580173fb7834d30819cbd5da7907a456153
fufroma <fufroma@users.noreply.github.com>
2026-07-24 04:46:48 +0200

    relabel misleading custom metadata disable preference (#4413)
    "disabled_custom_metadata_fields_input" was shown as "Define field
    list" but feeds the disable list, so entered fields get dropped and
    deleted on catalog update. Rename to "Additional fields to disable"
    and migrate existing installs.
    
    Fix #4260

--- src/Module/System/Update/Versions.php
+++ src/Module/System/Update/Versions.php
@@ -354,6 +354,7 @@ use Ampache\Module\System\Update\Migration\V8\Migration800022;
 use Ampache\Module\System\Update\Migration\V8\Migration800023;
 use Ampache\Module\System\Update\Migration\V8\Migration800024;
 use Ampache\Module\System\Update\Migration\V8\Migration800025;
+use Ampache\Module\System\Update\Migration\V8\Migration800026;
 use Generator;
 
 /**
@@ -361,7 +362,7 @@ use Generator;
  */
 final class Versions
 {
-    public const int MAXIMUM_UPDATABLE_VERSION = 800025; // AMPACHE_VERSION (db_version)
+    public const int MAXIMUM_UPDATABLE_VERSION = 800026; // AMPACHE_VERSION (db_version)
 
     /** @var array<int, class-string<MigrationInterface>> List of available migrations */
     private static array $versions = [
@@ -694,6 +695,7 @@ final class Versions
         800023 => Migration800023::class,
         800024 => Migration800024::class,
         800025 => Migration800025::class,
+        800026 => Migration800026::class,
     ];
 
     /**
