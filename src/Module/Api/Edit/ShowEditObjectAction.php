3f9b9c71b685fdc53467e33552f2a1bf55bde1d8
lachlan <lachlan.00@gmail.com>
2026-07-27 08:25:30 +1000

    make EditObjectAction testable
    a common problem

--- src/Module/Api/Edit/ShowEditObjectAction.php
+++ src/Module/Api/Edit/ShowEditObjectAction.php
@@ -30,7 +30,9 @@ use Ampache\Module\Authorization\GuiGatekeeperInterface;
 use Ampache\Module\Util\UiInterface;
 use Ampache\Repository\Model\Browse;
 use Ampache\Repository\Model\library_item;
+use Ampache\Repository\Model\LibraryItemLoaderInterface;
 use Ampache\Repository\Model\Share;
+use Ampache\Repository\ShareRepositoryInterface;
 use Ampache\Repository\UserRepositoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -50,10 +52,12 @@ final class ShowEditObjectAction extends AbstractEditAction
         ResponseFactoryInterface $responseFactory,
         StreamFactoryInterface $streamFactory,
         ConfigContainerInterface $configContainer,
+        LibraryItemLoaderInterface $libraryItemLoader,
         LoggerInterface $logger,
+        ShareRepositoryInterface $shareRepository,
         UiInterface $ui,
     ) {
-        parent::__construct($configContainer, $logger);
+        parent::__construct($configContainer, $libraryItemLoader, $logger, $shareRepository);
         $this->responseFactory = $responseFactory;
         $this->streamFactory   = $streamFactory;
         $this->ui              = $ui;
