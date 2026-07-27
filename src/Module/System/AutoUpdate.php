6185c76d0c20ebcadd674ea1d45054bfc9ec3e3e
lachlan <lachlan.00@gmail.com>
2026-07-28 07:35:46 +1000

    changed function name
    should have checked that

--- src/Module/System/AutoUpdate.php
+++ src/Module/System/AutoUpdate.php
@@ -325,11 +325,11 @@ class AutoUpdate
      */
     public static function show_ampache_message(): void
     {
-        if (self::is_develop()) {
+        if (self::_is_develop()) {
             echo '<div id="autoupdate">';
             echo '<span>' . T_("WARNING") . '</span>';
             echo ' (Ampache Develop is about to go through a major change!)<br />';
-            echo '<a href="https://github.com/ampache/ampache/pull/4387' . '" target="_blank">' . T_('View changes') . '</a><br /> ';
+            echo '<a href="https://github.com/ampache/ampache/pull/4387" target="_blank">' . T_('View changes') . '</a><br /> ';
             echo '</div>';
         }
     }
