02b4f0c1ce88e4df334cc529a10cdeb80781fad2
lachlan <lachlan.00@gmail.com>
2026-08-04 07:23:50 +1000

    Validate transcode resolution before the shell
    %RESOLUTION% reached proc_open unescaped.
    Only WIDTHxHEIGHT passes now; quality cast to int.

--- src/Module/Playback/Stream.php
+++ src/Module/Playback/Stream.php
@@ -963,9 +963,14 @@ class Stream
             '%FILE%' => $song_file,
         ];
         if ($media instanceof Video) {
-            $string_map['%RESOLUTION%'] = $options['resolution'] ?? $media->get_f_resolution() ?? '1280x720';
-            $string_map['%QUALITY%']    = (isset($options['quality']))
-                ? (31 * (101 - $options['quality'])) / 100
+            $resolution = (string) ($options['resolution'] ?? $media->get_f_resolution() ?? '1280x720');
+
+            // the command is handed to a shell, so only a literal WIDTHxHEIGHT is allowed to reach %RESOLUTION%
+            $string_map['%RESOLUTION%'] = (preg_match('/^\d{1,5}x\d{1,5}$/', $resolution) === 1)
+                ? $resolution
+                : '1280x720';
+            $string_map['%QUALITY%'] = (isset($options['quality']))
+                ? (31 * (101 - (int) $options['quality'])) / 100
                 : 10;
         }
 
