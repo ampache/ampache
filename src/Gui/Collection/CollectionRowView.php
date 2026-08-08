1b8114bda1849aa52b187507c33a15ebe47adfc3
lachlan <lachlan.00@gmail.com>
2026-08-08 17:14:34 +1000

    migrate the collection browse

---
+++ src/Gui/Collection/CollectionRowView.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * vim:set softtabstop=4 shiftwidth=4 expandtab:
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright Ampache.org, 2001-2026
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Ampache\Gui\Collection;
+
+use Ampache\Gui\View\AbstractView;
+use Ampache\Repository\Model\Collection;
+use Override;
+
+/**
+ * One row of the collection browse.
+ */
+final class CollectionRowView extends AbstractView
+{
+    public function __construct(
+        private readonly string $webPath,
+        private readonly Collection $collection,
+        private readonly string $coverClass,
+        private readonly bool $showDirectPlay,
+        private readonly bool $showRatings,
+    ) {}
+
+    /**
+     * Linked rather than picker-enabled: `Art::display()` only offers the edit and clear actions when it is
+     * given no link, and on a browse row the thumbnail should navigate to the collection.
+     */
+    public function getArt(): string
+    {
+        ob_start();
+        $this->collection->display_art(['width' => 100, 'height' => 100], true, true);
+
+        return (string) ob_get_clean();
+    }
+
+    public function getCollectionId(): int
+    {
+        return $this->collection->getId();
+    }
+
+    public function getCoverClass(): string
+    {
+        return $this->coverClass;
+    }
+
+    public function getFullname(): string
+    {
+        return $this->collection->getFullname();
+    }
+
+    public function getItemCount(): int
+    {
+        return $this->collection->get_item_count();
+    }
+
+    public function getLink(): string
+    {
+        return $this->collection->get_link();
+    }
+
+    /**
+     * A collection with no pinned type holds anything, which reads better than an empty cell.
+     */
+    public function getObjectType(): string
+    {
+        return ($this->collection->object_type === null || $this->collection->object_type === '')
+            ? T_('Mixed')
+            : $this->collection->object_type;
+    }
+
+    public function getOwner(): string
+    {
+        return (string) $this->collection->username;
+    }
+
+    public function getType(): string
+    {
+        return (string) $this->collection->type;
+    }
+
+    public function getWebPath(): string
+    {
+        return $this->webPath;
+    }
+
+    public function mayCollaborate(): bool
+    {
+        return $this->collection->has_collaborate();
+    }
+
+    public function mayDelete(): bool
+    {
+        return $this->collection->has_access();
+    }
+
+    public function showDirectPlay(): bool
+    {
+        return $this->showDirectPlay && $this->getItemCount() > 0;
+    }
+
+    public function showRatings(): bool
+    {
+        return $this->showRatings;
+    }
+
+    #[Override]
+    protected function templateFile(): string
+    {
+        return $this->findTemplate('collection_row.phtml');
+    }
+}
--- src/Module/Database/Query/Browse.php
+++ src/Module/Database/Query/Browse.php
@@ -142,7 +142,6 @@ class Browse extends Query
         'album' => 'show_albums.inc.php',
         'album_disk' => 'show_album_disks.inc.php',
         'artist' => 'show_artists.inc.php',
-        'collection' => 'show_collections.inc.php',
         'collection_items' => 'show_collection_items.inc.php',
         'playlist_media' => 'show_playlist_medias.inc.php',
         'song' => 'show_songs.inc.php',
