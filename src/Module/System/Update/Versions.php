2a03a139ec9973e47cbf3bb304753b1cd967c917
Alan <fufroma@gmail.com>
2026-08-12 19:14:54 +0200

    Forgot MAXIMUM_UPDATABLE_VERSION

--- src/Module/System/Update/Versions.php
+++ src/Module/System/Update/Versions.php
@@ -386,7 +386,7 @@ use Generator;
  */
 final class Versions
 {
-    public const int MAXIMUM_UPDATABLE_VERSION = 800050; // AMPACHE_VERSION (db_version)
+    public const int MAXIMUM_UPDATABLE_VERSION = 800051; // AMPACHE_VERSION (db_version)
 
     /** @var array<int, class-string<MigrationInterface>> List of available migrations */
     private static array $versions = [
