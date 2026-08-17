095635c6e7b0a85763579c289a20c489919fc393
lachlan <lachlan.00@gmail.com>
2026-08-18 08:45:30 +1000

    version

--- src/Module/System/Update/Versions.php
+++ src/Module/System/Update/Versions.php
@@ -381,7 +381,7 @@ use Ampache\Module\System\Update\Migration\V8\Migration800050;
 use Ampache\Module\System\Update\Migration\V8\Migration801001;
 use Ampache\Module\System\Update\Migration\V8\Migration801002;
 use Ampache\Module\System\Update\Migration\V8\Migration801003;
-use Ampache\Module\System\Update\Migration\V8\Migration801004;
+use Ampache\Module\System\Update\Migration\V8\Migration802001;
 use Generator;
 
 /**
@@ -389,7 +389,7 @@ use Generator;
  */
 final class Versions
 {
-    public const int MAXIMUM_UPDATABLE_VERSION = 801004; // AMPACHE_VERSION (db_version)
+    public const int MAXIMUM_UPDATABLE_VERSION = 802001; // AMPACHE_VERSION (db_version)
 
     /** @var array<int, class-string<MigrationInterface>> List of available migrations */
     private static array $versions = [
@@ -749,7 +749,7 @@ final class Versions
         801001 => Migration801001::class,
         801002 => Migration801002::class,
         801003 => Migration801003::class,
-        801004 => Migration801004::class,
+        802001 => Migration802001::class,
     ];
 
     /**
