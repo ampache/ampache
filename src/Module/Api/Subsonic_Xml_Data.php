9e81c1bc5632e3606433b4d6651f9e0c93088a90
lachlan <lachlan.00@gmail.com>
2026-09-15 11:02:45 +1000

    fix #4505
    Warming child object caches for folder/browse listings called
    Tag::build_cache() instead of build_object_tag_cache('song', ...),
    so every song's tags were fetched one at a time regardless of
    folder size. Also wrap the Subsonic/OpenSubsonic action dispatch
    in try/catch so an uncaught error returns a real error response
    instead of an empty 500.

--- src/Module/Api/Subsonic_Xml_Data.php
+++ src/Module/Api/Subsonic_Xml_Data.php
@@ -2166,6 +2166,6 @@ class Subsonic_Xml_Data
         Song::build_cache($songIds);
         Rating::build_cache('song', $songIds);
         Userflag::build_cache('song', $songIds);
-        Tag::build_cache($songIds);
+        Tag::build_object_tag_cache('song', $songIds);
     }
 }
