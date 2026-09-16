6f9c0cc3fbd91e1352743cf06426d641db1984c6
lachlan <lachlan.00@gmail.com>
2026-09-16 15:21:01 +1000

    put that into the css so you can change it

--- src/Plugin/AmpachePersonalFavorites.php
+++ src/Plugin/AmpachePersonalFavorites.php
@@ -126,9 +126,9 @@ class AmpachePersonalFavorites extends AmpachePlugin implements PluginDisplayHom
                         // $item[1] is `playlist` or `search`; labelling a smartlist as a playlist sent
                         // the context menu after whichever playlist happened to share that id
                         echo '<tr id="' . $item[1] . '_' . $item[0]->id . '" class="libitem_menu" data-object-type="' . $item[1] . '" data-object-id="' . $item[0]->id . '">';
-                        echo '<td style="min-height: 50px;">' . $item[0]->get_f_link() . '</td>';
-                        echo '<td style="height: auto;">';
-                        echo '<span style="margin-right: 10px;">';
+                        echo '<td class="personalfav-link">' . $item[0]->get_f_link() . '</td>';
+                        echo '<td>';
+                        echo '<span class="personalfav-actions">';
                         if (AmpConfig::get('directplay')) {
                             echo Ajax::button('?page=stream&action=directplay&object_type=' . $item[1] . '&object_id=' . $item[0]->id, 'play_circle', T_('Play'), 'play_playlist_' . $item[0]->id);
                             if (Stream_Playlist::check_autoplay_next()) {
